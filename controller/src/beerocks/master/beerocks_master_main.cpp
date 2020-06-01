2a848a924b7704d7dcf2e342dcc1ed7b4016fe71
Vitaly Bukhovsky <vitaly@devalore.com>
2020-06-01 14:09:23 +0000

    dummy commit
    Signed-off-by: Vitaly Bukhovsky <vitaly@devalore.com>

--- controller/src/beerocks/master/beerocks_master_main.cpp
+++ controller/src/beerocks/master/beerocks_master_main.cpp
@@ -47,8 +47,6 @@
 
 //#endif
 
-// #include <string>
-
 // Do not use this macro anywhere else in ire process
 // It should only be there in one place in each executable module
 BEEROCKS_INIT_BEEROCKS_VERSION
