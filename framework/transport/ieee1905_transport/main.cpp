08915599ce917d0028df20db8316e5322591e717
Mario Maz <mmazzapater@gmail.com>
2020-07-31 19:15:07 +0200

    transport: use new InterfaceStateManager
    https://jira.prplfoundation.org/browse/PPM-18
    
    Use the InterfaceStateManager class in the ieee1905_transport process
    as a new mechanism to read and detect changes in the state of the
    network interfaces. Existing code in `ieee1905_transport_netlink.cpp`
    is deprecated and will be removed in next commit.
    
    InterfaceStateManager uses interface names. Since we should use
    interface names everywhere rather than interface indexes (because
    interfaces are not renamed while prplMesh is running and conversely,
    indexes can change, and because names are easier to debug than
    indexes), modify existing code to favor names over indexes. BTW, this
    change also fixes a bug in calls to `handle_interface_status_change`
    that use a file descriptor when they shall be using an interface index.
    
    Signed-off-by: Mario Maz <mmazzapater@gmail.com>

--- framework/transport/ieee1905_transport/main.cpp
+++ framework/transport/ieee1905_transport/main.cpp
@@ -11,11 +11,17 @@
 #include <bcl/beerocks_backport.h>
 #include <bcl/beerocks_defines.h>
 #include <bcl/beerocks_event_loop_impl.h>
+#include <bcl/network/interface_flags_reader_impl.h>
+#include <bcl/network/interface_state_manager_impl.h>
+#include <bcl/network/interface_state_monitor_impl.h>
+#include <bcl/network/interface_state_reader_impl.h>
+#include <bcl/network/sockets_impl.h>
 
 #include <net/if.h>
 #include <unistd.h>
 
 using namespace beerocks;
+using namespace beerocks::net;
 using namespace beerocks::transport;
 
 static std::shared_ptr<EventLoop> create_event_loop()
@@ -40,6 +46,40 @@ create_broker_server(const std::shared_ptr<EventLoop> &event_loop)
     return std::make_shared<broker::BrokerServer>(server_socket, event_loop);
 }
 
+static std::shared_ptr<InterfaceStateManager>
+create_interface_state_manager(const std::shared_ptr<EventLoop> &event_loop)
+{
+    // Create NETLINK_ROUTE netlink socket for kernel/user-space communication
+    auto socket = std::make_shared<NetlinkRouteSocket>();
+
+    // Create client socket
+    ClientSocketImpl<NetlinkRouteSocket> client(socket);
+
+    // Bind client socket to "route netlink" multicast group to listen for multicast packets sent
+    // from the kernel containing network interface create/delete/up/down events
+    if (!client.bind(NetlinkAddress(RTMGRP_LINK))) {
+        return nullptr;
+    }
+
+    // Create connection to send/receive data using this socket
+    auto connection = std::make_shared<SocketConnectionImpl>(socket);
+
+    // Create the interface state monitor
+    auto interface_state_monitor =
+        std::make_unique<InterfaceStateMonitorImpl>(connection, event_loop);
+
+    // Create the interface flags reader
+    auto interface_flags_reader = std::make_shared<InterfaceFlagsReaderImpl>();
+
+    // Create the interface state reader
+    auto interface_state_reader =
+        std::make_unique<InterfaceStateReaderImpl>(interface_flags_reader);
+
+    // Create the interface state manager
+    return std::make_shared<InterfaceStateManagerImpl>(std::move(interface_state_monitor),
+                                                       std::move(interface_state_reader));
+}
+
 int main(int argc, char *argv[])
 {
     mapf::Logger::Instance().LoggerInit("transport");
@@ -48,48 +88,47 @@ int main(int argc, char *argv[])
      * Create required objects in the order defined by the dependency tree.
      */
     auto event_loop = create_event_loop();
-    auto broker     = create_broker_server(event_loop);
+    LOG_IF(!event_loop, FATAL) << "Unable to create event loop!";
 
-    /**
-     * Application exit code: 0 on success and -1 on failure.
-     * From this point on, there's a single exit point to allow for start/stop methods to be
-     * cleanly called in pairs.
-     */
-    int exit_code = 0;
+    auto broker = create_broker_server(event_loop);
+    LOG_IF(!broker, FATAL) << "Unable to create message broker!";
+
+    auto interface_state_manager = create_interface_state_manager(event_loop);
+    LOG_IF(!interface_state_manager, FATAL) << "Unable to create interface state manager!";
 
     /**
      * Create the IEEE1905 transport process.
      */
-    Ieee1905Transport ieee1905_transport(broker, event_loop);
+    Ieee1905Transport ieee1905_transport(interface_state_manager, broker, event_loop);
 
     /**
      * Start the message broker
      */
-    if (broker->start()) {
+    LOG_IF(!broker->start(), FATAL) << "Unable to start message broker!";
 
-        /**
-         * Start the IEEE1905 transport process
-         */
-        ieee1905_transport.run();
+    /**
+     * Start the IEEE1905 transport process
+     */
+    LOG_IF(!ieee1905_transport.start(), FATAL) << "Unable to start transport process!";
 
-        /**
-         * Run the application event loop
-         */
-        MAPF_INFO("starting main loop...");
-        while (0 == exit_code) {
-            if (event_loop->run() < 0) {
-                LOG(ERROR) << "Broker event loop failure!";
-                exit_code = -1;
-            }
+    /**
+     * Run the application event loop
+     */
+    MAPF_INFO("starting main loop...");
+    int exit_code = 0;
+    while (0 == exit_code) {
+        if (event_loop->run() < 0) {
+            LOG(ERROR) << "Broker event loop failure!";
+            exit_code = -1;
         }
-        MAPF_INFO("done");
-
-        broker->stop();
-
-    } else {
-        LOG(ERROR) << "Unable to start message broker!";
-        exit_code = -1;
     }
+    MAPF_INFO("done");
+
+    /**
+     * Stop running components and clean resources
+     */
+    ieee1905_transport.stop();
+    broker->stop();
 
     return exit_code;
 }
