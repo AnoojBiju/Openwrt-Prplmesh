prplfoundation/prplMesh

PPM-30: Define test entrypoint and json config
Entrypoint calls the json for "in compose" tests

Signed-off-by: pablo <pablo@odkq.com>

tests/run_bf_compose.sh
@@ -1,0 +1,18 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+# SPDX-FileCopyrightText: 2020 the prplMesh contributors (see AUTHORS.md)
+# This code is subject to the terms of the BSD+Patent license.
+# See LICENSE file for more details.
+
+scriptdir=$(dirname "$(readlink -f "${0}")")
+bf_plugins_dir=${scriptdir}/boardfarm_plugins
+
+if [ -n "${PYTHONPATH}" ]; then
+   PYTHONPATH="${bf_plugins_dir}:${scriptdir}:${PYTHONPATH}"
+else
+   PYTHONPATH="${bf_plugins_dir}:${scriptdir}"
+fi
+echo "$PYTHONPATH"
+export PYTHONPATH
+
+exec bft -c "${bf_plugins_dir}"/boardfarm_prplmesh/prplmesh_config_compose.json -n prplmesh_docker -x test_flows

AnoojBiju/Openwrt-Prplmesh

tests/run_bf_compose.sh
@@ -1,0 +1,18 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+# SPDX-FileCopyrightText: 2020 the prplMesh contributors (see AUTHORS.md)
+# This code is subject to the terms of the BSD+Patent license.
+# See LICENSE file for more details.
+
+scriptdir=$(dirname "$(readlink -f "${0}")")
+bf_plugins_dir=${scriptdir}/boardfarm_plugins
+
+if [ -n "${PYTHONPATH}" ]; then
+   PYTHONPATH="${bf_plugins_dir}:${scriptdir}:${PYTHONPATH}"
+else
+   PYTHONPATH="${bf_plugins_dir}:${scriptdir}"
+fi
+echo "$PYTHONPATH"
+export PYTHONPATH
+export BFT_DEBUG=y
+exec bft -c "${bf_plugins_dir}"/boardfarm_prplmesh/prplmesh_config_compose.json -n prplmesh_compose -x test_flows