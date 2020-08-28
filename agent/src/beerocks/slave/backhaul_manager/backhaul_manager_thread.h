a072057fbd6587c2f817973d8d58f840c3c66bf7
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2020-08-28 15:44:12 +0300

    agent: link metrics task: move associated_sta_link_metrics from bhm
    https://jira.prplfoundation.org/browse/PPM-346
    
    Move `handle_associated_sta_link_metrics_query` from backhaul manager to link metrics collection task.
    To do so several changes were made:
    - std::string -> sMacAddr conversion for the `src_mac` argument.
    - change of the return type of `handle_associated_sta_link_metrics_query` to `void`.
      `return` statements changed accordingly.
    - replace `std::dec` with `std::hex` to print message ID as a hexadecimal number.
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@gmail.com>

--- agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.h
+++ agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.h
@@ -128,8 +128,6 @@ private:
     bool handle_ap_capability_query(ieee1905_1::CmduMessageRx &cmdu_rx, const std::string &src_mac);
     bool handle_client_capability_query(ieee1905_1::CmduMessageRx &cmdu_rx,
                                         const std::string &src_mac);
-    bool handle_associated_sta_link_metrics_query(ieee1905_1::CmduMessageRx &cmdu_rx,
-                                                  const std::string &src_mac);
     bool handle_multi_ap_policy_config_request(ieee1905_1::CmduMessageRx &cmdu_rx,
                                                const std::string &src_mac);
     bool handle_ap_metrics_query(ieee1905_1::CmduMessageRx &cmdu_rx, const std::string &src_mac);
@@ -327,6 +325,7 @@ private:
         bool he_supported = false; /**< Is HE supported flag */
     };
 
+public:
     /**
      * @brief Gets radio info for the radio with given MAC address
      *
@@ -335,7 +334,6 @@ private:
      */
     std::shared_ptr<sRadioInfo> get_radio(const sMacAddr &radio_mac) const;
 
-public:
     /**
      * @brief Interface in this device which connects to an interface in one or more neighbors.
      *
