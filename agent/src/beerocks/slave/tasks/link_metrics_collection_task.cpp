8c17c60a36d1ffae34ab239ede01a2605f5e0c66
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2020-08-28 15:44:12 +0300

    agent: link metrics task: move handle_link_metric_query from bhm
    https://jira.prplfoundation.org/browse/PPM-346
    
    Move `handle_1905_link_metric_query` from backhaul manager to link metrics collection task.
    To do so several changes were made:
    - std::string -> sMacAddr conversion for the `src_mac` argument.
    - add namespace prefix: `network_utils` -> `net::network_utils`.
    - change of the return type of `handle_1905_link_metric_query` to `void`.
      `return` statements changed accordingly.
    - drop `1905` from the method name. Since the vendor-specific flow is being replaced
      by IEEE1905.1 messages it make more sence to explicitly mark remaining vendor-specific
      messages than standard ones.
    - remove `(int)` conversion for message IDs in logs.
      This conversion is done for 16-bit values by default (but not 8-bit ones).
    - make `if (invalid_neighbor)` branch return early.
      - explicitly state in the log that the supplied neighbour is invalid.
      - this allows to decrease indentation in `else` branch.
    - move few headers from BHM to the task
    - make several BHM methods and structs public because the code of the task is still
      highly coupled with the backhaul manager code. This issue needs to be solved
      in the future commits.
    - change years in copyright statement from `2020` to `2016-2020` because the old code
      was moved to the file.
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@gmail.com>

--- agent/src/beerocks/slave/tasks/link_metrics_collection_task.cpp
+++ agent/src/beerocks/slave/tasks/link_metrics_collection_task.cpp
@@ -1,13 +1,17 @@
 /* SPDX-License-Identifier: BSD-2-Clause-Patent
  *
- * SPDX-FileCopyrightText: 2020 the prplMesh contributors (see AUTHORS.md)
+ * SPDX-FileCopyrightText: 2016-2020 the prplMesh contributors (see AUTHORS.md)
  *
  * This code is subject to the terms of the BSD+Patent license.
  * See LICENSE file for more details.
  */
 
 #include "link_metrics_collection_task.h"
+#include "../agent_db.h"
 #include "../backhaul_manager/backhaul_manager_thread.h"
+
+#include <tlvf/ieee_1905_1/tlvLinkMetricQuery.h>
+#include <tlvf/ieee_1905_1/tlvLinkMetricResultCode.h>
 
 namespace beerocks {
 
@@ -22,12 +26,181 @@ bool LinkMetricsCollectionTask::handle_cmdu(ieee1905_1::CmduMessageRx &cmdu_rx,
                                             std::shared_ptr<beerocks_header> beerocks_header)
 {
     switch (cmdu_rx.getMessageType()) {
+    case ieee1905_1::eMessageType::LINK_METRIC_QUERY_MESSAGE: {
+        handle_link_metric_query(cmdu_rx, src_mac);
+        break;
+    }
     default: {
         // Message was not handled, therefore return false.
         return false;
     }
     }
     return true;
+}
+
+void LinkMetricsCollectionTask::handle_link_metric_query(ieee1905_1::CmduMessageRx &cmdu_rx,
+                                                              const sMacAddr &src_mac)
+{
+    const auto mid = cmdu_rx.getMessageId();
+    LOG(DEBUG) << "Received LINK_METRIC_QUERY_MESSAGE, mid=" << std::hex << mid;
+
+    /**
+     * The IEEE 1905.1 standard says about the Link Metric Query TLV and the neighbor type octet
+     * that "If the value is 0, then the EUI48 field is not present; if the value is 1, then the
+     * EUI-48 field shall be present."
+     *
+     * However, optional fields are not currently supported by TLVF.
+     *
+     * As a workaround, instead of defining a tlvLinkMetricQuery TLV with an optional field, we
+     * have defined two different TLVs: tlvLinkMetricQuery with the optional field and
+     * tlvLinkMetricQueryAllNeighbors without it. Application must check which of both TLVs has
+     * been received inside the message.
+     */
+    std::shared_ptr<ieee1905_1::tlvLinkMetricQueryAllNeighbors> tlvLinkMetricQueryAllNeighbors;
+    std::shared_ptr<ieee1905_1::tlvLinkMetricQuery> tlvLinkMetricQuery;
+
+    tlvLinkMetricQueryAllNeighbors = cmdu_rx.getClass<ieee1905_1::tlvLinkMetricQueryAllNeighbors>();
+    if (!tlvLinkMetricQueryAllNeighbors) {
+        tlvLinkMetricQuery = cmdu_rx.getClass<ieee1905_1::tlvLinkMetricQuery>();
+        if (!tlvLinkMetricQuery) {
+            LOG(ERROR) << "getClass ieee1905_1::tlvLinkMetricQueryAllNeighbors and "
+                          "ieee1905_1::tlvLinkMetricQuery failed";
+            return;
+        }
+    }
+
+    auto db = AgentDB::get();
+
+    /**
+     * 1905.1 AL MAC address of the device that transmits the response message.
+     */
+    sMacAddr reporter_al_mac = db->bridge.mac;
+
+    /**
+     * 1905.1 AL MAC address of a neighbor of the receiving device.
+     * Query can specify a particular neighbor device or all neighbor devices.
+     */
+    sMacAddr neighbor_al_mac = net::network_utils::ZERO_MAC;
+
+    /**
+     * Obtain link metrics for either all neighbors or a specific neighbor
+     */
+    ieee1905_1::eLinkMetricNeighborType neighbor_type;
+
+    /**
+     * The link metrics type requested: TX, RX or both
+     */
+    ieee1905_1::eLinkMetricsType link_metrics_type;
+
+    if (tlvLinkMetricQuery) {
+        /**
+    	   * If tlvLinkMetricQuery has been included in message, we will be permissive enough to
+    	   * allow it specify ALL_NEIGHBORS and if so, then we will just ignore the field
+    	   * containing the MAC address of neighbor.
+    	   */
+        neighbor_type     = tlvLinkMetricQuery->neighbor_type();
+        neighbor_al_mac   = tlvLinkMetricQuery->mac_al_1905_device();
+        link_metrics_type = tlvLinkMetricQuery->link_metrics_type();
+    } else {
+        neighbor_type = tlvLinkMetricQueryAllNeighbors->neighbor_type();
+        if (ieee1905_1::eLinkMetricNeighborType::ALL_NEIGHBORS != neighbor_type) {
+            LOG(ERROR) << "Unexpected neighbor type: " << std::hex << int(neighbor_type);
+            return;
+        }
+        link_metrics_type = tlvLinkMetricQueryAllNeighbors->link_metrics_type();
+    }
+
+    /**
+     * Set alias flag to true if link metrics for a specific neighbor have been requested
+     */
+    bool specific_neighbor =
+        ieee1905_1::eLinkMetricNeighborType::SPECIFIC_NEIGHBOR == neighbor_type;
+
+    /**
+     * Create response message
+     */
+    auto cmdu_tx_header =
+        m_cmdu_tx.create(mid, ieee1905_1::eMessageType::LINK_METRIC_RESPONSE_MESSAGE);
+    if (!cmdu_tx_header) {
+        LOG(ERROR) << "Failed creating LINK_METRIC_RESPONSE_MESSAGE header! mid=" << std::hex
+                   << mid;
+        return;
+    }
+
+    /**
+     * Get the list of neighbor links from the topology database.
+     * Neighbors are grouped by the interface that connects to them.
+     */
+    std::map<backhaul_manager::sLinkInterface, std::vector<backhaul_manager::sLinkNeighbor>>
+        neighbor_links_map;
+    if (!m_btl_ctx.get_neighbor_links(neighbor_al_mac, neighbor_links_map)) {
+        LOG(ERROR) << "Failed to get the list of neighbor links";
+        return;
+    }
+
+    /**
+     * If the specified neighbor 1905.1 AL ID does not identify a neighbor of the receiving 1905.1
+     * AL, then a link metric ResultCode TLV (see Table 6-21) with a value set to “invalid
+     * neighbor” shall be included in this message.
+     */
+    bool invalid_neighbor = specific_neighbor && neighbor_links_map.empty();
+    if (invalid_neighbor) {
+        auto tlvLinkMetricResultCode = m_cmdu_tx.addClass<ieee1905_1::tlvLinkMetricResultCode>();
+        if (!tlvLinkMetricResultCode) {
+            LOG(ERROR) << "addClass ieee1905_1::tlvLinkMetricResultCode failed, mid=" << std::hex
+                       << mid;
+            return;
+        }
+
+        LOG(INFO) << "Invalid neighbor 1905.1 AL ID specified: " << neighbor_al_mac;
+
+        tlvLinkMetricResultCode->value() = ieee1905_1::tlvLinkMetricResultCode::INVALID_NEIGHBOR;
+
+        LOG(DEBUG) << "Sending LINK_METRIC_RESPONSE_MESSAGE (invalid neighbour), mid: " << std::hex
+                   << mid;
+        m_btl_ctx.send_cmdu_to_broker(m_cmdu_tx, tlvf::mac_to_string(src_mac),
+                                      tlvf::mac_to_string(db->bridge.mac));
+        return;
+    }
+
+    /**
+     * Report link metrics for the link with specific neighbor or for all neighbors, as
+     * obtained from topology database
+     */
+    for (const auto &entry : neighbor_links_map) {
+        auto interface        = entry.first;
+        const auto &neighbors = entry.second;
+
+        std::unique_ptr<link_metrics_collector> collector =
+            m_btl_ctx.create_link_metrics_collector(interface);
+        if (!collector) {
+            continue;
+        }
+
+        for (const auto &neighbor : neighbors) {
+
+            LOG(TRACE) << "Getting link metrics for interface " << interface.iface_name
+                       << " (MediaType = " << std::hex << (int)interface.media_type
+                       << ") and neighbor " << neighbor.iface_mac;
+
+            sLinkMetrics link_metrics;
+            if (!collector->get_link_metrics(interface.iface_name, neighbor.iface_mac,
+                                             link_metrics)) {
+                LOG(ERROR) << "Unable to get link metrics for interface "
+                           << interface.iface_name << " and neighbor " << neighbor.iface_mac;
+                return;
+            }
+
+            if (!m_btl_ctx.add_link_metrics(reporter_al_mac, interface, neighbor, link_metrics,
+                                            link_metrics_type)) {
+                return;
+            }
+        }
+    }
+
+    LOG(DEBUG) << "Sending LINK_METRIC_RESPONSE_MESSAGE, mid: " << std::hex << mid;
+    m_btl_ctx.send_cmdu_to_broker(m_cmdu_tx, tlvf::mac_to_string(src_mac),
+                                  tlvf::mac_to_string(db->bridge.mac));
 }
 
 } // namespace beerocks
