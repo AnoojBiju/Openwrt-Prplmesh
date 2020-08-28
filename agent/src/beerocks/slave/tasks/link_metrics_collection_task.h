142f6b058bf859fbd248e68f2d46da128050263d
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2020-08-28 15:44:12 +0300

    agent: link metrics task: move handle_link_metric_query from bhm
    https://jira.prplfoundation.org/browse/PPM-346
    
    Move `handle_1905_link_metric_query` from backhaul manager to link metrics collection task.
    To do so several changes were made:
    - std::string -> sMacAddr conversion for the `src_mac` argument.
    - add namespace prefix: `network_utils` -> `net::network_utils`.
    - change of the return type of `handle_1905_link_metric_query` to `void`.
      `return` statements changed accordingly.
    - drop `1905` from the method name. Since the vendor-specific flow is being replaced
      by IEEE1905.1 messages it make more sence to explicitly mark remaining vendor-specific
      messages than standard ones.
    - make `if (invalid_neighbor)` branch return early.
      - explicitly state in the log that the supplied neighbour is invalid.
      - this allows to decrease indentation in `else` branch.
    - move few headers from BHM to the task
    - make several BHM methods and structs public because the code of the task is still
      highly coupled with the backhaul manager code. This issue needs to be solved
      in the future commits.
    - change years in copyright statement from `2020` to `2016-2020` because the old code
      was moved to the file.
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@gmail.com>

--- agent/src/beerocks/slave/tasks/link_metrics_collection_task.h
+++ agent/src/beerocks/slave/tasks/link_metrics_collection_task.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause-Patent
  *
- * SPDX-FileCopyrightText: 2020 the prplMesh contributors (see AUTHORS.md)
+ * SPDX-FileCopyrightText: 2016-2020 the prplMesh contributors (see AUTHORS.md)
  *
  * This code is subject to the terms of the BSD+Patent license.
  * See LICENSE file for more details.
@@ -29,6 +29,8 @@ public:
 private:
     backhaul_manager &m_btl_ctx;
     ieee1905_1::CmduMessageTx &m_cmdu_tx;
+
+    void handle_link_metric_query(ieee1905_1::CmduMessageRx &cmdu_rx, const sMacAddr &src_mac);
 };
 
 } // namespace beerocks
