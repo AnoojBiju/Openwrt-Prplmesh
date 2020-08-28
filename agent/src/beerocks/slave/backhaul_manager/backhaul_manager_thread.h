5bd1e75ed5d70e50cc24ef802814b1f65259f24b
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2020-08-28 15:44:12 +0300

    agent: link metrics task: move m_ap_metric_query from bhm
    https://jira.prplfoundation.org/browse/PPM-346
    
    Move `m_ap_metric_query` and `struct sApMetricsQuery`
    from backhaul manager to link metrics collection task.
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@gmail.com>

--- agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.h
+++ agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.h
@@ -446,11 +446,6 @@ public:
         wfa_map::tlvAssociatedStaLinkMetrics::sBssidInfo bssid_info;
     };
 
-    struct sApMetricsQuery {
-        Socket *soc;
-        sMacAddr bssid;
-    };
-
     struct sApMetrics {
         sMacAddr bssid;
         uint8_t channel_utilization;
@@ -467,8 +462,6 @@ private:
     };
 
 public:
-    std::vector<sApMetricsQuery> m_ap_metric_query;
-
     std::vector<sApMetricsResponse> m_ap_metric_response;
 
 private:
--- agent/src/beerocks/slave/tasks/link_metrics_collection_task.h
+++ agent/src/beerocks/slave/tasks/link_metrics_collection_task.h
@@ -13,6 +13,8 @@
 
 #include <tlvf/CmduMessageTx.h>
 
+#include "bcl/network/socket.h"
+
 namespace beerocks {
 
 // Forward decleration for backhaul_manager context saving
@@ -75,6 +77,13 @@ private:
      * AP Metrics Reporting configuration and status information.
      */
     sApMetricsReportingInfo m_ap_metrics_reporting_info;
+
+    struct sApMetricsQuery {
+        Socket *soc;
+        sMacAddr bssid;
+    };
+
+    std::vector<sApMetricsQuery> m_ap_metric_query;
 };
 
 } // namespace beerocks
