1b2c1542bd2c927a0c39607df02c9e3286ee8844
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2020-08-28 15:44:12 +0300

    agent: link metrics task: move handle_multi_ap_policy_config from bhm
    https://jira.prplfoundation.org/browse/PPM-346
    
    Move `handle_multi_ap_policy_config_request` from backhaul manager to link metrics collection task.
    To do so several changes were made:
    - std::string -> sMacAddr conversion for the `src_mac` argument.
    - change of the return type of `handle_multi_ap_policy_config_request` to `void`.
      `return` statements changed accordingly.
    - move few headers from BHM to the task.
    - make `struct sRadio` public in BHM because the code of the task is still
      highly coupled with the backhaul manager code. This issue needs to be solved
      in the future commits.
    - move a word from a comment to the next line to fit to 100-character line length
    - do preparations to move link metrics collection to agent context:
      - add a TODO to send message to fronthaul instead of slave.
      - replace "... to slave" with "... to fronthaul" in debug output.
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@gmail.com>

--- agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.h
+++ agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.h
@@ -72,10 +72,10 @@ public:
      */
     bool start_wps_pbc(const sMacAddr &radio_mac);
 
-private:
     // Forward declaration
     struct sRadioInfo;
 
+private:
     std::shared_ptr<bwl::sta_wlan_hal> get_selected_backhaul_sta_wlan_hal();
 
     virtual bool handle_cmdu(Socket *sd, ieee1905_1::CmduMessageRx &cmdu_rx) override;
@@ -113,8 +113,6 @@ private:
     bool handle_1905_1_message(ieee1905_1::CmduMessageRx &cmdu_rx, const std::string &src_mac,
                                Socket *&forward_to);
     // 1905 messages handlers
-    bool handle_multi_ap_policy_config_request(ieee1905_1::CmduMessageRx &cmdu_rx,
-                                               const std::string &src_mac);
     bool handle_slave_ap_metrics_response(ieee1905_1::CmduMessageRx &cmdu_rx,
                                           const std::string &src_mac);
     bool handle_channel_selection_request(ieee1905_1::CmduMessageRx &cmdu_rx,
@@ -245,6 +243,7 @@ private:
 
     TaskPool m_task_pool;
 
+public:
     /**
      * AP Metrics Reporting configuration and status information type.
      */
@@ -262,7 +261,6 @@ private:
         std::chrono::steady_clock::time_point last_reporting_time_point;
     } ap_metrics_reporting_info;
 
-public:
     /**
      * Unsuccessful Association Policy
      */
@@ -293,7 +291,6 @@ public:
         uint32_t number_of_reports_in_last_minute = 0;
     } unsuccessful_association_policy;
 
-private:
     /**
      * @brief Information gathered about a radio (= slave).
      *
