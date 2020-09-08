6a6b6adec2bb788435f4109f53201ba6d813a8c7
Mario Maz <mmazzapater@gmail.com>
2020-09-08 11:21:39 +0200

    agent: platform_manager: add CmduParser
    https://jira.prplfoundation.org/browse/PPM-591
    
    Add CmduParser member variable in platform_manager::main_thread.
    Create CmduParser in main and pass it to platform manager constructor.
    
    This is a preparatory commit. Next commits will use provided CMDU parser
    to get CMDU messages out of data received through a socket connection.
    
    Signed-off-by: Mario Maz <mmazzapater@gmail.com>

---
+++ common/beerocks/bcl/source/beerocks_cmdu_utils.cpp
@@ -0,0 +1,84 @@
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * SPDX-FileCopyrightText: 2016-2020 the prplMesh contributors (see AUTHORS.md)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#include <bcl/beerocks_cmdu_utils.h>
+
+#include <bcl/beerocks_message_structs.h>
+
+#include <tlvf/CmduMessage.h>
+#include <tlvf/ieee_1905_1/tlvVendorSpecific.h>
+
+#include <easylogging++.h>
+
+namespace beerocks {
+
+bool CmduUtils::verify_cmdu(uint8_t *data, size_t length)
+{
+    if (length < ieee1905_1::cCmduHeader::get_initial_size() + sizeof(ieee1905_1::sTlvHeader)) {
+        LOG(ERROR) << "Invalid CMDU length: " << length;
+        return false;
+    }
+
+    auto tlv = reinterpret_cast<ieee1905_1::sTlvHeader *>(
+        data + ieee1905_1::cCmduHeader::get_initial_size());
+
+    bool ret = true;
+
+    do {
+
+        auto tlv_type = static_cast<ieee1905_1::eTlvType>(tlv->type);
+
+        uint16_t tlv_length = tlv->length;
+        swap_16(tlv_length);
+
+        if (tlv_type == ieee1905_1::eTlvType::TLV_VENDOR_SPECIFIC) {
+            auto tlv_vendor_specific =
+                ieee1905_1::tlvVendorSpecific(reinterpret_cast<uint8_t *>(tlv),
+                                              sizeof(ieee1905_1::sTlvHeader) + tlv_length, true);
+            if (!tlv_vendor_specific.isInitialized()) {
+                LOG(ERROR) << "tlvVendorSpecific init() failure";
+                ret = false;
+                break;
+            }
+
+            if (tlv_vendor_specific.vendor_oui() ==
+                ieee1905_1::tlvVendorSpecific::eVendorOUI::OUI_INTEL) {
+                // assuming that the magic is the first data on the beerocks header
+                auto beerocks_magic =
+                    *(reinterpret_cast<uint32_t *>(tlv_vendor_specific.payload()));
+                swap_32(beerocks_magic);
+                if (beerocks_magic != message::MESSAGE_MAGIC) {
+                    LOG(WARNING) << "mismatch magic " << std::hex << int(beerocks_magic)
+                                 << " != " << int(message::MESSAGE_MAGIC) << std::dec;
+                    ret = false;
+                    break;
+                }
+
+            } else {
+                LOG(INFO) << "Not an Intel vendor specific message!";
+            }
+
+            // cancel the swap we did
+            tlv_vendor_specific.class_swap();
+        } else if ((tlv_type == ieee1905_1::eTlvType::TLV_END_OF_MESSAGE) && (0 == tlv_length)) {
+            return true;
+        }
+
+        // set the next tlv
+        tlv = reinterpret_cast<ieee1905_1::sTlvHeader *>(
+            reinterpret_cast<uint8_t *>(tlv) + sizeof(ieee1905_1::sTlvHeader) + tlv_length);
+
+    } while (reinterpret_cast<uint8_t *>(tlv) < (data + length));
+
+    LOG(ERROR) << "TLV end of message not found! tlv_type=" << tlv->type
+               << ", tlv_length=" << tlv->length;
+
+    return ret;
+}
+
+} // namespace beerocks
--- common/beerocks/bcl/source/network/cmdu_parser_stream_impl.cpp
+++ common/beerocks/bcl/source/network/cmdu_parser_stream_impl.cpp
@@ -8,11 +8,8 @@
 
 #include <bcl/network/cmdu_parser_stream_impl.h>
 
-#include <bcl/beerocks_backport.h>
-#include <bcl/beerocks_string_utils.h>
+#include <bcl/beerocks_cmdu_utils.h>
 #include <bcl/beerocks_utils.h>
-
-#include <tlvf/ieee_1905_1/tlvVendorSpecific.h>
 
 namespace beerocks {
 namespace net {
@@ -46,8 +43,8 @@ bool CmduParserStreamImpl::parse_cmdu(Buffer &buffer, uint32_t &iface_index, sMa
 
     // Verify and parse the message
     bool result = false;
-    if (!verify_cmdu(cmdu_data, cmdu_length)) {
-        LOG(ERROR) << "Failed verifying cmdu!";
+    if (!CmduUtils::verify_cmdu(cmdu_data, cmdu_length)) {
+        LOG(ERROR) << "Failed verifying CMDU!";
         LOG(DEBUG) << "hex_dump (" << cmdu_length << " bytes):" << std::endl
                    << utils::dump_buffer(cmdu_data, cmdu_length);
     } else if (cmdu_length > cmdu_rx.getMessageBuffLength()) {
@@ -56,8 +53,9 @@ bool CmduParserStreamImpl::parse_cmdu(Buffer &buffer, uint32_t &iface_index, sMa
     } else {
         std::copy_n(cmdu_data, cmdu_length, cmdu_rx.getMessageBuff());
         if (!cmdu_rx.parse()) {
-            LOG(ERROR) << "Failed parsing CMDU, rx_buffer: " << std::hex << data << std::dec
-                       << ", CMDU length =" << cmdu_length;
+            LOG(ERROR) << "Failed parsing CMDU!";
+            LOG(DEBUG) << "hex_dump (" << cmdu_length << " bytes):" << std::endl
+                       << utils::dump_buffer(cmdu_data, cmdu_length);
         } else {
             iface_index = uds_header->if_index;
             std::copy_n(uds_header->dst_bridge_mac, MAC_ADDR_LEN, dst_mac.oct);
@@ -72,70 +70,6 @@ bool CmduParserStreamImpl::parse_cmdu(Buffer &buffer, uint32_t &iface_index, sMa
     buffer.shift(message_length);
 
     return result;
-}
-
-bool CmduParserStreamImpl::verify_cmdu(uint8_t *data, size_t length)
-{
-    if (length < ieee1905_1::cCmduHeader::get_initial_size() + sizeof(ieee1905_1::sTlvHeader)) {
-        LOG(ERROR) << "Invalid CMDU length";
-        return false;
-    }
-
-    ieee1905_1::sTlvHeader *tlv = reinterpret_cast<ieee1905_1::sTlvHeader *>(
-        data + ieee1905_1::cCmduHeader::get_initial_size());
-
-    bool ret = true;
-
-    do {
-
-        auto tlv_type = static_cast<ieee1905_1::eTlvType>(tlv->type);
-
-        uint16_t tlv_length = tlv->length;
-        swap_16(tlv_length);
-
-        if (tlv_type == ieee1905_1::eTlvType::TLV_VENDOR_SPECIFIC) {
-            auto tlv_vendor_specific =
-                ieee1905_1::tlvVendorSpecific(reinterpret_cast<uint8_t *>(tlv),
-                                              sizeof(ieee1905_1::sTlvHeader) + tlv_length, true);
-            if (!tlv_vendor_specific.isInitialized()) {
-                LOG(ERROR) << "tlvVendorSpecific init() failure";
-                ret = false;
-                break;
-            }
-
-            if (tlv_vendor_specific.vendor_oui() ==
-                ieee1905_1::tlvVendorSpecific::eVendorOUI::OUI_INTEL) {
-                // assuming that the magic is the first data on the beerocks header
-                auto beerocks_magic =
-                    *(reinterpret_cast<uint32_t *>(tlv_vendor_specific.payload()));
-                swap_32(beerocks_magic);
-                if (beerocks_magic != message::MESSAGE_MAGIC) {
-                    LOG(WARNING) << "mismatch magic " << std::hex << int(beerocks_magic)
-                                 << " != " << int(message::MESSAGE_MAGIC) << std::dec;
-                    ret = false;
-                    break;
-                }
-
-            } else {
-                LOG(INFO) << "Not an Intel vendor specific message!";
-            }
-
-            // cancel the swap we did
-            tlv_vendor_specific.class_swap();
-        } else if ((tlv_type == ieee1905_1::eTlvType::TLV_END_OF_MESSAGE) && (0 == tlv_length)) {
-            return true;
-        }
-
-        // set the next tlv
-        tlv = reinterpret_cast<ieee1905_1::sTlvHeader *>(
-            reinterpret_cast<uint8_t *>(tlv) + sizeof(ieee1905_1::sTlvHeader) + tlv_length);
-
-    } while (reinterpret_cast<uint8_t *>(tlv) < (data + length));
-
-    LOG(ERROR) << "TLV end of message not found! tlv_type=" << tlv->type
-               << ", tlv_length=" << tlv->length;
-
-    return ret;
 }
 
 } // namespace net
