4a7a6adee0071d6166254b818565ff2b520eeba0
Vladyslav Tupikin <v.tupikin@inango-systems.com>
2020-09-14 13:45:30 +0300

    framework: hlapi: Add implementation for hlapi methods
    Need to add implementation for ambiorix class contructor,
    destructor, init and load data model methods.
    
    Signed-off-by: Vladyslav Tupikin <v.tupikin@inango-systems.com>

--- framework/platform/hlapi/hlapi.cpp
+++ framework/platform/hlapi/hlapi.cpp
@@ -5,7 +5,74 @@
  * This code is subject to the terms of the BSD+Patent license.
  * See LICENSE file for more details.
  */
+#include "hlapi.h"
 
-extern "C" {
-#include <amxc/amxc.h>
+using namespace nbapi;
+
+ambiorix::ambiorix()
+{
+    amxo_parser_init(&m_parser);
+    amxd_dm_init(&m_datamodel);
+}
+
+bool ambiorix::init(const std::string &amxb_backend, const std::string &bus_uri,
+                    const std::string &datamodel_path)
+{
+    LOG(DEBUG) << "Initializing the bus connection.";
+    int status = 0;
+
+    status = amxb_be_load(amxb_backend.c_str());
+    if (status != 0) {
+        LOG(ERROR) << "Failed to load backend, status: " << status;
+        return false;
+    }
+
+    // Connect to the bus
+    status = amxb_connect(&m_bus_ctx, bus_uri.c_str());
+    if (status != 0) {
+        LOG(ERROR) << "Failed to connect to the bus, status: " << status;
+        return false;
+    }
+
+    if (!load_datamodel(datamodel_path)) {
+        LOG(ERROR) << "Failed to load data model.";
+        return false;
+    }
+
+    status = amxb_register(m_bus_ctx, &m_datamodel);
+    if (status != 0) {
+        LOG(ERROR) << "Failed to register the data model.";
+        return false;
+    }
+
+    LOG(DEBUG) << "The bus connection initialized successfully.";
+    return true;
+}
+
+bool ambiorix::load_datamodel(const std::string &datamodel_path)
+{
+    LOG(DEBUG) << "Loading the data model.";
+    auto *root_obj = amxd_dm_get_root(&m_datamodel);
+    if (!root_obj) {
+        LOG(ERROR) << "Failed to get datamodel root object.";
+        return false;
+    }
+
+    // Disable eventing while loading odls
+    amxp_sigmngr_enable(&m_datamodel.sigmngr, false);
+
+    amxo_parser_parse_file(&m_parser, datamodel_path.c_str(), root_obj);
+
+    amxp_sigmngr_enable(&m_datamodel.sigmngr, true);
+
+    LOG(DEBUG) << "The data model loaded successfully.";
+    return true;
+}
+
+ambiorix::~ambiorix()
+{
+    amxb_free(&m_bus_ctx);
+    amxd_dm_clean(&m_datamodel);
+    amxo_parser_clean(&m_parser);
+    amxb_be_remove_all();
 }
