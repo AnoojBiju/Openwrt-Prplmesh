5ec5e51e64e1994e1b103556c1c3e08b3fea8eb6
Adam Dov <adamx.dov@intel.com>
2020-09-14 20:08:12 +0000

    db: align operating class calculations to hostapd expected
    The hostapd uses the operating-class and the channel to determine
    the frequency. This is required since int the the 6GHz band the same
    channel indexing is reused and thus channel number is not enough to
    determine the frequency.
    Although the translation in the hostapd is not new, it is only recently
    that this functionality is used and one of the flows that use it is
    RRM-beacon-requests.
    The prplmesh existing calculation of operating class is not aligned to
    what is expected by the hostapd and many RRM-beacon-requests are failed
    due to this misalignment.
    
    Changed the operating class calculation in the db to match the expected
    results in the hostapd.
    
    The full implementation of operating classes as defined by the IEEE
    requirement (IEEE 802.11-2016.pdf,  Annex E, " Country elements and
    operating classes") will be handled separately by PPM-26.
    
    Signed-off-by: Adam Dov <adamx.dov@intel.com>

--- controller/src/beerocks/master/db/db.cpp
+++ controller/src/beerocks/master/db/db.cpp
@@ -3878,12 +3878,21 @@ bool db::set_node_channel_bw(const std::string &mac, int channel, beerocks::eWiF
             n->hostap->vht_center_frequency      = vht_center_frequency;
             auto is_dfs                          = wireless_utils::is_dfs_channel(channel);
             set_hostap_is_dfs(mac, is_dfs);
-            if (channel <= 13)
+            if (channel >= 1 && channel <= 13) {
                 n->hostap->operating_class = 81;
-            else if (channel <= 48)
+            } else if (channel == 14) {
+                n->hostap->operating_class = 82;
+            } else if (channel >= 36 && channel <= 48) {
                 n->hostap->operating_class = 115;
-            else
+            } else if (channel >= 52 && channel <= 64) {
+                n->hostap->operating_class = 118;
+            } else if (channel >= 100 && channel <= 140) {
                 n->hostap->operating_class = 121;
+            } else if (channel >= 149 && channel <= 169) {
+                n->hostap->operating_class = 125;
+            } else {
+                LOG(ERROR) << "Unsupported Operating Class for channel=" << channel;
+            }
         } else {
             LOG(ERROR) << __FUNCTION__ << " - node " << mac << " is null!";
             return false;
