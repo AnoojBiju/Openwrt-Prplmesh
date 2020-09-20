1253ba2b44f88e4516b352ad4221490e8293c1ab
Daniel Edri <daniel.edri@devalore.com>
2020-09-20 17:28:21 +0000

    master: db: add persistent_data_commit_task support
    Preparative Commit
    
    This is the 3rd and last branch related to the development
    of the recurring commit task for the bpl_db. The task should
    be triggered upon startup, there's a default value:
    DEFAULT_COMMIT_CHANGES_INTERVAL_VALUE but in the upcoming commit
    the blp_cfg there'll be an option to modify the value on runtime.
    
    The additions that are supplied in this batch are:
    - Adding three routines on DB.h/.cpp to return and reset the
    private datamember (db_changes_made); and to commit the data
    (wrapper function to bpl::db_commit_changes(); Adding required
    *task_id members and functions required for the task component
    (also on node.h)
    
    Signed-off-by: Daniel Edri <daniel.edri@devalore.com>

--- controller/src/beerocks/master/db/db.cpp
+++ controller/src/beerocks/master/db/db.cpp
@@ -3548,6 +3548,12 @@ bool db::set_node_stats_info(const std::string &mac, beerocks_message::sStaStats
 
 void db::clear_node_stats_info(const std::string &mac) { set_node_stats_info(mac, nullptr); }
 
+bool db::commit_db_changes() { return bpl::db_commit_changes(); }
+
+bool db::get_db_changes_made() { return db_changes_made; }
+
+void db::reset_db_changes_made() { db_changes_made = false; }
+
 int db::get_hostap_stats_measurement_duration(const std::string &mac)
 {
     auto n = get_node(mac);
