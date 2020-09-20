6adf1ebac85a2ce751ab52ab4f3a538c8a35554f
Daniel Edri <daniel.edri@devalore.com>
2020-09-20 17:56:15 +0000

    bpl_cfg: bee_master_main: add commit_changes_interval support
    The commit_changes_task should be rescheduled
    every certain time, so it can minimize the number
    of calls to the uci_commit, but eventually it
    suppose to also make sure things are written to the
    flash and no data is lost in between the downtime
    of each interval.
    
    Add missing functions to each platform (linux, RDKB)
    to obtain the runtime config value if present, otherwise
    take the default value instead. the addition is also
    based on the 1st commit, where we add the
    db_changes_interval declaration
    in the db.h/config structure
    
    Signed-off-by: Daniel Edri <daniel.edri@devalore.com>

--- framework/platform/bpl/linux/bpl_cfg.cpp
+++ framework/platform/bpl/linux/bpl_cfg.cpp
@@ -372,6 +372,21 @@ bool cfg_get_persistent_db_enable(bool &enable)
     return true;
 }
 
+bool cfg_get_commit_changes_interval(int &interval)
+{
+    int commit_changes_interval_value = DEFAULT_COMMIT_CHANGES_INTERVAL_VALUE;
+
+    // persistent db value is optional
+    if (cfg_get_param_int("commit_changes_interval=", commit_changes_interval_value) < 0) {
+        MAPF_DBG("Failed to read commit_changes_interval parameter - setting default value");
+        commit_changes_interval_value = DEFAULT_COMMIT_CHANGES_INTERVAL_VALUE;
+    }
+
+    interval = commit_changes_interval_value;
+
+    return true;
+}
+
 bool cfg_get_clients_persistent_db_max_size(int &max_size)
 {
     int max_size_val = -1;
