fa99867da955985648b32477bd30056ee4f8e917
Daniel Edri <daniel.edri@devalore.com>
2020-09-22 13:26:08 +0000

    fixup! master: db: add persistent_data_commit_task support

--- controller/src/beerocks/master/db/db.h
+++ controller/src/beerocks/master/db/db.h
@@ -1182,10 +1182,6 @@ private:
 
     std::queue<std::string> disconnected_slave_mac_queue;
 
-    /*
-    * a variable that indicates that data is awaiting to flashed from memory into
-    * the prplMesh.bb (non-rational persistent database file).
-    */
     bool db_changes_made = false;
 
     int slaves_stop_on_failure_attempts = 0;
