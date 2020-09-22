15306741c82a303671d04adbb8a7e212b435a667
Vladyslav Tupikin <v.tupikin@inango-systems.com>
2020-09-22 13:36:19 +0300

    framework: nbapi: Add ambiorix signal event loop
    Need to add ambiorix signal event loop by using
    amxp_signal_read() function.
    
    Signed-off-by: Vladyslav Tupikin <v.tupikin@inango-systems.com>

--- framework/platform/nbapi/nbapi.cpp
+++ framework/platform/nbapi/nbapi.cpp
@@ -52,6 +52,12 @@ bool Ambiorix::init(const std::string &amxb_backend, const std::string &bus_uri,
         return false;
     }
 
+    if (!init_signal_loop()) {
+        LOG(ERROR) << "Failed to initialize event handlers for the Ambiorix signals in the "
+                      "event loop.";
+        return false;
+    }
+
     LOG(DEBUG) << "The bus connection initialized successfully.";
     return true;
 }
@@ -116,6 +122,46 @@ bool Ambiorix::init_event_loop()
     return true;
 }
 
+bool Ambiorix::init_signal_loop()
+{
+    LOG(DEBUG) << "Register event handlers for the Ambiorix signals fd in the event loop.";
+
+    auto ambiorix_fd = amxp_signal_fd();
+    if (ambiorix_fd < 0) {
+        LOG(ERROR) << "Failed to get ambiorix file descriptor.";
+        return false;
+    }
+
+    EventLoop::EventHandlers handlers = {
+        .on_read =
+            [&](int fd, EventLoop &loop) {
+                amxp_signal_read();
+                return true;
+            },
+        // Not implemented
+        .on_write      = nullptr,
+        .on_disconnect = nullptr,
+
+        // Handle interface errors
+        .on_error =
+            [&](int fd, EventLoop &loop) {
+                LOG(ERROR) << "Error on ambiorix fd.";
+                return true;
+            },
+    };
+
+    if (!m_event_loop->register_handlers(ambiorix_fd, handlers)) {
+        LOG(ERROR) << "Couldn't register event handlers for the Ambiorix signals in the "
+                      "event loop.";
+        return false;
+    }
+
+    LOG(DEBUG) << "Event handlers for the Ambiorix signals fd: " << ambiorix_fd
+               << " successfully registered in the event loop.";
+
+    return true;
+}
+
 bool Ambiorix::remove_event_loop()
 {
     LOG(DEBUG) << "Remove event handlers for Ambiorix fd from the event loop.";
@@ -136,9 +182,32 @@ bool Ambiorix::remove_event_loop()
     return true;
 }
 
+bool Ambiorix::remove_signal_loop()
+{
+    LOG(DEBUG) << "Remove event handlers for the Ambiorix signals fd from the event loop.";
+
+    auto ambiorix_fd = amxp_signal_fd();
+    if (ambiorix_fd < 0) {
+        LOG(ERROR) << "Failed to get ambiorix file descriptor.";
+        return false;
+    }
+
+    if (!m_event_loop->remove_handlers(ambiorix_fd)) {
+        LOG(ERROR) << "Couldn't remove event handlers for the Ambiorix signals fd from the "
+                      "event loop.";
+        return false;
+    }
+
+    LOG(DEBUG) << "The event handlers for the Ambiorix signals fd removed successfully from the "
+                  "event loop.";
+
+    return true;
+}
+
 Ambiorix::~Ambiorix()
 {
     remove_event_loop();
+    remove_signal_loop();
     amxb_free(&m_bus_ctx);
     amxd_dm_clean(&m_datamodel);
     amxo_parser_clean(&m_parser);
