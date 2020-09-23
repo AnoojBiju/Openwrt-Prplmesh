d895c36440db7978f981b5befcd5ce6b320af5d5
Ran Regev <ran.regev@devalore.com>
2020-09-23 08:49:58 +0300

    backhaul: added unsuccessful association policy configuration
    Added configuration for unsuccessful association policy.
    Extract the policy when received inside
    Multi-AP Policy Config Request message
    
    Signed-off-by: Ran Regev <ran.regev@devalore.com>

--- framework/tlvf/src/src/CmduMessageRx.cpp
+++ framework/tlvf/src/src/CmduMessageRx.cpp
@@ -52,6 +52,7 @@
 #include <tlvf/wfa_map/tlvProfile2ApRadioAdvancedCapabilities.h>
 #include <tlvf/wfa_map/tlvProfile2Default802dotQSettings.h>
 #include <tlvf/wfa_map/tlvProfile2TrafficSeparationPolicy.h>
+#include <tlvf/wfa_map/tlvProfile2UnsuccessfulAssociationPolicy.h>
 #include <tlvf/wfa_map/tlvRadioOperationRestriction.h>
 #include <tlvf/wfa_map/tlvSearchedService.h>
 #include <tlvf/wfa_map/tlvStaMacAddressType.h>
@@ -274,6 +275,9 @@ std::shared_ptr<BaseClass> CmduMessageRx::parseNextTlv()
     }
     case (194): {
         return msg.addClass<wfa_map::tlvTunnelledData>();
+    }
+    case (196): {
+        return msg.addClass<wfa_map::tlvProfile2UnsuccessfulAssociationPolicy>();
     }
     default: {
         LOG(DEBUG) << "Unknown TLV type: " << tlv_type;
