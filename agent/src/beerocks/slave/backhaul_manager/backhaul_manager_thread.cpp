prplfoundation/prplMesh

commit e5ec2b1856bb50c698a795fedd141e4f55915767

agent: Use bssids (vaps) list from AgentDB
The bssids (vaps) list is being sent from the son_slave to the backhaul manager when updated,
and the backhaul manager saves a copy of it.
Replace the use of the backhaul manager's local copy of it with the list on the AgentDB.

On this opportunity, refactor the function "send_slave_ap_metric_query_message" to be more
efficient, by not require a mandatory bssid list, and be able to send the query on all bssids
if the given bssid list is empty.

PPM-216

Signed-off-by: Moran Shoeg <moranx.shoeg@intel.com>

agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.cpp
@@ -1060,16 +1060,6 @@ bool backhaul_manager::backhaul_fsm_main(bool &skip_select)
             if (elapsed_time_s >= ap_metrics_reporting_info.reporting_interval_s) {
                 ap_metrics_reporting_info.last_reporting_time_point = now;
 
-                std::vector<sMacAddr> bssid_list;
-                for (const auto &socket : slaves_sockets) {
-                    if (socket) {
-                        for (int i = 0; i < beerocks::IFACE_TOTAL_VAPS; ++i) {
-                            if (socket->vaps_list.vaps[i].mac != network_utils::ZERO_MAC) {
-                                bssid_list.push_back(socket->vaps_list.vaps[i].mac);
-                            }
-                        }
-                    }
-                }
                 // We must generate a new MID for the periodic AP Metrics Response messages that
                 // do not correspond to an AP Metrics Query message.
                 // We cannot set MID to 0 here because we must also differentiate periodic
@@ -1077,11 +1067,9 @@ bool backhaul_manager::backhaul_fsm_main(bool &skip_select)
                 // due to channel utilization crossed configured threshold value.
                 // As a temporary solution, set MID to UINT16_MAX here.
                 // TODO: to be fixed as part of #1328
-                if (!bssid_list.empty()) {
-                    send_slave_ap_metric_query_message(UINT16_MAX, bssid_list);
-                } else {
-                    LOG(DEBUG) << "Skipping AP_METRICS_QUERY for slave, empty BSSID list";
-                }
+
+                // Send ap_metrics query on all bssids exists on the Agent.
+                send_slave_ap_metric_query_message(UINT16_MAX);
             }
         }
 
@@ -1149,7 +1137,7 @@ bool backhaul_manager::backhaul_fsm_main(bool &skip_select)
     }
 
     return (true);
-}
+} // namespace beerocks
 
 bool backhaul_manager::send_1905_topology_discovery_message()
 {
@@ -1309,53 +1297,58 @@ bool backhaul_manager::send_autoconfig_search_message(const std::string &front_r
                                tlvf::mac_to_string(db->bridge.mac));
 }
 
-bool backhaul_manager::send_slave_ap_metric_query_message(uint16_t mid,
-                                                          const std::vector<sMacAddr> &bssid_list)
+bool backhaul_manager::send_slave_ap_metric_query_message(
+    uint16_t mid, const std::unordered_set<sMacAddr> &bssid_list)
 {
-    bool ret = false;
-    for (auto socket : slaves_sockets) {
-        for (const auto &mac : bssid_list) {
-            int i = 0;
-            if (mac == socket->vaps_list.vaps[i].mac) {
-                LOG(DEBUG) << "Forwarding AP_METRICS_QUERY_MESSAGE message to son_slave, bssid: "
-                           << std::hex << tlvf::mac_to_string(mac);
+    auto db = AgentDB::get();
 
-                auto forward =
-                    cmdu_tx.create(mid, ieee1905_1::eMessageType::AP_METRICS_QUERY_MESSAGE);
-                if (!forward) {
-                    LOG(ERROR) << "Failed to create AP_METRICS_QUERY_MESSAGE";
-                    return false;
-                }
-
-                auto query = cmdu_tx.addClass<wfa_map::tlvApMetricQuery>();
-                if (!query) {
-                    LOG(ERROR) << "Failed addClass<wfa_map::tlvApMetricQuery>";
-                    return false;
-                }
-
-                if (!query->alloc_bssid_list(1)) {
-                    LOG(ERROR) << "Failed allocate memory for bssid_list";
-                    return false;
-                }
-
-                auto list         = query->bssid_list(0);
-                std::get<0>(list) = true;
-                std::get<1>(list) = mac;
-
-                if (!message_com::send_cmdu(socket->slave, cmdu_tx)) {
-                    LOG(ERROR) << "Failed forwarding AP_METRICS_QUERY_MESSAGE message to son_slave";
-                    ret = false;
-                    continue;
-                } else {
-                    ret = true;
-                    // Fill a query vector
-                    m_ap_metric_query.push_back({socket->slave, mac});
-                }
+    for (const auto &radio : db->get_radios_list()) {
+        if (!radio) {
+            continue;
+        }
+        for (const auto &bssid : radio->front.bssids) {
+            if (!bssid_list.empty() && bssid_list.find(bssid.mac) == bssid_list.end()) {
+                continue;
             }
-            i++;
+            LOG(DEBUG) << "Forwarding AP_METRICS_QUERY_MESSAGE message to son_slave, bssid: "
+                       << bssid.mac;
+
+            if (!cmdu_tx.create(mid, ieee1905_1::eMessageType::AP_METRICS_QUERY_MESSAGE)) {
+                LOG(ERROR) << "Failed to create AP_METRICS_QUERY_MESSAGE";
+                return false;
+            }
+
+            auto query = cmdu_tx.addClass<wfa_map::tlvApMetricQuery>();
+            if (!query) {
+                LOG(ERROR) << "Failed addClass<wfa_map::tlvApMetricQuery>";
+                return false;
+            }
+
+            if (!query->alloc_bssid_list(1)) {
+                LOG(ERROR) << "Failed to allocate memory for bssid_list";
+                return false;
+            }
+
+            auto list = query->bssid_list(0);
+            if (!std::get<0>(list)) {
+                LOG(ERROR) << "Failed to get element of bssid_list";
+            }
+            std::get<1>(list) = bssid.mac;
+
+            auto radio_info = get_radio(radio->front.iface_mac);
+            if (!radio_info) {
+                LOG(ERROR) << "Failed to get radio info for " << radio->front.iface_mac;
+                return false;
+            }
+
+            if (!message_com::send_cmdu(radio_info->slave, cmdu_tx)) {
+                LOG(ERROR) << "Failed forwarding AP_METRICS_QUERY_MESSAGE message to son_slave";
+            }
+
+            m_ap_metric_query.push_back({radio_info->slave, bssid.mac});
         }
     }
-    return ret;
+    return true;
 }
 
 bool backhaul_manager::backhaul_fsm_wireless(bool &skip_select)
@@ -2159,10 +2152,19 @@ bool backhaul_manager::handle_slave_backhaul_message(std::shared_ptr<sRadioInfo>
             return false;
         }
 
-        soc->vaps_list = msg->params();
-        if (m_agent_ucc_listener) {
-            m_agent_ucc_listener->update_vaps_list(tlvf::mac_to_string(soc->radio_mac),
-                                                   msg->params());
+        // Create a local copy on this process database instance. Will be removed on PPM-83 phase 5
+        auto db    = AgentDB::get();
+        auto radio = db->radio(soc->hostap_iface);
+        if (!radio) {
+            LOG(DEBUG) << "Radio of iface " << soc->hostap_iface << " does not exist on the db";
+            return false;
+        }
+        for (uint8_t vap_idx = 0; vap_idx < eBeeRocksIfaceIds::IFACE_TOTAL_VAPS; vap_idx++) {
+            radio->front.bssids[vap_idx].mac  = msg->params().vaps[vap_idx].mac;
+            radio->front.bssids[vap_idx].ssid = msg->params().vaps[vap_idx].ssid;
+            radio->front.bssids[vap_idx].type = msg->params().vaps[vap_idx].backhaul_vap
+                                                    ? AgentDB::sRadio::sFront::sBssid::eType::bAP
+                                                    : AgentDB::sRadio::sFront::sBssid::eType::fAP;
         }
         break;
     }
@@ -2675,25 +2677,26 @@ bool backhaul_manager::handle_ap_capability_query(ieee1905_1::CmduMessageRx &cmd
 bool backhaul_manager::handle_ap_metrics_query(ieee1905_1::CmduMessageRx &cmdu_rx,
                                                const std::string &src_mac)
 {
-    std::vector<sMacAddr> bssid;
     const auto mid           = cmdu_rx.getMessageId();
     auto ap_metric_query_tlv = cmdu_rx.getClass<wfa_map::tlvApMetricQuery>();
     if (!ap_metric_query_tlv) {
         LOG(ERROR) << "AP Metrics Query CMDU mid=" << mid << " does not have AP Metric Query TLV";
         return false;
     }
+
+    std::unordered_set<sMacAddr> bssids;
     for (size_t bssid_idx = 0; bssid_idx < ap_metric_query_tlv->bssid_list_length(); bssid_idx++) {
         auto bssid_tuple = ap_metric_query_tlv->bssid_list(bssid_idx);
         if (!std::get<0>(bssid_tuple)) {
             LOG(ERROR) << "Failed to get bssid " << bssid_idx << " from AP_METRICS_QUERY";
             return false;
         }
-        bssid.push_back(std::get<1>(bssid_tuple));
+        bssids.insert(std::get<1>(bssid_tuple));
         LOG(DEBUG) << "Received AP_METRICS_QUERY_MESSAGE, mid=" << std::hex << int(mid)
                    << "  bssid " << std::get<1>(bssid_tuple);
     }
 
-    if (!send_slave_ap_metric_query_message(mid, bssid)) {
+    if (!send_slave_ap_metric_query_message(mid, bssids)) {
         LOG(ERROR) << "Failed to forward AP_METRICS_RESPONSE to the son_slave_thread";
         return false;
     }
@@ -3139,22 +3142,24 @@ bool backhaul_manager::handle_1905_topology_query(ieee1905_1::CmduMessageRx &cmd
         return false;
     }
 
-    for (const auto &slave : slaves_sockets) {
-        // TODO skip slaves that are not operational
-        auto vaps_list = slave->vaps_list;
+    for (const auto &radio : db->get_radios_list()) {
+        if (!radio) {
+            continue;
+        }
 
         auto radio_list         = tlvApOperationalBSS->create_radio_list();
-        radio_list->radio_uid() = slave->radio_mac;
-        for (const auto &vap : vaps_list.vaps) {
-            if (vap.mac == network_utils::ZERO_MAC)
+        radio_list->radio_uid() = radio->front.iface_mac;
+        for (const auto &bssid : radio->front.bssids) {
+            if (bssid.mac == network_utils::ZERO_MAC) {
                 continue;
-            if (vap.ssid[0] == '\0')
+            }
+            if (bssid.ssid.empty()) {
                 continue;
+            }
             auto radio_bss_list           = radio_list->create_radio_bss_list();
-            radio_bss_list->radio_bssid() = vap.mac;
-            auto ssid =
-                std::string(vap.ssid, strnlen(vap.ssid, beerocks::message::WIFI_SSID_MAX_LENGTH));
-            radio_bss_list->set_ssid(ssid);
+            radio_bss_list->radio_bssid() = bssid.mac;
+            radio_bss_list->set_ssid(bssid.ssid);
+
             radio_list->add_radio_bss_list(radio_bss_list);
         }
         tlvApOperationalBSS->add_radio_list(radio_list);

agent/src/beerocks/slave/agent_ucc_listener.cpp
@@ -7,6 +7,7 @@
  */
 
 #include "agent_ucc_listener.h"
+#include "agent_db.h"
 
 #include <bcl/network/network_utils.h>
 #include <beerocks/tlvf/beerocks_message.h>
@@ -80,24 +81,6 @@ void agent_ucc_listener::clear_configuration()
 }
 
 /**
- * @brief Update list of VAPs for ruid.
- *
- * @return None.
- */
-
-typedef struct {
-    std::string mac;
-    std::string ssid;
-    bool backhaul_vap;
-} sVapElement;
-void agent_ucc_listener::update_vaps_list(std::string ruid, beerocks_message::sVapsList &vaps)
-{
-    LOG(INFO) << "Update VAP map for ruid " << ruid << " bssid " << vaps.vaps->mac << " ssid "
-              << std::string(vaps.vaps->ssid, 36);
-    vaps_map[ruid] = vaps;
-}
-
-/**
  * @brief get parameter command
  *
  * get agent parameter
@@ -136,14 +119,16 @@ bool agent_ucc_listener::handle_dev_get_param(std::unordered_map<std::string, st
         auto ruid = tlvf::mac_to_string(std::strtoull(params["ruid"].c_str(), nullptr, 16));
         auto ssid = params["ssid"];
 
-        auto it = vaps_map.find(ruid);
-        if (it == vaps_map.end()) {
+        auto db    = AgentDB::get();
+        auto radio = db->get_radio_by_mac(tlvf::mac_from_string(ruid), AgentDB::eMacType::RADIO);
+        if (!radio) {
             value = "ruid " + ruid + " not found";
             return false;
         }
-        for (const auto &vap : it->second.vaps) {
-            if (std::string(vap.ssid) == ssid) {
-                value = tlvf::mac_to_string(vap.mac);
+
+        for (const auto &bssid : radio->front.bssids) {
+            if (bssid.ssid == ssid) {
+                value = tlvf::mac_to_string(bssid.mac);
                 return true;
             }
         }
@@ -183,16 +168,16 @@ static enum eFreqType band_to_freq(const std::string &band)
 bool agent_ucc_listener::handle_start_wps_registration(const std::string &band,
                                                        std::string &err_string)
 {
-    auto freq      = band_to_freq(band);
-    auto radio_mac = m_backhaul_manager_ctx.freq_to_radio_mac(freq);
-    if (radio_mac.empty()) {
+    auto freq          = band_to_freq(band);
+    auto radio_mac_str = m_backhaul_manager_ctx.freq_to_radio_mac(freq);
+    if (radio_mac_str.empty()) {
         err_string = "Failed to get radio for " + band;
         return false;
     }
 
-    LOG(DEBUG) << "Trigger WPS PBC on radio mac " << radio_mac;
+    LOG(DEBUG) << "Trigger WPS PBC on radio mac " << radio_mac_str;
     err_string = "Failed to start wps pbc";
-    return m_backhaul_manager_ctx.start_wps_pbc(tlvf::mac_from_string(radio_mac));
+    return m_backhaul_manager_ctx.start_wps_pbc(tlvf::mac_from_string(radio_mac_str));
 }
 
 /**

agent/src/beerocks/slave/agent_ucc_listener.h
@@ -76,7 +76,6 @@ private:
     bool m_in_reset        = false;
     bool m_reset_completed = false;
     std::string m_selected_backhaul; // "ETH" or "<RUID of the selected radio>"
-    std::unordered_map<std::string, beerocks_message::sVapsList> vaps_map;
 
     std::mutex mutex;
 };

agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.h
@@ -105,7 +105,17 @@ private:
      */
     bool send_1905_topology_discovery_message(const std::string &iface_name);
 
-    bool send_slave_ap_metric_query_message(uint16_t mid, std::vector<sMacAddr> const &bssid_list);
+    /**
+     * @brief Sends an AP Metrics Query message for each bssid on 'bssid_list' to the son_slaves.
+     * If the 'bssid_list' is empty, sends a query on each bssid that exists on the Agent.
+     * 
+     * @param mid MID of the message to be sent.
+     * @param bssid_list List of bssids to send a query on.
+     * @return true on success, otherwise false.
+     */
+    bool send_slave_ap_metric_query_message(
+        uint16_t mid,
+        const std::unordered_set<sMacAddr> &bssid_list = std::unordered_set<sMacAddr>());
 
     /**
      * @brief Creates Backhaul STA Steering Response message with 2 tlvs Steering Response
@@ -369,8 +379,7 @@ private:
         uint32_t vht_capability = 0;     /**< VHT capabilities */
         std::array<uint8_t, beerocks::message::VHT_MCS_SET_SIZE>
             vht_mcs_set; /**< 32-byte attribute containing the MCS set as defined in 802.11ac */
-        bool he_supported = false;             /**< Is HE supported flag */
-        beerocks_message::sVapsList vaps_list; /**< List of VAPs in radio. */
+        bool he_supported = false; /**< Is HE supported flag */
         std::array<beerocks::message::sWifiChannel, beerocks::message::SUPPORTED_CHANNELS_LENGTH>
             preferred_channels; /**< Array of supported channels in radio. */
         std::unordered_map<sMacAddr, associated_clients_t>

agent/src/beerocks/slave/son_slave_thread.cpp
@@ -1815,6 +1815,20 @@ bool slave_thread::handle_cmdu_ap_manager_message(Socket *sd,
 
         LOG(INFO) << "received ACTION_APMANAGER_HOSTAP_VAPS_LIST_UPDATE_NOTIFICATION";
 
+        auto db    = AgentDB::get();
+        auto radio = db->radio(m_fronthaul_iface);
+        if (!radio) {
+            LOG(DEBUG) << "Radio of iface " << m_fronthaul_iface << " does not exist on the db";
+            return false;
+        }
+        for (uint8_t vap_idx = 0; vap_idx < eBeeRocksIfaceIds::IFACE_TOTAL_VAPS; vap_idx++) {
+            radio->front.bssids[vap_idx].mac  = notification_in->params().vaps[vap_idx].mac;
+            radio->front.bssids[vap_idx].ssid = notification_in->params().vaps[vap_idx].ssid;
+            radio->front.bssids[vap_idx].type = notification_in->params().vaps[vap_idx].backhaul_vap
+                                                    ? AgentDB::sRadio::sFront::sBssid::eType::bAP
+                                                    : AgentDB::sRadio::sFront::sBssid::eType::fAP;
+        }
+
         auto notification_out = message_com::create_vs_message<
             beerocks_message::cACTION_CONTROL_HOSTAP_VAPS_LIST_UPDATE_NOTIFICATION>(cmdu_tx);
         if (notification_out == nullptr) {

AnoojBiju/Openwrt-Prplmesh

commit e3a71bb5b43ce1b31e68accfb73376e461cdf140

backhaul manager: filter out zero BSSID when forwarding ap metric query
To handle an AP Metric Query, the function
send_slave_metric_query_message generates a new message for each radio
which contains only a single BSSIDs of a VAP that belongs to that radio.
The BSSIDs are given by the bssid_list argument - when it is empty, all
VAPs that belong to the radio are included. That is the case for the
automatic time-based ap metric responses.

However, the bssids list is just a fixed-size array where the BSSID is
empty for unused VAPs. Thus, we're going to send a lot of requests with
a zero BSSID. The slave will reply to that with a reponse without
metrics, and this in the end gets ignored by the backhaul manager. As a
result, no response is ever sent out by the backhaul manager (because it
thinks there are still responses pending from the slaves).

This issue was introduced by commit e5ec2b1856b. Before that commit, the
list of BSSIDs was generated in backhaul_fsm_main itself, and it
filtered out empty BSSIDs.

To fix this, simply reintroduce the filtering of empty BSSIDs, but this
time in the send_slave_ap_metric_query_message function instead of in
backhaul_fsm_main.

While we're at it, remove some spurious EOL whitespace in comments
(automatic by editor).

Fixes: PPM-666

Note that this solution only restores the behaviour to what it was
before commit e5ec2b1856b. There is still a remaining issue: responses
will also be generated for BSSIDs that are actually unused (PPM-667).
However, since certification doesn't check for that, we can ignore the
issue.

Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.cpp
@@ -886,9 +886,9 @@ bool backhaul_manager::backhaul_fsm_main(bool &skip_select)
     // Successfully connected to the master
     case EState::CONNECTED: {
 
-        /** 
+        /**
          * According to the 1905.1 specification section 8.2.1.1 - A 1905.1 management entity shall
-         * transmit a topology discovery message every 60 seconds or if an "implementation-specific" 
+         * transmit a topology discovery message every 60 seconds or if an "implementation-specific"
          * event occurs (e.g., device initialized or an interface is connected).
          * Sending "AGENT_DEVICE_INITIALIZED" event will trigger sending of topology discovery
          * message.
@@ -1073,6 +1073,9 @@ bool backhaul_manager::send_slave_ap_metric_query_message(
             if (!bssid_list.empty() && bssid_list.find(bssid.mac) == bssid_list.end()) {
                 continue;
             }
+            if (bssid.mac == network_utils::ZERO_MAC) {
+                continue;
+            }
             LOG(DEBUG) << "Forwarding AP_METRICS_QUERY_MESSAGE message to son_slave, bssid: "
                        << bssid.mac;
 
@@ -1180,7 +1183,7 @@ bool backhaul_manager::backhaul_fsm_wireless(bool &skip_select)
                 }
 
                 /**
-                 * This code was disabled as part of the effort to pass certification flow 
+                 * This code was disabled as part of the effort to pass certification flow
                  * (PR #1469), and broke wireless backhual flow.
                  * If a connected backhual interface has been discovered, the backhaul fsm was set
                  * to MASTER_DISCOVERY state, otherwise to INITIATE_SCAN.