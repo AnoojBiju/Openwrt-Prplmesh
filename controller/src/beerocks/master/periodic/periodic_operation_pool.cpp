4395600fbe6ad38a664979d6597717a914a8b153
Itay Elenzweig <itayx.elenzweig@intel.com>
2020-09-24 12:38:31 +0300

    son: Add new periodic_operation class
    This is a preparative commit.
    
    To provide an easy simple to use, generic periodic-operation solution a
    new class type is needed.
    This new periodic-operations class type can be inherited from, similar
    to the generic task class, and will allow for small operations that need
    to work once per interval.
    
    This new class type is needed to reduce the usage of the Task class
    type, as small operation do not require all the extended utility that
    comes with the Task class type.
    
    - Add new periodic operations class
    - Add new periodic operations pool class
    
    Signed-off-by: Itay Elenzweig <itayx.elenzweig@intel.com>

---
+++ controller/src/beerocks/master/periodic/periodic_operation_pool.cpp
@@ -0,0 +1,45 @@
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * SPDX-FileCopyrightText: 2016-2020 the prplMesh contributors (see AUTHORS.md)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#include "periodic_operation_pool.h"
+
+#include <easylogging++.h>
+
+using namespace son;
+
+bool periodic_operation_pool::add_operation(std::shared_ptr<periodic_operation> new_operation)
+{
+    LOG(TRACE) << "inserting new operation, id=" << int(new_operation->id)
+               << " operation name=" << new_operation->operation_name;
+    return (periodic_operations.insert(std::make_pair(new_operation->id, new_operation))).second;
+}
+
+bool periodic_operation_pool::is_operation_alive(int id)
+{
+    auto it = periodic_operations.find(id);
+    if (it != periodic_operations.end() && it->second != nullptr) {
+        return true;
+    }
+    return false;
+}
+
+void periodic_operation_pool::kill_operation(int id)
+{
+    auto it = periodic_operations.find(id);
+    if (it != periodic_operations.end() && it->second != nullptr) {
+        LOG(DEBUG) << "killing operation " << it->second->operation_name << ", id " << it->first;
+        periodic_operations.erase(it);
+    }
+}
+
+void periodic_operation_pool::run_operations()
+{
+    for (auto oper : periodic_operations) {
+        oper.second->work();
+    }
+}
