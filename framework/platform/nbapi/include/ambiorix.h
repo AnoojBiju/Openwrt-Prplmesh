f7452c8c8d1a32bb9f6c7032a73569cf4632e055
Krystyna Oliinyk <k.oliinyk@inango-systems.com>
2020-09-24 16:12:54 +0300

    nbapi:include: add ambiorix interface
    Create an interface for AmbiorixImpl and AmbiorixDummy classes
    
    Signed-off-by: Krystyna Oliinyk <k.oliinyk@inango-systems.com>

---
+++ framework/platform/nbapi/include/ambiorix.h
@@ -0,0 +1,30 @@
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * SPDX-FileCopyrightText: 2020 the prplMesh contributors (see AUTHORS.md)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#ifndef AMBIORIX_H
+#define AMBIORIX_H
+
+/**
+ * @class Ambiorix
+ * @brief Interface for AmbiorixImpl and AmbiorixDummy classes
+ */
+class Ambiorix {
+public:
+    virtual bool set(const std::string &relative_path, const std::string &value)   = 0;
+    virtual bool set(const std::string &relative_path, const int32_t &value)       = 0;
+    virtual bool set(const std::string &relative_path, const int64_t &value)       = 0;
+    virtual bool set(const std::string &relative_path, const uint32_t &value)      = 0;
+    virtual bool set(const std::string &relative_path, const uint64_t &value)      = 0;
+    virtual bool set(const std::string &relative_path, const bool &value)          = 0;
+    virtual bool set(const std::string &relative_path, const double &value)        = 0;
+    virtual bool add_instance(const std::string &relative_path)                    = 0;
+    virtual bool remove_instance(const std::string &relative_path, uint32_t index) = 0;
+    virtual void stop()                                                            = 0;
+};
+
+#endif
