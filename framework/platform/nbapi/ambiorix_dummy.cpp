c39cd88b728f7aa581e492b9da7443a2bb2a5f57
Krystyna Oliinyk <k.oliinyk@inango-systems.com>
2020-09-24 16:16:10 +0300

    nbapi: add ambiorix dummy class
    An AmbiorixDummy class will have the same methods as AmbiorixImpl
    class has, but without underlying implementation.
    
    Create an AmbiorixDummy class.
    Implement dummy version of bool init() method
    
    Signed-off-by: Krystyna Oliinyk <k.oliinyk@inango-systems.com>

---
+++ framework/platform/nbapi/ambiorix_dummy.cpp
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * SPDX-FileCopyrightText: 2020 the prplMesh contributors (see AUTHORS.md)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#include "./include/ambiorix_dummy.h"
+
+AmbiorixDummy::AmbiorixDummy() {}
+
+AmbiorixDummy::~AmbiorixDummy() {}
+
+bool AmbiorixDummy::set(const std::string &relative_path, const std::string &value) { return true; }
+
+bool AmbiorixDummy::set(const std::string &relative_path, const int32_t &value) { return true; }
+
+bool AmbiorixDummy::set(const std::string &relative_path, const int64_t &value) { return true; }
+
+bool AmbiorixDummy::set(const std::string &relative_path, const uint32_t &value) { return true; }
+
+bool AmbiorixDummy::set(const std::string &relative_path, const uint64_t &value) { return true; }
+
+bool AmbiorixDummy::set(const std::string &relative_path, const bool &value) { return true; }
+
+bool AmbiorixDummy::set(const std::string &relative_path, const double &value) { return true; }
+
+bool AmbiorixDummy::add_instance(const std::string &relative_path) { return true; }
+
+bool AmbiorixDummy::remove_instance(const std::string &relative_path, uint32_t index)
+{
+    return true;
+}
+
+void AmbiorixDummy::stop() {}
