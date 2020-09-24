aed48a56c942b0c40fb3c7c8923c2c1cb72877c4
Itay Elenzweig <itayx.elenzweig@intel.com>
2020-09-24 15:04:14 +0300

    son_master: add persistent DB aging operation
    On init after the run-time DB is synced with the persistent DB, the
    aging operation needs to be initiated.
    This will cause the operation to run periodically every interval.
    
    Signed-off-by: Itay Elenzweig <itayx.elenzweig@intel.com>

--- controller/src/beerocks/master/son_master_thread.cpp
+++ controller/src/beerocks/master/son_master_thread.cpp
@@ -7,6 +7,7 @@
  */
 
 #include "son_master_thread.h"
+#include "periodic/persistent_database_aging.h"
 #include "son_actions.h"
 #include "son_management.h"
 #include "tasks/bml_task.h"
@@ -102,6 +103,16 @@ bool master_thread::init()
         }
     }
 
+    if (operations.is_operation_alive(database.get_persistent_db_aging_operation_id())) {
+        LOG(DEBUG) << "persistent DB aging operation already running";
+    } else {
+        auto aging_interval_seconds =
+            std::chrono::seconds(database.config.persistent_db_aging_interval);
+        auto new_operation =
+            std::make_shared<persistent_database_aging_operation>(aging_interval_seconds, database);
+        operations.add_operation(new_operation);
+    }
+
     if (!transport_socket_thread::init()) {
         LOG(ERROR) << "Failed init of transport_socket_thread";
         stop();
@@ -186,7 +197,7 @@ bool master_thread::work()
     }
 
     tasks.run_tasks();
-    operatios.run_operations();
+    operations.run_operations();
     return true;
 }
 
