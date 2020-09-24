70bc432c60deb4b7a0ff424569986dbd08649b24
Itay Elenzweig <itayx.elenzweig@intel.com>
2020-09-24 12:40:22 +0300

    son: persistent DB aging operation: Implementation
    As part of the persistent DB feature, clients that are stored
    persistently need to go thought an aging validation periodically.
    Currently the aging mechanism is only checked during the initial load
    to the run-time DB.
    
    A client's lifetime is compared against the time passed since it's last
    update, if a client passes its timelife threshold, it is cleared from
    the run-time DB and removed from the persistent DB.
    
    Signed-off-by: Itay Elenzweig <itayx.elenzweig@intel.com>

--- controller/src/beerocks/master/periodic/persistent_database_aging.cpp
+++ controller/src/beerocks/master/periodic/persistent_database_aging.cpp
@@ -19,4 +19,39 @@ persistent_database_aging_operation::persistent_database_aging_operation(
     last_aging_check = std::chrono::system_clock::time_point::min();
 }
 
-void persistent_database_aging_operation::work() {}
+void persistent_database_aging_operation::work()
+{
+    last_aging_check              = std::chrono::system_clock::now();
+    auto clients_                 = database.get_clients_with_persistent_data_configured();
+    std::vector<sMacAddr> clients = {clients_.begin(), clients_.end()};
+    auto aged_clients =
+        beerocks::utils::vector_filter<sMacAddr>(clients, [&](const sMacAddr &client_mac) {
+            const auto max_timelife_delay_sec =
+                std::chrono::seconds(database.config.max_timelife_delay_days * 24 * 3600);
+            const auto unfriendly_device_max_timelife_delay_sec = std::chrono::seconds(
+                database.config.unfriendly_device_max_timelife_delay_days * 24 * 3600);
+            // Client timelife delay
+            // If a client has a predetermined timelife delay use that.
+            // Otherwise use the Max timelife delay.
+            auto timelife_delay = database.get_client_time_life_delay(client_mac);
+            if (timelife_delay == std::chrono::seconds::zero()) {
+                timelife_delay = database.get_client_is_friendly(client_mac) == eTriStateBool::FALSE
+                                     ? unfriendly_device_max_timelife_delay_sec
+                                     : max_timelife_delay_sec;
+            }
+
+            // Calculate client expiry due time
+            auto parameters_last_edit = database.get_client_parameters_last_edit(client_mac);
+            auto expiry_due           = parameters_last_edit + timelife_delay;
+            // If the expiry due is less then the last aging check, the client is considered aged.
+            return expiry_due < last_aging_check;
+        });
+
+    if (aged_clients.size() > 0) {
+        OPERATION_LOG(TRACE) << "Found " << aged_clients.size()
+                             << " aged clients, clearing persistent data";
+    }
+    for (const auto &aged_client : aged_clients) {
+        database.clear_client_persistent_db(aged_client);
+    }
+}
