1740ef41863354071971a783a0c76f51e02cb444
Moran Shoeg <moranx.shoeg@intel.com>
2020-10-01 12:55:17 +0000

    agent: Fill channels list on AgentDB
    Fill the channels list received on APMANAGER_CHANNELS_LIST_RESPONSE
    handler on the AgentDB.
    
    Signed-off-by: Moran Shoeg <moranx.shoeg@intel.com>

--- agent/src/beerocks/slave/son_slave_thread.cpp
+++ agent/src/beerocks/slave/son_slave_thread.cpp
@@ -2192,6 +2192,20 @@ bool slave_thread::handle_cmdu_ap_manager_message(Socket *sd,
             return false;
         }
 
+        // Copy channels list to the AgentDB
+        auto channels_list_length = response->channel_list()->channels_list_length();
+        for (uint8_t ch_idx = 0; ch_idx < channels_list_length; ch_idx++) {
+            auto &channel_info = std::get<1>(response->channel_list()->channels_list(ch_idx));
+            auto channel       = channel_info.channel();
+            radio->channels_list[channel].tx_power_dbm = channel_info.tx_power_dbm();
+            radio->channels_list[channel].dfs_state    = channel_info.dfs_state();
+            auto supported_bw_size                     = channel_info.supported_bandwidths_length();
+            radio->channels_list[channel].supported_bw_list.resize(supported_bw_size);
+            std::copy_n(&std::get<1>(channel_info.supported_bandwidths(0)), supported_bw_size,
+                        radio->channels_list[channel].supported_bw_list.begin());
+        }
+
+        // Create Channel preference report
         auto tuple_preferred_channels = response->preferred_channels(0);
         radio->front.preferred_channels.resize(response->preferred_channels_size());
         std::copy_n(&std::get<1>(tuple_preferred_channels), response->preferred_channels_size(),
