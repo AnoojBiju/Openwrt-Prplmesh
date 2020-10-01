b73ee63cbf75bc689e7756c675d8b2df8b9c7dd8
Mario Maz <mmazzapater@gmail.com>
2020-10-01 17:14:04 +0200

    controller: add UCC server
    https://jira.prplfoundation.org/browse/PPM-658
    
    Add UCCServer member variable in beerocks_ucc_listener. Create
    UCCServer in main and pass it to the class constructor.
    
    This is a preparatory commit. Next commits will use provided UCC server
    to exchange UCC commands and replies with a client.
    
    Signed-off-by: Mario Maz <mmazzapater@gmail.com>

--- controller/src/beerocks/master/son_master_thread.cpp
+++ controller/src/beerocks/master/son_master_thread.cpp
@@ -79,11 +79,12 @@ using namespace net;
 using namespace son;
 
 master_thread::master_thread(const std::string &master_uds_, db &database_,
+                             std::unique_ptr<beerocks::UccServer> ucc_server,
                              std::unique_ptr<beerocks::CmduServer> cmdu_server,
                              std::shared_ptr<beerocks::EventLoop> event_loop)
     : transport_socket_thread(master_uds_), database(database_),
-      m_controller_ucc_listener(database_, cert_cmdu_tx), m_cmdu_server(std::move(cmdu_server)),
-      m_event_loop(event_loop)
+      m_controller_ucc_listener(database_, cert_cmdu_tx, std::move(ucc_server)),
+      m_cmdu_server(std::move(cmdu_server)), m_event_loop(event_loop)
 {
     LOG_IF(!m_cmdu_server, FATAL) << "CMDU server is a null pointer!";
     LOG_IF(!m_event_loop, FATAL) << "Event loop is a null pointer!";
