f3dde6ba0d13c724177c0435066a4beba56ca153
Mario Maz <mmazzapater@gmail.com>
2020-10-06 12:25:37 +0200

    btl: create broker client factory interface and implementation
    https://jira.prplfoundation.org/browse/PPM-658
    
    This is a preparatory commit.
    
    Create the broker client factory component to create instances of
    broker client class whenever requested.
    
    An implementation of the factory interface will be later injected into
    the controller.
    
    The reason for injecting a factory instead of an instance of broker
    client is to let the controller create such instances when it needs
    them. The first instance will be created when the controller is started
    and the rest whenever the connection with the broker server goes down
    (as part of a connection recovery mechanism).
    
    Signed-off-by: Mario Maz <mmazzapater@gmail.com>

---
+++ common/beerocks/btl/include/btl/broker_client_factory.h
@@ -0,0 +1,49 @@
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * SPDX-FileCopyrightText: 2020 the prplMesh contributors (see AUTHORS.md)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#ifndef BTL_BROKER_CLIENT_FACTORY_H_
+#define BTL_BROKER_CLIENT_FACTORY_H_
+
+#include <btl/broker_client.h>
+
+#include <memory>
+
+namespace beerocks {
+namespace btl {
+
+/**
+ * Broker client factory interface.
+ *
+ * Classes that need to dynamically create broker client objects must use a factory to do so,
+ * instead of creating the clients directly (to avoid dependencies with real implementations). Such
+ * factory will be provided as a dependency so it can be mocked while unit testing. A broker client
+ * factory mock will return broker client mocks whenever an expectation of a call to
+ * `create_instance()` is satisfied.
+ */
+class BrokerClientFactory {
+public:
+    /**
+     * Default destructor.
+     */
+    virtual ~BrokerClientFactory() = default;
+
+    /**
+     * @brief Creates an instance of a broker client.
+     *
+     * The broker client created is already connected to the broker server.
+     *
+     * @return Broker client instance.
+     */
+    virtual std::unique_ptr<BrokerClient> create_instance() = 0;
+};
+
+} // namespace btl
+
+} // namespace beerocks
+
+#endif // BTL_BROKER_CLIENT_FACTORY_H_
---
+++ common/beerocks/btl/include/btl/broker_client_factory_impl.h
@@ -0,0 +1,80 @@
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * SPDX-FileCopyrightText: 2020 the prplMesh contributors (see AUTHORS.md)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#ifndef BTL_BROKER_CLIENT_FACTORY_IMPL_H_
+#define BTL_BROKER_CLIENT_FACTORY_IMPL_H_
+
+#include <btl/broker_client_factory.h>
+
+#include <btl/broker_client_impl.h>
+
+#include <btl/message_parser.h>
+#include <btl/message_serializer.h>
+
+#include <bcl/beerocks_event_loop.h>
+
+namespace beerocks {
+namespace btl {
+
+/**
+ * Implementation of the broker client factory interface.
+ * This implementation creates instances of BrokerClientImpl class.
+ */
+class BrokerClientFactoryImpl : public BrokerClientFactory {
+public:
+    /**
+     * @brief Class constructor.
+     *
+     * @param uds_path UDS path of the broker server that created client instances are connected to.
+     * @param message_parser Message parser used to get transport messages out of a byte array
+     * received through a socket connection.
+     * @param message_serializer Message serializer used to put transport messages into a byte
+     * array ready to be sent through a socket connection.
+     * @param event_loop Application event loop used by the process to wait for I/O events.
+     */
+    BrokerClientFactoryImpl(const std::string &uds_path,
+                            std::shared_ptr<MessageParser> message_parser,
+                            std::shared_ptr<MessageSerializer> message_serializer,
+                            std::shared_ptr<beerocks::EventLoop> event_loop);
+
+    /**
+     * @brief Creates an instance of a broker client.
+     *
+     * @see BrokerClientFactory::create_instance
+     */
+    std::unique_ptr<BrokerClient> create_instance() override;
+
+private:
+    /**
+     * UDS path of the broker server that created client instances are connected to.
+     */
+    std::string m_uds_path;
+
+    /**
+     * Message parser used to get transport messages out of a byte array received through a socket
+     * connection.
+     */
+    std::shared_ptr<MessageParser> m_message_parser;
+
+    /**
+     * Message serializer used to put transport messages into a byte array ready to be sent through
+     * a socket connection.
+     */
+    std::shared_ptr<MessageSerializer> m_message_serializer;
+
+    /**
+     * Application event loop used by the process to wait for I/O events.
+     */
+    std::shared_ptr<EventLoop> m_event_loop;
+};
+
+} // namespace btl
+
+} // namespace beerocks
+
+#endif // BTL_BROKER_CLIENT_FACTORY_IMPL_H_
