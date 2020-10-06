c96df6976257be3527396fd0c2a3899addf90df8
Raphaël Mélotte <raphael.melotte@mind.be>
2020-10-06 11:07:10 +0200

    tests: turris-omnia: set multi_ap parameter for APs
    The multi_ap setting was currently not set, so it had the default
    value of 0 (no multi_ap support).
    
    Set it to 3 for all APs (fronthaul and backhaul BSS).
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- tests/certification/turris-omnia/uci.sh
+++ tests/certification/turris-omnia/uci.sh
@@ -17,6 +17,7 @@ set wireless.default_radio0.key='prplmesh_pass'
 set wireless.default_radio0.encryption='psk2'
 set wireless.default_radio0.ssid='prplmesh'
 set wireless.default_radio0.wps_pushbutton='1'
+set wireless.default_radio10.multi_ap=3
 
 set wireless.default_radio2=wifi-iface
 set wireless.default_radio2.device='radio2'
@@ -26,6 +27,7 @@ set wireless.default_radio2.key='prplmesh_pass'
 set wireless.default_radio2.encryption='psk2'
 set wireless.default_radio2.ssid='prplmesh'
 set wireless.default_radio2.wps_pushbutton='1'
+set wireless.default_radio10.multi_ap=3
 
 set wireless.default_radio10=wifi-iface
 set wireless.default_radio10.device='radio0'
@@ -34,6 +36,7 @@ set wireless.default_radio10.mode='ap'
 set wireless.default_radio10.key='prplmesh_pass'
 set wireless.default_radio10.encryption='psk2'
 set wireless.default_radio10.ssid='prplmesh'
+set wireless.default_radio10.multi_ap=3
 
 set wireless.default_radio20=wifi-iface
 set wireless.default_radio20.device='radio2'
@@ -42,6 +45,7 @@ set wireless.default_radio20.mode='ap'
 set wireless.default_radio20.key='prplmesh_pass'
 set wireless.default_radio20.encryption='psk2'
 set wireless.default_radio20.ssid='prplmesh'
+set wireless.default_radio10.multi_ap=3
 
 set wireless.default_radio11=wifi-iface
 set wireless.default_radio11.device='radio0'
