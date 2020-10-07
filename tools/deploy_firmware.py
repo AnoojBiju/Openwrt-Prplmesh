367007ec7c0c2403efa66c69cecb8acde005abc3
Raphaël Mélotte <raphael.melotte@mind.be>
2020-10-07 09:47:31 +0200

    tools: deploy_firmware: set the image name (rax40)
    By default the devices do not have the image name we use set in their
    u-boot environment. To avoid manual intervention and because we
    already know the image name from the deploy script, we can set it from
    there. Note that the u-boot environment is not saved after setting the
    image name, so users can keep their old configuration.
    
    This change will also help when we change the actual image name in
    prplwrt (which hopefully shouldn't happen too often).
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- tools/deploy_firmware.py
+++ tools/deploy_firmware.py
@@ -186,6 +186,9 @@ class NetgearRax40(PrplwrtDevice):
             # stop autoboot:
             shell.sendline("")
             shell.expect(self.uboot_prompt)
+            # set the image name:
+            shell.sendline("setenv fullimage {}".format(self.image))
+            shell.expect(self.uboot_prompt)
             # do the actual upgrade:
             shell.sendline("run update_fullimage")
             shell.expect(self.uboot_prompt, timeout=600)
