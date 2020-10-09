323cc0f87ca34579472e5f87a1225262286c36c5
Krystyna Oliinyk <k.oliinyk@inango-systems.com>
2020-10-09 15:44:22 +0300

    nbapi:ambiorix: modify add_instance() method
    Need keep track of the data element object
    
    Modify an add_instance() method. Now it return an index
    of recently added object on success and 0 otherwise.
    
    PPM-459
    --
    Signed-off-by: Krystyna Oliinyk <k.oliinyk@inango-systems.com>

--- framework/platform/nbapi/ambiorix_impl.cpp
+++ framework/platform/nbapi/ambiorix_impl.cpp
@@ -414,13 +414,15 @@ bool AmbiorixImpl::set(const std::string &relative_path, const bool &value)
     return true;
 }
 
-bool AmbiorixImpl::add_instance(const std::string &relative_path)
+uint32_t AmbiorixImpl::add_instance(const std::string &relative_path)
 {
     amxd_trans_t transaction;
+    uint32_t index;
+
     auto object = prepare_transaction(relative_path, transaction);
     if (!object) {
         LOG(ERROR) << "Couldn't find the object for: " << relative_path;
-        return false;
+        return 0;
     }
 
     auto status = amxd_trans_add_inst(&transaction, 0, NULL);
@@ -430,11 +432,16 @@ bool AmbiorixImpl::add_instance(const std::string &relative_path)
 
     if (!apply_transaction(transaction)) {
         LOG(ERROR) << "Failed to apply transaction for: " << object->name;
-        return false;
+        return 0;
+    }
+
+    index = amxd_object_get_index(object);
+    if (!index) {
+        LOG(ERROR) << "Failed to get index for object: " << object->name;
     }
 
     LOG(DEBUG) << "Instance added for: " << object->name;
-    return true;
+    return index;
 }
 
 bool AmbiorixImpl::remove_instance(const std::string &relative_path, uint32_t index)
