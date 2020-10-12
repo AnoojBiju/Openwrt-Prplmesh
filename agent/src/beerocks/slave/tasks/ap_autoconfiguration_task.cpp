ccb154b968968b03805c03aebefb0b781cdb2560
Moran Shoeg <moranx.shoeg@intel.com>
2020-10-12 14:50:51 +0300

    agent: Add a socket argument to agent task handle_cmdu
    Add a Socket argument to the task infrastructure on the agent, in "handle_cmdu" functions.
    Update all required handlers on each of the Agent tasks.
    
    This change is required since the channel selection task on the agent needs it
    for understanding which radio has sent the message since the system flow is different on
    each radio interface that sends a message.
    
    Signed-off-by: Moran Shoeg <moranx.shoeg@intel.com>

--- agent/src/beerocks/slave/tasks/ap_autoconfiguration_task.cpp
+++ agent/src/beerocks/slave/tasks/ap_autoconfiguration_task.cpp
@@ -191,7 +191,7 @@ void ApAutoConfigurationTask::handle_event(uint8_t event_enum_value, const void 
 }
 
 bool ApAutoConfigurationTask::handle_cmdu(ieee1905_1::CmduMessageRx &cmdu_rx,
-                                          const sMacAddr &src_mac,
+                                          const sMacAddr &src_mac, Socket *sd,
                                           std::shared_ptr<beerocks_header> beerocks_header)
 {
     switch (cmdu_rx.getMessageType()) {
