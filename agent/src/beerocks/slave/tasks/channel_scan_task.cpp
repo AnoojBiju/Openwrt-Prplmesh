69b2bd657d886b6fc94e411692064ddfde0f57f3
Oren Vormaser <oren.vormaser@devalore.com>
2020-10-14 14:47:33 +0300

    agent: channel scan task skeleton and preparations
    Add a skeleton for the channel scan task,
    which is planned be moved from the controller to the agent.
    
    The skeleton include task state machine and messages and events handler.
    
    - Add channel_scan_task skeleton.
    - Add the task to the task pool in the backhaul manager.
    
    PPM-363
    
    Signed-off-by: Oren Vormaser <oren.vormaser@devalore.com>

---
+++ agent/src/beerocks/slave/tasks/channel_scan_task.cpp
@@ -0,0 +1,81 @@
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * SPDX-FileCopyrightText: 2016-2020 the prplMesh contributors (see AUTHORS.md)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#include "channel_scan_task.h"
+#include "../agent_db.h"
+#include <easylogging++.h>
+
+using namespace beerocks;
+
+#define FSM_MOVE_STATE(radio_iface, new_state)                                                     \
+    ({                                                                                             \
+        LOG(TRACE) << "CHANNEL_SCAN " << radio_iface                                               \
+                   << " FSM: " << fsm_state_to_string(m_state[radio_iface].state) << " --> "       \
+                   << fsm_state_to_string(new_state);                                              \
+        m_state[radio_iface].state = new_state;                                                    \
+    })
+
+const std::string ChannelScanTask::fsm_state_to_string(eState status)
+{
+    switch (status) {
+    case eState::INIT:
+        return "INIT";
+    case eState::IDLE:
+        return "IDLE";
+    default:
+        LOG(ERROR) << "state argument doesn't have an enum";
+        break;
+    }
+    return std::string();
+}
+
+ChannelScanTask::ChannelScanTask(backhaul_manager &btl_ctx, ieee1905_1::CmduMessageTx &cmdu_tx)
+    : Task(eTaskType::CHANNEL_SCAN), m_btl_ctx(btl_ctx), m_cmdu_tx(cmdu_tx)
+{
+}
+
+void ChannelScanTask::work()
+{
+    for (auto &state_kv : m_state) {
+        const auto &radio_iface = state_kv.first;
+        auto &state_status      = state_kv.second;
+        switch (state_status.state) {
+        case eState::INIT: {
+            FSM_MOVE_STATE(radio_iface, eState::IDLE);
+            break;
+        }
+        case eState::IDLE: {
+            break;
+        }
+        default:
+            break;
+        }
+    }
+}
+
+void ChannelScanTask::handle_event(uint8_t event_enum_value, const void *event_obj)
+{
+    switch (eEvent(event_enum_value)) {
+    default: {
+        LOG(DEBUG) << "Message handler doesn't exists for event type " << event_enum_value;
+        break;
+    }
+    }
+}
+
+bool ChannelScanTask::handle_cmdu(ieee1905_1::CmduMessageRx &cmdu_rx, const sMacAddr &src_mac,
+                                  std::shared_ptr<beerocks_header> beerocks_header)
+{
+    switch (cmdu_rx.getMessageType()) {
+    default: {
+        // Message was not handled, therefore return false.
+        return false;
+    }
+    }
+    return true;
+}
