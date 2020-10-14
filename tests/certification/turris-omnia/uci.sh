a588f358c2eddd16d307de249990a7adea86c1fb
Raphaël Mélotte <raphael.melotte@mind.be>
2020-10-14 19:00:09 +0200

    ci: set different default credentials for omnia radio2
    MAP-4.2.3_ETH_FH24G:turris-omnia
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- tests/certification/turris-omnia/uci.sh
+++ tests/certification/turris-omnia/uci.sh
@@ -23,9 +23,11 @@ set wireless.default_radio2=wifi-iface
 set wireless.default_radio2.device='radio2'
 set wireless.default_radio2.network='lan'
 set wireless.default_radio2.mode='ap'
-set wireless.default_radio2.key='prplmesh_pass'
+set wireless.default_radio2.key='maprocks2'
 set wireless.default_radio2.encryption='psk2'
-set wireless.default_radio2.ssid='prplmesh'
+set wireless.default_radio2.ssid='Multi-AP-24G-2'
+set wireless.default_radio2.multi_ap_backhaul_ssid='A_TEST_SSID'
+set wireless.default_radio2.multi_ap_backhaul_key='maprocks2'
 set wireless.default_radio2.wps_pushbutton='1'
 set wireless.default_radio2.multi_ap=3
 
