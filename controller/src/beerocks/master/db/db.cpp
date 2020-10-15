338a2fcd66852345b5baa7435d1e6cab3eeb4512
Vladyslav Tupikin <v.tupikin@inango-systems.com>
2020-10-15 16:30:21 +0300

    controller: db: Implement method for adding operating class
    Need to add new data base method for adding operating
    class to the Controller Data Model.
    
    Signed-off-by: Vladyslav Tupikin <v.tupikin@inango-systems.com>

--- controller/src/beerocks/master/db/db.cpp
+++ controller/src/beerocks/master/db/db.cpp
@@ -4913,3 +4913,60 @@ std::string db::dm_get_path_to_radio(const son::node &radio_node)
 
     return radio_path;
 }
+
+bool db::add_current_op_class(const sMacAddr &radio_mac, uint8_t op_class, uint8_t op_channel,
+                              int8_t tx_power)
+{
+    auto radio_path = dm_get_path_to_radio(*(get_node(radio_mac)));
+    if (radio_path.empty()) {
+        LOG(ERROR) << "Failed to get Radio path with mac: " << radio_mac
+                   << " in Controller Data model.";
+        return false;
+    }
+
+    // Prepare path to the CurrentOperatingClasses instance
+    // Data model path example: Controller.Network.Device.1.Radio.1.CurrentOperatingClasses
+    auto op_class_path = radio_path + "CurrentOperatingClasses";
+
+    auto op_class_index = m_ambiorix_datamodel->add_instance(op_class_path);
+    if (!op_class_index) {
+        LOG(ERROR) << "Failed to add instance: " << radio_path << "CurrentOperatingClasses";
+        return false;
+    }
+
+    auto time_stamp = m_ambiorix_datamodel->get_datamodel_time_format();
+    if (time_stamp.empty()) {
+        LOG(ERROR) << "Failed to get Date and Time in RFC 3339 format.";
+        return false;
+    }
+
+    //Set TimeStamp
+    //Data model path example: Controller.Network.Device.1.Radio.1.CurrentOperatingClasses.TimeStamp
+    if (!m_ambiorix_datamodel->set(op_class_path, "TimeStamp", time_stamp)) {
+        LOG(ERROR) << "Failed to set " << radio_path << "TimeStamp";
+        return false;
+    }
+
+    //Set Operating class
+    //Data model path: Controller.Network.Device.1.Radio.1.CurrentOperatingClasses.Class
+    if (!m_ambiorix_datamodel->set(op_class_path, "Class", op_class)) {
+        LOG(ERROR) << "Failed to set" << op_class_path << "Class";
+        return false;
+    }
+
+    //Set Operating channel
+    //Data model path example: Controller.Network.Device.1.Radio.1.CurrentOperatingClasses.Channel
+    if (!m_ambiorix_datamodel->set(op_class_path, "Channel", op_channel)) {
+        LOG(ERROR) << "Failed to set" << op_class_path << "Channel";
+        return false;
+    }
+
+    //Set TX power
+    //Data model path example: Controller.Network.Device.1.Radio.1.CurrentOperatingClasses.TxPower
+    if (!m_ambiorix_datamodel->set(op_class_path, "TxPower", tx_power)) {
+        LOG(ERROR) << "Failed to set" << op_class_path << "TxPower";
+        return false;
+    }
+
+    return true;
+}
