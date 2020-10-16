538da78961724f6dd5c5db42adca18913255ca8b
Raphaël Mélotte <raphael.melotte@mind.be>
2020-10-16 12:36:30 +0200

    uci: set multi_ap back to 3
    Now that we know only the credentials initially set are used, set
    multi_ap back to 3 to see if we can use it.
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- tests/certification/turris-omnia/uci.sh
+++ tests/certification/turris-omnia/uci.sh
@@ -28,7 +28,7 @@ set wireless.default_radio2.encryption='psk2'
 set wireless.default_radio2.ssid='TEST_SSID'
 set wireless.default_radio2.multi_ap_backhaul_ssid='Multi-AP-24G-2'
 set wireless.default_radio2.wps_pushbutton='1'
-set wireless.default_radio2.multi_ap=2
+set wireless.default_radio2.multi_ap=3
 
 set wireless.default_radio10=wifi-iface
 set wireless.default_radio10.device='radio0'
@@ -46,7 +46,7 @@ set wireless.default_radio20.mode='ap'
 set wireless.default_radio20.key='prplmesh_pass'
 set wireless.default_radio20.encryption='psk2'
 set wireless.default_radio20.ssid='prplmesh'
-set wireless.default_radio20.multi_ap=1
+set wireless.default_radio20.multi_ap=3
 
 set wireless.default_radio11=wifi-iface
 set wireless.default_radio11.device='radio0'
