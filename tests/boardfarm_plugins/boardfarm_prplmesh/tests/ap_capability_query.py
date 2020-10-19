21d445377c1ea76faa158332f281c20f6796e3d7
David <davidmgafonso@gmail.com>
2020-10-19 12:00:13 +0100

    tests: boardfarm: Ported ap_capability_query to boardfarm
    Signed-off-by: David <davidmgafonso@gmail.com>

---
+++ tests/boardfarm_plugins/boardfarm_prplmesh/tests/ap_capability_query.py
@@ -0,0 +1,47 @@
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+# SPDX-FileCopyrightText: 2020 the prplMesh contributors (see AUTHORS.md)
+# This code is subject to the terms of the BSD+Patent license.
+# See LICENSE file for more details.
+
+from .prplmesh_base_test import PrplMeshBaseTest
+from boardfarm.exceptions import SkipTest
+from opts import debug
+import time
+
+
+class ApCapabilityQuery(PrplMeshBaseTest):
+
+    def runTest(self):
+        # Locate test participants
+        try:
+            agent = self.dev.DUT.agent_entity
+            controller = self.dev.lan.controller_entity
+        except AttributeError as ae:
+            raise SkipTest(ae)
+
+        self.dev.DUT.wired_sniffer.start(self.__class__.__name__ + "-" + self.dev.DUT.name)
+
+        controller.dev_send_1905(agent.mac, 0x8001)
+        time.sleep(1)
+
+        debug("Confirming ap capability query has been received on agent")
+        self.check_log(agent, "AP_CAPABILITY_QUERY_MESSAGE")
+
+        debug("Confirming ap capability report has been received on controller")
+        self.check_log(controller, "AP_CAPABILITY_REPORT_MESSAGE")
+
+    @classmethod
+    def teardown_class(cls):
+        """Teardown method, optional for boardfarm tests."""
+        test = cls.test_obj
+        print("Sniffer - stop")
+        test.dev.DUT.wired_sniffer.stop()
+        # Send additional Ctrl+C to the device to terminate "tail -f"
+        # Which is used to read log from device. Required only for tests on HW
+        try:
+            test.dev.DUT.agent_entity.device.send('\003')
+        except AttributeError:
+            # If AttributeError was raised - we are dealing with dummy devices.
+            # We don't have to additionaly send Ctrl+C for dummy devices.
+            pass
+        test.dev.wifi.disable_wifi()
