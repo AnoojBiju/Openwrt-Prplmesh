f814dbae34cf364d9ad7813027f9a1ddb6ff93b1
Daniel Edri <daniel.edri@devalore.com>
2020-10-21 13:48:11 +0300

    aging: clients can now age against their own custom timelife
    the persistent_aging_operation evaluates if a client has
    reached the end of its life expectancy. Initialy the time
    constraints that gets selected are the friendly/unfriendly
    unless is already defined like described
    in the previous commits.
    
    alter
    persistent_aging_operations::perioidic_operation_function
    to be aware of time_life_duration_minutes when
    evaluating whethera client is expired and
    should be removed.
    
    Signed-off-by: Daniel Edri <daniel.edri@devalore.com>

--- controller/src/beerocks/master/periodic/persistent_database_aging.cpp
+++ controller/src/beerocks/master/periodic/persistent_database_aging.cpp
@@ -27,27 +27,34 @@ void persistent_database_aging_operation::periodic_operation_function()
     auto clients     = m_database.get_clients_with_persistent_data_configured();
     std::vector<sMacAddr> aged_clients;
     std::copy_if(
-        clients.begin(), clients.end(), aged_clients.begin(), [&](const sMacAddr &client_mac) {
+        clients.begin(), clients.end(), std::back_inserter(aged_clients),
+        [&](const sMacAddr &client_mac) {
             const auto max_timelife_delay_sec =
                 std::chrono::seconds(m_database.config.max_timelife_delay_minutes * 60);
             const auto unfriendly_device_max_timelife_delay_sec = std::chrono::seconds(
                 m_database.config.unfriendly_device_max_timelife_delay_minutes * 60);
+            auto temp_timelife =
+                std::chrono::seconds(m_database.get_client_time_life_delay(client_mac));
+            auto timelife_delay_seconds =
+                (m_database.get_client_is_unfriendly(client_mac) == eTriStateBool::TRUE)
+                    ? unfriendly_device_max_timelife_delay_sec
+                    : max_timelife_delay_sec;
+
+            // Calculate client expiry due time
+            auto parameters_last_edit = m_database.get_client_parameters_last_edit(client_mac);
+            auto expiry_due           = parameters_last_edit + timelife_delay_seconds;
+
             // Client timelife delay
             // If a client has a predetermined timelife delay use that.
             // Otherwise use the Max timelife delay/unfriendly_device_max_timelife_delay_sec according
             // to the client's unfriendliness status.
-            auto timelife_delay = std::chrono::duration_cast<std::chrono::seconds>(
-                m_database.get_client_time_life_delay(client_mac));
-            if (timelife_delay == std::chrono::seconds::zero()) {
-                timelife_delay =
-                    (m_database.get_client_is_unfriendly(client_mac) == eTriStateBool::TRUE)
-                        ? unfriendly_device_max_timelife_delay_sec
-                        : max_timelife_delay_sec;
+            if ((temp_timelife == std::chrono::seconds::zero())) {
+                return false;
+            } else if (temp_timelife > std::chrono::seconds::zero()) {
+
+                expiry_due = parameters_last_edit + temp_timelife;
             }
 
-            // Calculate client expiry due time
-            auto parameters_last_edit = m_database.get_client_parameters_last_edit(client_mac);
-            auto expiry_due           = parameters_last_edit + timelife_delay;
             // If the expiry due is less then the last aging check, the client is considered aged.
             return expiry_due < last_aging_check;
         });
