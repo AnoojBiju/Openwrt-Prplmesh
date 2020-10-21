f906a4a439065bccc690af3652e26c7616ab104c
Daniel Edri <daniel.edri@devalore.com>
2020-10-21 13:35:58 +0300

    DB: applies changes related to life_time_duration_minutes
    The DB is responsile for storing the client_time_life_delay
    and obtaining it upon demand.
    
    Alter the function set_client_time_life_delay, add prints
    to indicate if a client is not supposed to continue aging.
    
    Signed-off-by: Daniel Edri <daniel.edri@devalore.com>

--- controller/src/beerocks/master/db/db.cpp
+++ controller/src/beerocks/master/db/db.cpp
@@ -2667,7 +2667,13 @@ bool db::set_client_time_life_delay(const sMacAddr &mac,
         return false;
     }
 
-    LOG(DEBUG) << "time_life_delay_minutes = " << time_life_delay_minutes.count();
+    // if the time_life_delay_minutes is configured it wont't age, otherwise
+    // it'll behave as is.
+    if (time_life_delay_minutes == std::chrono::minutes::zero()) {
+        LOG(DEBUG) << "time_life_delay_minutes is set to prevent the client from aging";
+    } else {
+        LOG(DEBUG) << "time_life_delay_minutes = " << time_life_delay_minutes.count();
+    }
 
     auto timestamp = std::chrono::system_clock::now();
     if (save_to_persistent_db) {
@@ -2950,7 +2956,7 @@ bool db::clear_client_persistent_db(const sMacAddr &mac)
     LOG(DEBUG) << "setting client " << mac << " runtime info to default values";
 
     node->client_parameters_last_edit    = std::chrono::system_clock::time_point::min();
-    node->client_time_life_delay_minutes = std::chrono::minutes::zero();
+    node->client_time_life_delay_minutes = std::chrono::minutes(-1);
     node->client_stay_on_initial_radio   = eTriStateBool::NOT_CONFIGURED;
     node->client_initial_radio           = network_utils::ZERO_MAC;
     node->client_selected_bands          = PARAMETER_NOT_CONFIGURED;
@@ -3022,12 +3028,9 @@ bool db::update_client_persistent_db(const sMacAddr &mac)
     //fill values map of client persistent params
     values_map[TIMESTAMP_STR] = timestamp_to_string_seconds(node->client_parameters_last_edit);
 
-    if (node->client_time_life_delay_minutes != std::chrono::minutes::zero()) {
-        LOG(DEBUG) << "Setting client time-life-delay in persistent-db to "
-                   << node->client_time_life_delay_minutes.count() << " for " << mac;
-        values_map[TIMELIFE_DELAY_STR] =
-            std::to_string(node->client_time_life_delay_minutes.count());
-    }
+    LOG(DEBUG) << "Setting client time-life-delay in persistent-db to "
+               << node->client_time_life_delay_minutes.count() << " for " << mac;
+    values_map[TIMELIFE_DELAY_STR] = std::to_string(node->client_time_life_delay_minutes.count());
 
     if (node->client_stay_on_initial_radio != eTriStateBool::NOT_CONFIGURED) {
         auto enable = (node->client_stay_on_initial_radio == eTriStateBool::TRUE);
@@ -4721,7 +4724,7 @@ sMacAddr db::get_candidate_client_for_removal(sMacAddr client_to_skip)
 
             // Client timelife delay
             auto timelife_delay =
-                (client->client_time_life_delay_minutes != std::chrono::seconds::zero())
+                (client->client_time_life_delay_minutes != std::chrono::seconds(-1))
                     ? std::chrono::seconds(client->client_time_life_delay_minutes)
                     : max_timelife_delay;
             // Calculate client expiry due time
