9332b7f5a93c348646fb6917922df065bf3c96ad
Oren Vormaser <oren.vormaser@devalore.com>
2020-10-28 14:08:49 +0200

    controller: add dynamic channel selection r2 task to the controller.
    Preparative Commit.
    Add a new dynamic channel selection task (r2) separately to the
    existing DCS task in order not to affect the existing feature
    until the full transition to DCS R2 is complete.
    
    The skeleton includes a state-machine and events handler.
    
    - add dynamic_channel_selection_r2_task skeleton to the controller.
    - register task id in db.
    - Add the task to the task pool in the controller thread
      (formerly son_master_thread).
    
    PPM-360
    ---
    Signed-off-by: Oren Vormaser <oren.vormaser@devalore.com>

---
+++ controller/src/beerocks/master/tasks/dynamic_channel_selection_r2_task.cpp
@@ -0,0 +1,50 @@
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * SPDX-FileCopyrightText: 2019-2020 the prplMesh contributors (see AUTHORS.md)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#include "dynamic_channel_selection_r2_task.h"
+#include "../son_actions.h"
+#include <easylogging++.h>
+
+#define FSM_MOVE_STATE(new_state)                                                                  \
+    ({                                                                                             \
+        LOG(TRACE) << "DYNAMIC_CHANNEL_SELECTION_R2 "                                              \
+                   << " FSM: " << m_states_string.at(m_state) << " --> "                           \
+                   << m_states_string.at(new_state);                                               \
+        m_state = new_state;                                                                       \
+    })
+
+dynamic_channel_selection_r2_task::dynamic_channel_selection_r2_task(
+    db &database_, ieee1905_1::CmduMessageTx &cmdu_tx_, task_pool &tasks_)
+    : task("DCS R2 task"), database(database_), cmdu_tx(cmdu_tx_), tasks(tasks_)
+{
+    LOG(TRACE) << "Start dynamic_channel_selection_r2_task(id=" << id << ")";
+    database.assign_dynamic_channel_selection_r2_task_id(id);
+    m_state = eState::IDLE;
+}
+
+void dynamic_channel_selection_r2_task::work()
+{
+    switch (m_state) {
+    case eState::IDLE: {
+        break;
+    }
+
+    default:
+        break;
+    }
+}
+
+void dynamic_channel_selection_r2_task::handle_event(int event_enum_value, void *event_obj)
+{
+    switch (eEvent(event_enum_value)) {
+    default: {
+        LOG(DEBUG) << "Message handler doesn't exists for event type " << event_enum_value;
+        break;
+    }
+    }
+}
