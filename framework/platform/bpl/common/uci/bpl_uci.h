4dc460ce58460f63d3abecd8475b85d2173a7bee
Mario Maz <mmazzapater@gmail.com>
2020-11-20 20:26:44 +0100

    bpl: cfg: create method to find UCI section by value
    https://jira.prplfoundation.org/browse/PPM-832
    
    Create a new method to find a section in UCI configuration containing a
    given option name=value.
    
    This is a preparatory commit. In a follow-up commit this method will be
    used to find the section containing the wireless configuration for a
    given network interface.
    
    Signed-off-by: Mario Maz <mmazzapater@gmail.com>

--- framework/platform/bpl/common/uci/bpl_uci.h
+++ framework/platform/bpl/common/uci/bpl_uci.h
@@ -41,6 +41,23 @@ bool uci_section_exists(const std::string &package_name, const std::string &sect
                         const std::string &section_name);
 
 /**
+ * @brief Finds a section with given type and containing option with name=value.
+ *
+ * The returned @a secion_name can be used as the @a secion_name in e.g. uci_set_section and
+ * uci_get_section.
+ *
+ * @param[in] package_name name of the requested configuration file.
+ * @param[in] section_type type of the requested section.
+ * @param[in] option_name name of the requested option.
+ * @param[in] option_value value of the requested option.
+ * @param[out] section_name name of the section found or empty if it does not exist.
+ * @return true on success, false otherwise.
+ */
+bool uci_find_section_by_option(const std::string &package_name, const std::string &section_type,
+                                const std::string &option_name, const std::string &option_value,
+                                std::string &section_name);
+
+/**
  * @brief Add new named section.
  * 
  * @param[in] package_name name of the requested configuration file.
