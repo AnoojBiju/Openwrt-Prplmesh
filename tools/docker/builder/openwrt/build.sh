c38161662f73643c869fe1deef99b7d60d2fe870
Raphaël Mélotte <raphael.melotte@mind.be>
2020-11-27 12:40:31 +0100

    openwrt: reconfigure wps on reload
    Update prplWrt to include a new hostapd patch to reconfigure the wps
    settings on reload.
    
    This will first be merged in the prplMesh target
    branch ('prplwrt-1.0-rc1-prplmesh') because it's definitely not worse
    than the previous patch that introduced the same functionality, and
    because we need it to be able to pass any test where we use WPS on one
    of our backhaul APs.
    
    At the same time, the patch will be submitted for a review between the
    prplWrt/prplMesh developers, and if no issue is raised it will be sent
    to upstream hostapd.
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -185,7 +185,7 @@ main() {
 VERBOSE=false
 IMAGE_ONLY=false
 OPENWRT_REPOSITORY='https://gitlab.com/prpl-foundation/prplwrt/prplwrt.git'
-OPENWRT_VERSION='f3359a00f876becbc337e9291891de672d7cbf7b'
+OPENWRT_VERSION='1147055bc76597175037e6428a2dc1e98dd86150'
 PRPL_FEED='https://gitlab.com/prpl-foundation/prplwrt/feed-prpl.git^9dc5664f501e31335cf78c2cfa6d9c140dae353f'
 SAH_FEED='https://gitlab.com/soft.at.home/buildsystems/openwrt/sah-packages.git^cc3991bef565448e2c107c79d3efc3c759a0ca0a'
 PRPLMESH_VARIANT="-nl80211"
