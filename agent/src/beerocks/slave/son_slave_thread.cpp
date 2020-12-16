1ad97bca0aae1a01ef3fd1ed5baaefa5b817769c
Moran Shoeg <moranx.shoeg@intel.com>
2020-12-16 18:28:47 +0200

    agent: Fill correctly combined_front_back field
    Fill correctly from the AgentDB, the "combined_front_back" field on the
    ap_radio_advanced_capabilities_tlv which is added to the AP
    Autoconfiguration message with M1 which is sent to the controller.
    
    Signed-off-by: Moran Shoeg <moranx.shoeg@intel.com>

--- agent/src/beerocks/slave/son_slave_thread.cpp
+++ agent/src/beerocks/slave/son_slave_thread.cpp
@@ -3639,7 +3639,8 @@ bool slave_thread::slave_fsm(bool &call_slave_select)
         ap_radio_advanced_capabilities_tlv->radio_uid() = radio->front.iface_mac;
 
         // Currently Set the flag as we don't support traffic separation.
-        ap_radio_advanced_capabilities_tlv->traffic_separation_flag().combined_front_back = 0;
+        ap_radio_advanced_capabilities_tlv->traffic_separation_flag().combined_front_back =
+            radio->front.hybrid_mode_supported;
         ap_radio_advanced_capabilities_tlv->traffic_separation_flag()
             .combined_profile1_and_profile2 = 0;
 
