e12d66fe6d91335e13ef1338414d08102147c86a
David <davidmgafonso@gmail.com>
2021-02-17 15:28:29 +0000

    test: boardfarm: added cpu and ram stats retrieval Currently resources_test_run.py doesn't test anything besides the validation of a correct output type. These functions will be used on the Altran test cases.
    Signed-off-by: David <davidmgafonso@gmail.com>

---
+++ tests/boardfarm_plugins/boardfarm_prplmesh/tests/resources_test_run.py
@@ -0,0 +1,22 @@
+from .prplmesh_base_test import PrplMeshBaseTest
+from opts import debug
+
+
+class ResourcesTestRun(PrplMeshBaseTest):
+    """Check initial configuration on device."""
+
+    def runTest(self):
+
+        agent = self.dev.DUT.agent_entity
+
+        memory_stats = agent.get_memory_usage()
+
+        debug('memory_stats', memory_stats)
+
+        assert all([isinstance(v, int) for v in memory_stats])
+
+        cpu_stats = agent.get_cpu_usage()
+
+        debug('cpu_stats', cpu_stats)
+
+        assert all([isinstance(v, float) for v in cpu_stats])
