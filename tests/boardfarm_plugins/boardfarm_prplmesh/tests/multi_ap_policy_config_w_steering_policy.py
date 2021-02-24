58ab06a7321ae6519483bda51b2b4174d9238a71
Vitalii Komisarenko <vitalii.komisarenko@mind.be>
2021-02-24 23:17:20 +0100

    tests: boardfarm: remove teardown_class if they duplicate parent class method
    Each test can have `teardown_class` method to be called
    after running the test.
    The behaviour of these method was very similar, so it
    was put into the parent class.
    
    Remove `teardown_class` methods in the individual tests
    if their functionality is covered with parent class
    method.
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@mind.be>

--- tests/boardfarm_plugins/boardfarm_prplmesh/tests/multi_ap_policy_config_w_steering_policy.py
+++ tests/boardfarm_plugins/boardfarm_prplmesh/tests/multi_ap_policy_config_w_steering_policy.py
@@ -41,19 +41,3 @@ class MultiApPolicyConfigWSteeringPolicy(PrplMeshBaseTest):
         time.sleep(1)
         debug("Confirming multi-ap policy config ack message has been received on the controller")
         self.check_cmdu_type_single("ACK", 0x8000, agent.mac, controller.mac, mid)
-
-    @classmethod
-    def teardown_class(cls):
-        """Teardown method, optional for boardfarm tests."""
-        test = cls.test_obj
-        print("Sniffer - stop")
-        test.dev.DUT.wired_sniffer.stop()
-        # Send additional Ctrl+C to the device to terminate "tail -f"
-        # Which is used to read log from device. Required only for tests on HW
-        try:
-            test.dev.DUT.agent_entity.device.send('\003')
-        except AttributeError:
-            # If AttributeError was raised - we are dealing with dummy devices.
-            # We don't have to additionaly send Ctrl+C for dummy devices.
-            pass
-        test.dev.wifi.disable_wifi()
--- tests/boardfarm_plugins/boardfarm_prplmesh/tests/multi_ap_policy_config_w_unsucessful_association.py
+++ tests/boardfarm_plugins/boardfarm_prplmesh/tests/multi_ap_policy_config_w_unsucessful_association.py
@@ -30,19 +30,3 @@ class MultiApPolicyConfigWUnsucessfulAssociation(CommonFlows):
         self.configure_multi_ap_policy_config_with_unsuccessful_association(
             agent, controller, 0x80, 0x01)
         self.mismatch_psk(agent.radios[0], controller, sta)
-
-    @classmethod
-    def teardown_class(cls):
-        """Teardown method, optional for boardfarm tests."""
-        test = cls.test_obj
-        print("Sniffer - stop")
-        test.dev.DUT.wired_sniffer.stop()
-        # Send additional Ctrl+C to the device to terminate "tail -f"
-        # Which is used to read log from device. Required only for tests on HW
-        try:
-            test.dev.DUT.agent_entity.device.send('\003')
-        except AttributeError:
-            # If AttributeError was raised - we are dealing with dummy devices.
-            # We don't have to additionaly send Ctrl+C for dummy devices.
-            pass
-        test.dev.wifi.disable_wifi()
