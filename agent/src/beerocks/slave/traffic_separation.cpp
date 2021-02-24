c83b5355e51d2f7c09760759b8717c880c324ea7
Moran Shoeg <moranx.shoeg@intel.com>
2021-02-24 21:05:15 +0200

    agent: Add the new traffic separation files
    Preparative commit.
    Add the new traffic separation files, which contain new class and documentation.
    On the next commits, contents will be added to the new class.
    
    Signed-off-by: Moran Shoeg <moranx.shoeg@intel.com>

---
+++ agent/src/beerocks/slave/traffic_separation.cpp
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * SPDX-FileCopyrightText: 2016-2020 the prplMesh contributors (see AUTHORS.md)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+#include <bcl/beerocks_utils.h>
+#include <bcl/network/network_utils.h>
+
+#include "agent_db.h"
+#include "traffic_separation.h"
+
+namespace beerocks {
+namespace net {
+
+
+} // namespace net
+} // namespace beerocks
