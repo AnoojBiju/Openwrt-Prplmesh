34830f58b67836898f22c866bb7cec28aae7c254
Ran Regev <ran.regev@devalore.com>
2021-02-25 12:24:16 +0200

    boardfarm: 4_7_10 simulation
    Added phase 6 checks.
    This phase wasn't checked up till now as it was
    supposed to work "as before". However, certification
    tests showed that we are missing Radio Metrics TLV
    in AP Metrics Response.
    This commit adds triggering report policy and
    checks the metrics-response for having the added
    radio metrics tlv
    
    Signed-off-by: Ran Regev <ran.regev@devalore.com>

--- tests/boardfarm_plugins/boardfarm_prplmesh/tests/simulate_v2_certification_4_7_10.py
+++ tests/boardfarm_plugins/boardfarm_prplmesh/tests/simulate_v2_certification_4_7_10.py
@@ -16,7 +16,6 @@ class V2Certification_4_7_10(CommonFlows):
         STA2 - WIFI repeater
         STA3 - WIFI repeater
         AP1 - Agent1 [DUT]
-
         GW - Controller
     """
 
@@ -44,19 +43,16 @@ class V2Certification_4_7_10(CommonFlows):
         sleep(1)
         # Phase 2 (step 4)
         '''
-        Todo:
         Verify that MAUT sends a correctly formatted AP Capability Report message within 1 sec of
         receiving the AP Capability Query message sent by the Controller.
-        Verify that the AP Capability Report message contains one Metric Collection Interval TLV and
-        one R2 AP Capability TLV with the Byte Counter Units field set to 0x01.
+        Verify that the AP Capability Report message contains
+            one Metric Collection Interval TLV and
+            one R2 AP Capability TLV with the Byte Counter Units field set to 0x01.
         '''
         resp = self.check_cmdu_type_single("AP Capability Report message", 0x8002,
-                                           agent.mac, controller.mac,
-                                           mid)
-
+                                           agent.mac, controller.mac, mid)
+        self.check_cmdu_has_tlvs(resp, 0xB4)
         self.check_cmdu_has_tlvs(resp, 0xC5)
-        ap_capability_tlv = self.check_cmdu_has_tlvs(resp, 0xB4)
-        print(ap_capability_tlv)
 
         # Phase 3
         # Phase 4
@@ -68,7 +64,62 @@ class V2Certification_4_7_10(CommonFlows):
         # Phase 5
         # Phase 6
 
+        '''
+        Trigger (DEV_SEND_1905, DestALid, WTS_REPLACE_MAUT_ALID, MessageTypeValue,0x8003)
+        CTT Controller to send a Multi-AP Policy Config Request message to MAUT,
+        containing:
+        a) a Metric Reporting Policy TLV: (
+                AP Metric Reporting Interval = 10;
+                Number of Radios =1; (
+                    RUID=WTS_REPLACE_RUID
+                    STA Metrics Reporting RCPI Threshold = 0;
+                    AP Metrics Channel Utilization Reporting Threshold.= 0;
+                    STA Metrics Reporting RCPI Hysteresis Margin Override = 0;
+                    AP Metrics Channel Utilization Reporting Threshold = 0;
+                    Associated STA Traffic Stats Inclusion Policy = 1;
+                    Associated STA Link Metrics Inclusion Policy = 1
+                );
+                and
+        b) an Unsuccessful Association Policy TLV: (
+                Report Unsuccessful Associations = 1;
+                Maximum Reporting Rate = 60
+            )
+        '''
+
+        metric_reporting_policy_tlv = tlv(0x8A, 0x000D,
+                                          '0x0a 0x01 {} 0x00 0x00 0x00 0x00 0x0f'
+                                          .format(agent.radios[0].mac))
+        unsuccessful_association_policy_tlv = tlv(0xC4, 0x0005, '0x01 0x0000003c')
+
+        mid = controller.dev_send_1905(agent.mac, 0x8003,
+                                       metric_reporting_policy_tlv,
+                                       unsuccessful_association_policy_tlv)
+
+        sleep(1)
+        self.check_cmdu_type_single("ACK", 0x8000, agent.mac, controller.mac, mid)
+
+        sleep(12)  # TIME_METRICS value in test suite v2.
+
+        response = self.check_cmdu_type_single("AP metrics response", 0x800C,
+                                               agent.mac, controller.mac)
+
+        '''
+        AP Metrics Response message containing
+            one AP Metrics TLV and
+            one AP Extended Metrics TLV for each of the two fronthaul BSSes and
+            one Radio Metrics TLV for the fronthaul radio and
+            one Associated STA Traffic Stats TLV,
+            one Associated STA Link Metrics TLV and
+            one Associated STA Extended Link Metrics TLV for each of
+                CTT STA 1, CTT STA 2 and CTT STA 3.
+        '''
+
+        # one Radio Metrics TLV for the fronthaul radio and <-- we check only this at the moment
+        self.check_cmdu_has_tlvs(response, 0xC6)
+
         # Phase 7
+
+        # Phase 8
 
         # prepare tlvs
         sta_mac_addr_tlv = tlv(0x95, 0x0006, '{}'.format(sta2.mac))
@@ -77,14 +128,13 @@ class V2Certification_4_7_10(CommonFlows):
         # wait
         sleep(5)
         # check response
-        resp = self.check_cmdu_type_single("associated sta link metrics response", 0x800E,
-                                           agent.mac, controller.mac,
-                                           mid)
-        self.check_cmdu_has_tlvs(resp, 0xC8)
-        self.check_cmdu_has_tlvs(resp, 0x96)
+        associated_link_metrics_resp = self.check_cmdu_type_single(
+                                            "associated sta link metrics response", 0x800E,
+                                            agent.mac, controller.mac, mid)
+        self.check_cmdu_has_tlvs(associated_link_metrics_resp, 0xC8)
+        self.check_cmdu_has_tlvs(associated_link_metrics_resp, 0x96)
 
         # Phases 9 + 10
-
         # Disable reporting
         self.configure_multi_ap_policy_config_with_unsuccessful_association(agent, controller,
                                                                             0x00, 0x00)
