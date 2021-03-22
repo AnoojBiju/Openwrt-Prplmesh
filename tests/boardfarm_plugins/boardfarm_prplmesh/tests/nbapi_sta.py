898ff114d98f8a673d1f2af7e0e3fe63129b6c97
Krystyna Oliinyk <k.oliinyk@inango-systems.com>
2021-03-22 11:00:59 +0200

    boardfarm_prplmesh:nbapi_sta: test for nbapi sta object
    Boardfarm test for NBAPI sta pbject.
    
    PPM-1164
    --
    Signed-off-by: Krystyna Oliinyk <k.oliinyk@inango-systems.com>

---
+++ tests/boardfarm_plugins/boardfarm_prplmesh/tests/nbapi_sta.py
@@ -0,0 +1,143 @@
+
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+# SPDX-FileCopyrightText: 2021 the prplMesh contributors (see AUTHORS.md)
+# This code is subject to the terms of the BSD+Patent license.
+# See LICENSE file for more details.
+
+from .prplmesh_base_test import PrplMeshBaseTest
+from boardfarm.exceptions import SkipTest
+from capi import tlv
+from opts import debug
+
+import time
+from datetime import datetime, timedelta
+import dateutil.parser
+import pytz
+import re
+
+
+class NbapiSta(PrplMeshBaseTest):
+    '''
+    This test checks various parameters of NBAPI sta object.
+    Path to this object: Controller.Network.Device.Radio.BSS.STA
+    '''
+
+    def assertStats(self, name: str, actual: int, expected: str):
+        assert actual == int(expected), f"Wrong value for {name}: {actual} expected {expected}"
+
+    def runTest(self):
+        try:
+            agent = self.dev.DUT.agent_entity
+            sta1 = self.dev.wifi
+            controller = self.dev.lan.controller_entity
+            vap1 = agent.radios[0].vaps[0]
+        except AttributeError as ae:
+            raise SkipTest(ae)
+
+        self.dev.DUT.wired_sniffer.start(self.__class__.__name__ + "-" + self.dev.DUT.name)
+        self.configure_ssids_clear()
+        self.configure_ssid("ImSSID24G", "Fronthaul", {"Band2_4G": True})
+        self.configure_ssid("ImSSID5GH", "Fronthaul", {"Band5GH": True})
+        controller.nbapi_command("Controller.Network", "AccessPointCommit")
+
+        time.sleep(3)
+        sta1.wifi_connect(vap1)
+
+        agent.radios[1].send_bwl_event(
+            "DATA STA-UPDATE-STATS {} rssi=-38,-39,-40,-41 snr=38,39,40,41 "
+            "uplink=1000 downlink=800".format(sta1.mac))
+
+        print("\nNetwork topology after settings:")
+        topology = self.get_topology()
+        for device in topology.values():
+            print(device)
+
+        time_before_query = datetime.now()
+        time_before_query = pytz.utc.localize(time_before_query)
+
+        debug('Send AP Metrics Query Message')
+        mid1 = controller.dev_send_1905(agent.mac, 0x800B,
+                                        tlv(0x93, 0x0007, "0x01 {%s}" % (vap1.bssid)))
+        debug("Send Associated STA Link Metrics Query message")
+        controller.ucc_socket.dev_send_1905(agent.mac, 0x800D, tlv(0x95, 0x0006, sta1.mac))
+        time.sleep(2)
+
+        ap_metrics_resp = self.check_cmdu_type_single("AP metrics response", 0x800C, agent.mac,
+                                                      controller.mac, mid1)
+
+        debug("Check AP metrics response has STA traffic stats")
+        traffic_stats = self.check_cmdu_has_tlv_single(ap_metrics_resp, 0xa2)
+        debug("Check AP metrics response has STA Link Metrics")
+        sta_link_metrics = self.check_cmdu_has_tlv_single(ap_metrics_resp, 0x96)
+
+        for radio in topology[agent.mac].radios.values():
+            for bss in radio.vaps.values():
+                for sta in bss.clients.values():
+                    sta_mac = controller.nbapi_get_parameter(sta.path, "MACAddress")
+                    time_stamp = controller.nbapi_get_parameter(sta.path, "TimeStamp")
+                    est_mac_data_rate_downlink = controller.nbapi_get_parameter(
+                        sta.path, "EstMACDataRateDownlink")
+                    est_mac_data_rate_uplink = controller.nbapi_get_parameter(
+                        sta.path, "EstMACDataRateUplink")
+                    signal_strength = controller.nbapi_get_parameter(sta.path, "SignalStrength")
+                    bytes_sent = controller.nbapi_get_parameter(sta.path, "BytesSent")
+                    bytes_received = controller.nbapi_get_parameter(sta.path, "BytesReceived")
+                    packets_sent = controller.nbapi_get_parameter(sta.path, "PacketsSent")
+                    packets_received = controller.nbapi_get_parameter(sta.path, "PacketsReceived")
+                    error_sent = controller.nbapi_get_parameter(sta.path, "ErrorsSent")
+                    errors_received = controller.nbapi_get_parameter(sta.path, "ErrorsReceived")
+                    retrans_count = controller.nbapi_get_parameter(sta.path, "RetransCount")
+                    ipv4_address = controller.nbapi_get_parameter(sta.path, "IPV4Address")
+                    ipv6_address = controller.nbapi_get_parameter(sta.path, "IPV6Address")
+                    hostname = controller.nbapi_get_parameter(sta.path, "Hostname")
+
+                    assert sta_mac == sta1.mac, f"Wrong sta mac {sta_mac} expect {sta1.mac}"
+                    self.assertStats("BytesSent", bytes_sent,
+                                     traffic_stats.assoc_sta_traffic_stats_bytes_sent)
+                    self.assertStats("BytesReceived", bytes_received,
+                                     traffic_stats.assoc_sta_traffic_stats_bytes_rcvd)
+                    self.assertStats("PacketsSent", packets_sent,
+                                     traffic_stats.assoc_sta_traffic_stats_packets_sent)
+                    self.assertStats("PacketsReceived", packets_received,
+                                     traffic_stats.assoc_sta_traffic_stats_packets_rcvd)
+                    self.assertStats("ErrorsReceived", errors_received,
+                                     traffic_stats.assoc_sta_traffic_stats_rx_packet_errs)
+                    self.assertStats("ErrorsSent", error_sent,
+                                     traffic_stats.assoc_sta_traffic_stats_tx_pkt_errs)
+                    self.assertStats("RetransCount", retrans_count,
+                                     traffic_stats.assoc_sta_traffic_stats_retrans_count)
+                    self.assertStats("EstMACDataRateDownlink", est_mac_data_rate_downlink,
+                                     sta_link_metrics.bss[0].down_rate)
+                    self.assertStats("EstMACDataRateUplink", est_mac_data_rate_uplink,
+                                     sta_link_metrics.bss[0].up_rate)
+                    assert signal_strength == sta_link_metrics.bss[0].rssi, \
+                        f"Wrong value for SignalStrength {signal_strength}"
+                    assert sta_link_metrics.bss[0].bssid == bss.bssid, \
+                        f"Wrong BSSID [{bss.bssid}] specified for sta {sta.mac}"
+                    assert time_stamp != 0, "Value for TimeStamp is not specified."
+
+                    time_sta = dateutil.parser.isoparse(time_stamp)
+                    # TO DO: PPM-1230
+                    if time_sta <= time_before_query:
+                        debug('Fail. Sta time group was collected earlier '
+                              'than AP Metrics Query was sent.')
+                    if time_before_query + timedelta(seconds=10) <= time_sta:
+                        self.fail('Fail. Sta time group was collected '
+                                  'more than 10s after AP Metrics Query was send.')
+                    print(
+                        f'Sta group was collected at: [{time_sta}], '
+                        f'time before query: {time_before_query}')
+
+                    check_time_format = re.match(
+                        r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d+(Z|[-+]\d{2}:\d{2})',
+                        time_stamp)
+                    if check_time_format is None:
+                        self.fail(f'Fail. NBAPI time stamp has unncorrect format: {time_stamp}')
+                    # TO DO: PPM-535, PPM-534 Uncomment after issues are complete.
+                    # assert ipv6_address != "0" or ipv4_address != "0",\
+                    #     "Value for ipv4 and for ipv6 address not specified."
+                    # assert hostname != 0, "Missing value for ipv4 address."
+                    (ipv4_address)
+                    (ipv6_address)
+                    (hostname)
+        sta1.wifi_disconnect(vap1)
