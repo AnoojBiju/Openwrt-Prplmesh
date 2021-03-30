a5e529a58b696654a04c654189a9be31d76d3020
Krystyna Oliinyk <k.oliinyk@inango-systems.com>
2021-03-30 17:04:21 +0300

    controller:task_pool.cpp: fix loop stopping
    Loop in the controller task pool stops after handling only first particular 1905 message.
    If any other task relies on the same message it will causes to failure.
    
    PPM-992
    --
    Signed-off-by: Krystyna Oliinyk <k.oliinyk@inango-systems.com>

--- controller/src/beerocks/master/tasks/task_pool.h
+++ controller/src/beerocks/master/tasks/task_pool.h
@@ -35,9 +35,8 @@ public:
      *
      * @param src_mac MAC address of the message sender.
      * @param cmdu_rx CMDU object containing the received message to be handled.
-     * @return True if the message has been handled, otherwise false.
      */
-    bool handle_ieee1905_1_msg(const std::string &src_mac, ieee1905_1::CmduMessageRx &cmdu_rx);
+    void handle_ieee1905_1_msg(const std::string &src_mac, ieee1905_1::CmduMessageRx &cmdu_rx);
 
 private:
     std::unordered_map<int, std::shared_ptr<task>> m_scheduled_tasks;
