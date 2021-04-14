66e60be657818f12655c1f273d918ccd2b77a744
mmazzapater <mmazzapater@gmail.com>
2021-04-14 14:17:41 +0200

    tlvf: add autogenerated files
    https://jira.prplfoundation.org/browse/PPM-6
    
    Signed-off-by: mmazzapater <mmazzapater@gmail.com>

--- common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_backhaul.h
+++ common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_backhaul.h
@@ -746,6 +746,25 @@ class cACTION_BACKHAUL_RADIO_DISABLE_REQUEST : public BaseClass
         eActionOp_BACKHAUL* m_action_op = nullptr;
 };
 
+class cACTION_BACKHAUL_RADIO_TEAR_DOWN_REQUEST : public BaseClass
+{
+    public:
+        cACTION_BACKHAUL_RADIO_TEAR_DOWN_REQUEST(uint8_t* buff, size_t buff_len, bool parse = false);
+        explicit cACTION_BACKHAUL_RADIO_TEAR_DOWN_REQUEST(std::shared_ptr<BaseClass> base, bool parse = false);
+        ~cACTION_BACKHAUL_RADIO_TEAR_DOWN_REQUEST();
+
+        static eActionOp_BACKHAUL get_action_op(){
+            return (eActionOp_BACKHAUL)(ACTION_BACKHAUL_RADIO_TEAR_DOWN_REQUEST);
+        }
+        void class_swap() override;
+        bool finalize() override;
+        static size_t get_initial_size();
+
+    private:
+        bool init();
+        eActionOp_BACKHAUL* m_action_op = nullptr;
+};
+
 class cACTION_BACKHAUL_CHANNEL_SCAN_TRIGGER_SCAN_REQUEST : public BaseClass
 {
     public:
