40b7373e396ab800a4f9e640d356998a31d7dca0
Vitalii Komisarenko <vitalii.komisarenko@mind.be>
2021-04-25 03:26:24 +0200

    controller: db: use sMacAddr in get_hostap_vht_center_frequency
    PPM-1325
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@mind.be>

--- controller/src/beerocks/master/tasks/topology_task.cpp
+++ controller/src/beerocks/master/tasks/topology_task.cpp
@@ -435,7 +435,7 @@ bool topology_task::handle_topology_notification(const std::string &src_mac,
         database.set_node_channel_bw(client_mac_str, database.get_node_channel(bssid_str),
                                      database.get_node_bw(bssid_str),
                                      database.get_node_channel_ext_above_secondary(bssid_str), 0,
-                                     database.get_hostap_vht_center_frequency(bssid_str));
+                                     database.get_hostap_vht_center_frequency(bssid));
 
         // Note: The Database node stats and the Datamodels' stats are not the same.
         // Therefore, client information in data model and in node DB might differ.
