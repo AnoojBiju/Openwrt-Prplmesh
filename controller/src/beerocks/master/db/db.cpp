d582ba067b980cdf651005ef576fb5b7d55cbc01
Vitalii Komisarenko <vitalii.komisarenko@mind.be>
2021-04-25 03:18:26 +0200

    controller: db: use sMacAddr in set_hostap_is_acs_enabled
    PPM-1057
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@mind.be>

--- controller/src/beerocks/master/db/db.cpp
+++ controller/src/beerocks/master/db/db.cpp
@@ -2946,7 +2946,7 @@ bool db::clear_hostap_dfs_reentry_clients(const sMacAddr &mac)
     return true;
 }
 
-bool db::set_hostap_is_acs_enabled(const std::string &mac, bool enable)
+bool db::set_hostap_is_acs_enabled(const sMacAddr &mac, bool enable)
 {
     auto n = get_node(mac);
 
