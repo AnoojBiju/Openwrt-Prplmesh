624cf9cc36c99a6bdf6302c6560ddeab6790e0b1
Vitalii Komisarenko <vitalii.komisarenko@mind.be>
2021-04-27 13:00:30 +0200

    controller: controller.cpp: move sMacAddr of radio_mac higher
    Radio MACs are being converted to `sMacAddr` but a lot of
    code still uses `std::string`. As a result, there are many
    conversions between these types.
    
    Move cached `sMacAddr` version of `radio` mac higher in
    the code to remove a bunch of `tlvf::mac_from_string` calls.
    
    PPM-1325
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@mind.be>

--- controller/src/beerocks/master/controller.cpp
+++ controller/src/beerocks/master/controller.cpp
@@ -2226,14 +2226,15 @@ bool Controller::handle_intel_slave_join(
     /*
     * handle the HOSTAP node
     */
-    if (database.has_node(tlvf::mac_from_string(radio_mac))) {
+    auto mac = tlvf::mac_from_string(radio_mac);
+    if (database.has_node(mac)) {
         if (database.get_node_type(radio_mac) != beerocks::TYPE_SLAVE) {
             database.set_node_type(radio_mac, beerocks::TYPE_SLAVE);
             LOG(ERROR) << "Existing mac node is not TYPE_SLAVE";
         }
         database.clear_hostap_stats_info(radio_mac);
     } else {
-        database.add_node_radio(tlvf::mac_from_string(radio_mac), tlvf::mac_from_string(bridge_mac),
+        database.add_node_radio(mac, tlvf::mac_from_string(bridge_mac),
                                 tlvf::mac_from_string(radio_identifier));
     }
     database.set_hostap_is_acs_enabled(radio_mac, acs_enabled);
@@ -2252,18 +2253,15 @@ bool Controller::handle_intel_slave_join(
             }
         }
     }
-    database.set_hostap_repeater_mode_flag(tlvf::mac_from_string(radio_mac),
-                                           notification->enable_repeater_mode());
+    database.set_hostap_repeater_mode_flag(mac, notification->enable_repeater_mode());
     database.set_hostap_backhaul_manager(radio_mac, backhaul_manager);
 
     database.set_node_state(radio_mac, beerocks::STATE_CONNECTED);
     database.set_node_backhaul_iface_type(radio_mac, is_gw_slave ? beerocks::IFACE_TYPE_GW_BRIDGE
                                                                  : beerocks::IFACE_TYPE_BRIDGE);
-    database.set_hostap_iface_name(tlvf::mac_from_string(radio_mac),
-                                   notification->hostap().iface_name);
-    database.set_hostap_iface_type(tlvf::mac_from_string(radio_mac), hostap_iface_type);
-    database.set_hostap_driver_version(tlvf::mac_from_string(radio_mac),
-                                       notification->hostap().driver_version);
+    database.set_hostap_iface_name(mac, notification->hostap().iface_name);
+    database.set_hostap_iface_type(mac, hostap_iface_type);
+    database.set_hostap_driver_version(mac, notification->hostap().driver_version);
 
     database.set_hostap_ant_num(radio_mac, (beerocks::eWiFiAntNum)notification->hostap().ant_num);
     database.set_hostap_ant_gain(radio_mac, notification->hostap().ant_gain);
@@ -2275,19 +2273,15 @@ bool Controller::handle_intel_slave_join(
 
     if (database.get_node_5ghz_support(radio_mac)) {
         if (notification->low_pass_filter_on()) {
-            database.set_hostap_band_capability(tlvf::mac_from_string(radio_mac),
-                                                beerocks::LOW_SUBBAND_ONLY);
+            database.set_hostap_band_capability(mac, beerocks::LOW_SUBBAND_ONLY);
         } else {
-            database.set_hostap_band_capability(tlvf::mac_from_string(radio_mac),
-                                                beerocks::BOTH_SUBBAND);
+            database.set_hostap_band_capability(mac, beerocks::BOTH_SUBBAND);
         }
     } else {
-        database.set_hostap_band_capability(tlvf::mac_from_string(radio_mac),
-                                            beerocks::SUBBAND_CAPABILITY_UNKNOWN);
+        database.set_hostap_band_capability(mac, beerocks::SUBBAND_CAPABILITY_UNKNOWN);
     }
     autoconfig_wsc_parse_radio_caps(radio_mac, radio_caps);
 
-    auto mac = tlvf::mac_from_string(radio_mac);
     if (tasks.is_task_running(database.get_dynamic_channel_selection_task_id(mac))) {
         LOG(DEBUG) << "dynamic channel selection task already running for " << mac;
     } else {
