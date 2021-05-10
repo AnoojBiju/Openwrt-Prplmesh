b368bbad6632337e10c4473d3a3de5c81320b28f
Vitalii Komisarenko <vitalii.komisarenko@mind.be>
2021-05-10 16:20:32 +0200

    controller: db: move node::radio to sAgent::sRadio
    Inline `node::hostap` data member into `sAgent::sRadio`.
    
    Move `stats_info` initialization to the header file.
    
    Change `get_radio_by_uid` to look for `sAgent::sRadio`
    instead of `node::hostap`.
    
    Move code that prints radio-specific part of node into
    a separate method. The callers are NOT updated.
    
    PPM-1060
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@mind.be>

--- controller/src/beerocks/master/db/db.h
+++ controller/src/beerocks/master/db/db.h
@@ -1763,7 +1763,8 @@ private:
      * @return std::shared_ptr<node> pointer to the node on success, nullptr otherwise.
      */
     std::shared_ptr<node> get_node_verify_type(const sMacAddr &mac, beerocks::eType type);
-    std::shared_ptr<node::radio> get_radio_by_uid(const sMacAddr &radio_uid);
+    std::shared_ptr<prplmesh::controller::db::sAgent::sRadio>
+    get_radio_by_uid(const sMacAddr &radio_uid);
     int get_node_hierarchy(std::shared_ptr<node> n);
     std::set<std::shared_ptr<node>> get_node_subtree(std::shared_ptr<node> n);
     void adjust_subtree_hierarchy(std::shared_ptr<node> n);
