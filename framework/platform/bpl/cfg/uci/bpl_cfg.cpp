21434be7205249278e23be554de2023792f6a0bf
Raphaël Mélotte <raphael.melotte@mind.be>
2021-06-08 18:08:43 +0200

    bpl: skip configuration of hidden BSS when reading wireless settings
    MaxLinear devices have a dummy VAP for each radio, which should not be
    used when reading the existing wireless settings.
    
    Besides the fact that they miss the 'encryption' option (which is
    optional in OpenWrt) the only thing that we can use to distinguish
    them from regular VAPs is the 'hidden' option.
    
    Aside from that, prplMesh's representation of a BSS configuration
    anyway doesn't support the hidden option, and we most likely don't
    want the controller to propagate its hidden SSIDs to other agents.
    
    When a hidden BSS is found, log it and skip it.
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- framework/platform/bpl/cfg/uci/bpl_cfg.cpp
+++ framework/platform/bpl/cfg/uci/bpl_cfg.cpp
@@ -665,6 +665,16 @@ bool bpl_cfg_get_wireless_settings(std::list<son::wireless_utils::sBssInfoConf> 
             continue;
         }
 
+        std::string hidden;
+        uci_get_option(package_name, section_type, section_name, "hidden", hidden);
+        // the hidden option might not exist, in which case we treat
+        // it as if it was 0 (i.e. we don't skip the section).
+
+        if (hidden == "1") {
+            LOG(INFO) << "Skipping configuration for section with 'hidden=1':" << section_name;
+            continue;
+        }
+
         // Silently ignore sections that do not configure a fronthaul interface
         if (mode != "ap") {
             continue;
