2a7a0e308f9b27378c09d0bb8f71bf9e70192045
Adam Dov <adov@maxlinear.com>
2021-07-21 17:25:22 +0300

    monitor_db: add the measurements mode flag
    Preparative Commit.
    The configuration can be read from UCI only once and used throughout the
    monitor lifetime since any change requires prplmesh restart.
    
    Add parameter to the monitor_db to store the read configuration.
    Add APIs to get and set the parameter.
    Add enum, equivalent to the BPL enum that can be used in the monitor_db
    header without the need to include the BPL in the header.
    
    PPM-1519.
    
    Signed-off-by: Adam Dov <adov@maxlinear.com>

--- agent/src/beerocks/fronthaul_manager/monitor/monitor_db.h
+++ agent/src/beerocks/fronthaul_manager/monitor/monitor_db.h
@@ -412,6 +412,13 @@ private:
 ////////////////////////////////////////////
 class monitor_db {
 public:
+    enum class eClientsMeasurementMode : uint8_t {
+        // equivalent to bpl::eClientsMeasurementMode
+        DISABLE_ALL = 0,
+        ENABLE_ALL,
+        ONLY_CLIENTS_SELECTED_FOR_STEERING
+    };
+
     monitor_db();
     ~monitor_db();
 
@@ -474,6 +481,12 @@ public:
     std::chrono::steady_clock::time_point get_ap_poll_next_time();
     void set_ap_poll_next_time(std::chrono::steady_clock::time_point pt, bool reset_poll = false);
 
+    void set_clients_measuremet_mode(eClientsMeasurementMode mode)
+    {
+        m_clients_measurement_mode = mode;
+    }
+    eClientsMeasurementMode get_clients_measuremet_mode() { return m_clients_measurement_mode; }
+
     const int MONITOR_LAST_CHANGE_TIMEOUT_MSEC   = 30000;
     const int MONITOR_DB_POLLING_RATE_MSEC       = 250;
     const int MONITOR_DB_MEASUREMENT_WINDOW_MSEC = (4 * MONITOR_DB_POLLING_RATE_MSEC);
@@ -501,6 +514,13 @@ private:
 
     int arp_burst_pkt_num = 0;
     int arp_burst_delay   = 0;
+
+    /**
+     * The mode is read once from BPL as part of the monitor start and used to determine if
+     * measurements for clients are: disable, enabled or enabled-only-for-selected-clients.
+     * The default value is set to ENABLE_ALL.
+    */
+    eClientsMeasurementMode m_clients_measurement_mode = eClientsMeasurementMode::ENABLE_ALL;
 };
 
 } // namespace son
