00db56365f4aeb492dac2c972db48724a8dc06f3
Moran Shoeg <moranx.shoeg@intel.com>
2021-08-10 12:07:32 +0300

    bpl: Prepare bpl_err.h
    The file bpl_err.h defines an enum using a magic macro.
    This is not needed anymore, and the enumAutoPrint does
    handle it properly.
    Remove the magic macro, and instead rewrite it as a normal enum.
    Also, add include to ostream.
    
    Signed-off-by: Moran Shoeg <moranx.shoeg@intel.com>

--- framework/platform/bpl/include/bpl/bpl_err.h
+++ framework/platform/bpl/include/bpl/bpl_err.h
@@ -9,6 +9,8 @@
 #ifndef _BPL_ERR_H_
 #define _BPL_ERR_H_
 
+#include <ostream>
+
 namespace beerocks {
 namespace bpl {
 
@@ -24,55 +26,49 @@ namespace bpl {
 #define GENERATE_ERROR_STRING(STRING) "BPL_ERR_" #STRING,
 
 /* Beerocks Error Codes */
-
-// clang-format off
-
-#define FOREACH_ERROR_CODE(ERROR_CODE)                                                              \
-/* 00 */ ERROR_CODE(NONE)                                                                   \
-/* 01 */ ERROR_CODE(UNKNOWN)                                                                \
-/* 02 */ ERROR_CODE(OPERATION_NOT_SUPPORTED)                                                \
-/* 03 */ ERROR_CODE(BH_READING_DATA_FROM_THE_BRIDGE)                                        \
-/* 04 */ ERROR_CODE(BH_TIMEOUT_ATTACHING_TO_WPA_SUPPLICANT)                                 \
-/* 05 */ ERROR_CODE(BH_SCAN_FAILED_TO_INITIATE_SCAN)                                        \
-/* 06 */ ERROR_CODE(BH_SCAN_TIMEOUT)                                                        \
-/* 07 */ ERROR_CODE(BH_SCAN_EXCEEDED_MAXIMUM_FAILED_SCAN_ATTEMPTS)                          \
-/* 08 */ ERROR_CODE(BH_CONNECTING_TO_MASTER)                                                \
-/* 09 */ ERROR_CODE(BH_ASSOCIATE_4ADDR_TIMEOUT)                                             \
-/* 10 */ ERROR_CODE(BH_ASSOCIATE_4ADDR_FAILURE)                                             \
-/* 11 */ ERROR_CODE(BH_ROAMING)                                                             \
-/* 12 */ ERROR_CODE(BH_DISCONNECTED)                                                        \
-/* 13 */ ERROR_CODE(BH_WPA_SUPPLICANT_TERMINATED)                                           \
-/* 14 */ ERROR_CODE(BH_SLAVE_SOCKET_DISCONNECTED)                                           \
-/* 15 */ ERROR_CODE(BH_STOPPED)                                                             \
-/* 16 */ ERROR_CODE(SLAVE_CONNECTING_TO_BACKHAUL_MANAGER)                                   \
-/* 17 */ ERROR_CODE(SLAVE_INVALID_MASTER_SOCKET)                                            \
-/* 18 */ ERROR_CODE(SLAVE_FAILED_CONNECT_TO_PLATFORM_MANAGER)                               \
-/* 19 */ ERROR_CODE(SLAVE_PLATFORM_MANAGER_REGISTER_TIMEOUT)                                \
-/* 20 */ ERROR_CODE(SLAVE_SLAVE_BACKHAUL_MANAGER_DISCONNECTED)                              \
-/* 21 */ ERROR_CODE(SLAVE_STOPPED)                                                          \
-/* 22 */ ERROR_CODE(AP_MANAGER_START)                                                       \
-/* 23 */ ERROR_CODE(AP_MANAGER_DISCONNECTED)                                                \
-/* 24 */ ERROR_CODE(AP_MANAGER_HOSTAP_DISABLED)                                             \
-/* 25 */ ERROR_CODE(AP_MANAGER_ATTACH_FAIL)                                                 \
-/* 26 */ ERROR_CODE(AP_MANAGER_SUDDEN_DETACH)                                               \
-/* 27 */ ERROR_CODE(AP_MANAGER_HAL_DISCONNECTED)                                            \
-/* 28 */ ERROR_CODE(AP_MANAGER_CAC_TIMEOUT)                                                 \
-/* 29 */ ERROR_CODE(MONITOR_DISCONNECTED)                                                   \
-/* 30 */ ERROR_CODE(MONITOR_HOSTAP_DISABLED)                                                \
-/* 31 */ ERROR_CODE(MONITOR_ATTACH_FAIL)                                                    \
-/* 32 */ ERROR_CODE(MONITOR_SUDDEN_DETACH)                                                  \
-/* 33 */ ERROR_CODE(MONITOR_HAL_DISCONNECTED)                                               \
-/* 34 */ ERROR_CODE(MONITOR_REPORT_PROCESS_FAIL)                                            \
-/* 35 */ ERROR_CODE(CONFIG_PLATFORM_REPORTED_INVALID_CONFIGURATION)                         \
-/* 36 */ ERROR_CODE(CONFIG_NO_VALID_BACKHAUL_INTERFACE)                                     \
-/* 37 */ ERROR_CODE(WATCHDOG_PROCESS_STUCK)                                                 \
-/* 38 */ ERROR_CODE(WATCHDOG_PROCESS_ZOMBIE)                                                \
-/* 39 */ ERROR_CODE(LAST)
-
-// clang-format on
-
 // Error codes ENUM
-enum class eErrorCode { FOREACH_ERROR_CODE(GENERATE_ERROR_ENUM) };
+enum class eErrorCode {
+    /* 00 */ NONE,
+    /* 01 */ UNKNOWN,
+    /* 02 */ OPERATION_NOT_SUPPORTED,
+    /* 03 */ BH_READING_DATA_FROM_THE_BRIDGE,
+    /* 04 */ BH_TIMEOUT_ATTACHING_TO_WPA_SUPPLICANT,
+    /* 05 */ BH_SCAN_FAILED_TO_INITIATE_SCAN,
+    /* 06 */ BH_SCAN_TIMEOUT,
+    /* 07 */ BH_SCAN_EXCEEDED_MAXIMUM_FAILED_SCAN_ATTEMPTS,
+    /* 08 */ BH_CONNECTING_TO_MASTER,
+    /* 09 */ BH_ASSOCIATE_4ADDR_TIMEOUT,
+    /* 10 */ BH_ASSOCIATE_4ADDR_FAILURE,
+    /* 11 */ BH_ROAMING,
+    /* 12 */ BH_DISCONNECTED,
+    /* 13 */ BH_WPA_SUPPLICANT_TERMINATED,
+    /* 14 */ BH_SLAVE_SOCKET_DISCONNECTED,
+    /* 15 */ BH_STOPPED,
+    /* 16 */ SLAVE_CONNECTING_TO_BACKHAUL_MANAGER,
+    /* 17 */ SLAVE_INVALID_MASTER_SOCKET,
+    /* 18 */ SLAVE_FAILED_CONNECT_TO_PLATFORM_MANAGER,
+    /* 19 */ SLAVE_PLATFORM_MANAGER_REGISTER_TIMEOUT,
+    /* 20 */ SLAVE_SLAVE_BACKHAUL_MANAGER_DISCONNECTED,
+    /* 21 */ SLAVE_STOPPED,
+    /* 22 */ AP_MANAGER_START,
+    /* 23 */ AP_MANAGER_DISCONNECTED,
+    /* 24 */ AP_MANAGER_HOSTAP_DISABLED,
+    /* 25 */ AP_MANAGER_ATTACH_FAIL,
+    /* 26 */ AP_MANAGER_SUDDEN_DETACH,
+    /* 27 */ AP_MANAGER_HAL_DISCONNECTED,
+    /* 28 */ AP_MANAGER_CAC_TIMEOUT,
+    /* 29 */ MONITOR_DISCONNECTED,
+    /* 30 */ MONITOR_HOSTAP_DISABLED,
+    /* 31 */ MONITOR_ATTACH_FAIL,
+    /* 32 */ MONITOR_SUDDEN_DETACH,
+    /* 33 */ MONITOR_HAL_DISCONNECTED,
+    /* 34 */ MONITOR_REPORT_PROCESS_FAIL,
+    /* 35 */ CONFIG_PLATFORM_REPORTED_INVALID_CONFIGURATION,
+    /* 36 */ CONFIG_NO_VALID_BACKHAUL_INTERFACE,
+    /* 37 */ WATCHDOG_PROCESS_STUCK,
+    /* 38 */ WATCHDOG_PROCESS_ZOMBIE,
+    /* 39 */ LAST,
+};
 
 } // namespace bpl
 } // namespace beerocks
