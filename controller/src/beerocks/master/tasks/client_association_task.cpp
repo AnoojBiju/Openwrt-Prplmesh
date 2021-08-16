e7d4cf0b26c0d7e2b1895b437b2caf31d2603521
Krystyna Oliinyk <k.oliinyk@inango-systems.com>
2021-08-16 23:20:20 +0300

    controller/client_association_task: detect station association
    Need to request a Client Capability Query when a STA is first detected.
    
    Add 'verify_sta_association' method which will send
    Client Capability Query, when station associate first time.
    Store timestamp and mac of station that (re)associate.
    
    PPM-1272
    --
    Signed-off-by: Krystyna Oliinyk <k.oliinyk@inango-systems.com>

--- controller/src/beerocks/master/tasks/client_association_task.cpp
+++ controller/src/beerocks/master/tasks/client_association_task.cpp
@@ -7,6 +7,10 @@
  */
 
 #include "client_association_task.h"
+#include "../son_actions.h"
+
+#include <tlvf/wfa_map/tlvClientAssociationEvent.h>
+#include <tlvf/wfa_map/tlvClientInfo.h>
 
 using namespace beerocks;
 using namespace net;
@@ -18,8 +22,76 @@ client_association_task::client_association_task(db &database_, ieee1905_1::Cmdu
 {
 }
 
+void client_association_task ::work() {}
+
 bool client_association_task::handle_ieee1905_1_msg(const sMacAddr &src_mac,
                                                     ieee1905_1::CmduMessageRx &cmdu_rx)
 {
+    switch (cmdu_rx.getMessageType()) {
+    case ieee1905_1::eMessageType::TOPOLOGY_NOTIFICATION_MESSAGE: {
+        verify_sta_association(src_mac, cmdu_rx);
+        break;
+    }
+    default: {
+        return false;
+    }
+    }
+    return true;
+}
+
+bool client_association_task::verify_sta_association(const sMacAddr &src_mac,
+                                                     ieee1905_1::CmduMessageRx &cmdu_rx)
+{
+    auto sta_assoc_tlv = cmdu_rx.getClass<wfa_map::tlvClientAssociationEvent>();
+
+    if (!sta_assoc_tlv) {
+        return false;
+    }
+
+    auto ambiorix_dm = m_database.get_ambiorix_obj();
+
+    if (!ambiorix_dm) {
+        LOG(ERROR) << "Failed to get Ambiorix datamodel";
+        return false;
+    }
+    if (sta_assoc_tlv->association_event() ==
+        wfa_map::tlvClientAssociationEvent::eAssociationEvent::CLIENT_HAS_JOINED_THE_BSS) {
+        if (m_assoc_sta.find(sta_assoc_tlv->client_mac()) != m_assoc_sta.end()) {
+            // STA Reassociate
+            m_assoc_sta[sta_assoc_tlv->client_mac()] = ambiorix_dm->get_datamodel_time_format();
+            return false;
+        }
+        m_assoc_sta[sta_assoc_tlv->client_mac()] = ambiorix_dm->get_datamodel_time_format();
+        if (!send_sta_capability_query(src_mac, cmdu_rx)) {
+            LOG(ERROR) << "Failed to send Client Capability Query.";
+            return false;
+        }
+        return true;
+    }
     return false;
+}
+
+bool client_association_task::send_sta_capability_query(const sMacAddr &src_mac,
+                                                        ieee1905_1::CmduMessageRx &cmdu_rx)
+{
+    auto client_association_event_tlv = cmdu_rx.getClass<wfa_map::tlvClientAssociationEvent>();
+
+    if (!client_association_event_tlv) {
+        return false;
+    }
+    if (!m_cmdu_tx.create(0, ieee1905_1::eMessageType::CLIENT_CAPABILITY_QUERY_MESSAGE)) {
+        LOG(ERROR) << "cmdu creation of type CLIENT_CAPABILITY_QUERY_MESSAGE, has failed";
+        return false;
+    }
+
+    auto client_info_tlv = m_cmdu_tx.addClass<wfa_map::tlvClientInfo>();
+
+    if (!client_info_tlv) {
+        LOG(ERROR) << "addClass wfa_map::tlvClientInfo has failed";
+        return false;
+    }
+    client_info_tlv->client_mac() = client_association_event_tlv->client_mac();
+    client_info_tlv->bssid()      = client_association_event_tlv->bssid();
+    son_actions::send_cmdu_to_agent(src_mac, m_cmdu_tx, m_database);
+    return true;
 }
