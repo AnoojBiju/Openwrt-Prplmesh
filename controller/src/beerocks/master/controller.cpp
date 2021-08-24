2f61abf59be2f9b41db8c0adb0e7184109875eec
Cihangir Odabas <codabas@assia-inc.com>
2021-08-25 01:22:24 +0300

    controller: db: update last contact time of the agent
    Whenever new Multi-AP control protocol is received from an agent,
    this logic updates agents last contact time in database and also in
    datamodel.
    
    https://jira.prplfoundation.org/browse/PPM-1396
    --
    Signed-off-by: Cihangir Odabas <codabas@assia-inc.com>

--- controller/src/beerocks/master/controller.cpp
+++ controller/src/beerocks/master/controller.cpp
@@ -408,6 +408,8 @@ bool Controller::handle_cmdu(int fd, uint32_t iface_index, const sMacAddr &dst_m
         LOG(DEBUG) << "received 1905.1 cmdu message";
         handle_cmdu_1905_1_message(src_mac, cmdu_rx);
         tasks.handle_ieee1905_1_msg(src_mac, cmdu_rx);
+
+        database.update_last_contact_time(src_mac);
     }
 
     return true;
