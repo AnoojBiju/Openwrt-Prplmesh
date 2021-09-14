e7f2125a80d8a69d0297bee8341d0a1f77842a39
Moran Shoeg <moranx.shoeg@intel.com>
2021-09-14 13:35:05 +0300

    Revert "build: Fix build with VSCODE task"
    This reverts commit 2720af0f8a3df430dda4b11a4589c75b0a410bab.

--- tools/commands/build.py
+++ tools/commands/build.py
@@ -93,12 +93,9 @@ class cmakebuilder(builder):
         cmd = ["cmake",
                "--build", self.build_path,
                "--target", "install"]
-
-        logger.info("building args: {}".format(self.cmake_flags))
-
         if self.make_verbose:
             cmd.extend(["--", "VERBOSE=1"])
-        elif not self.generator:  # self.generator is empty
+        else:
             cmd.extend(["--", "-j", str(self.parallel)])
         logger.info("building & installing {}: {}".format(self.name, " ".join(cmd)))
         subprocess.check_call(cmd, env=self.env)
