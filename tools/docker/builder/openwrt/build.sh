030e6203aac239461d0942a35ea68d060146fe1e
Raphaël Mélotte <raphael.melotte@mind.be>
2021-11-02 10:50:38 +0100

    tools: openwrt: bump to use prplOS-built python
    Also bump the base version, as prplMesh dependencies changed.
    
    This depends on the following MR:
    https://gitlab.com/prpl-foundation/prplos/prplos/-/merge_requests/157
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -185,7 +185,7 @@ main() {
 VERBOSE=false
 IMAGE_ONLY=false
 OPENWRT_REPOSITORY='https://gitlab.com/prpl-foundation/prplwrt/prplwrt.git'
-OPENWRT_VERSION='096301732a20020194fd1ae8e4fa62575bc5b62a'
+OPENWRT_VERSION='4a9fc811619d06f21e7ec93f8958f890b5ca6e06'
 PRPLMESH_VARIANT="-nl80211"
 DOCKER_TARGET_STAGE="prplmesh-builder"
 SHELL_ONLY=false
