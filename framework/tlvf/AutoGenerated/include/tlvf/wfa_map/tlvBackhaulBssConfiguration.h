b28670f0f455885cf92408e53d16c0dafd1305ca
hksrirama <hksrirama@maxlinear.com>
2021-11-19 08:41:50 +0200

    tlvf : Generated tlvBackhaulBssConfiguration cpp files
    Generated tlvBackhaulBssConfiguration
     contains all  tlv attributes and required
      functions.
    
    Signed-off-by: hksrirama <hksrirama@maxlinear.com>

---
+++ framework/tlvf/AutoGenerated/include/tlvf/wfa_map/tlvBackhaulBssConfiguration.h
@@ -0,0 +1,73 @@
+///////////////////////////////////////
+// AUTO GENERATED FILE - DO NOT EDIT //
+///////////////////////////////////////
+
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * SPDX-FileCopyrightText: 2016-2020 the prplMesh contributors (see AUTHORS.md)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#ifndef _TLVF_WFA_MAP_TLVBACKHAULBSSCONFIGURATION_H_
+#define _TLVF_WFA_MAP_TLVBACKHAULBSSCONFIGURATION_H_
+
+#include <cstddef>
+#include <stdint.h>
+#include <tlvf/swap.h>
+#include <string.h>
+#include <memory>
+#include <tlvf/BaseClass.h>
+#include <tlvf/ClassList.h>
+#include "tlvf/wfa_map/eTlvTypeMap.h"
+#include "tlvf/common/sMacAddr.h"
+#include <asm/byteorder.h>
+
+namespace wfa_map {
+
+
+class tlvBackhaulBssConfiguration : public BaseClass
+{
+    public:
+        tlvBackhaulBssConfiguration(uint8_t* buff, size_t buff_len, bool parse = false);
+        explicit tlvBackhaulBssConfiguration(std::shared_ptr<BaseClass> base, bool parse = false);
+        ~tlvBackhaulBssConfiguration();
+
+        typedef struct sFlags {
+            #if defined(__LITTLE_ENDIAN_BITFIELD)
+            uint8_t reserved : 6;
+            uint8_t profile_2_bsta_disallowed : 1;
+            uint8_t profile_1_bsta_disallowed : 1;
+            #elif defined(__BIG_ENDIAN_BITFIELD)
+            uint8_t profile_1_bsta_disallowed : 1;
+            uint8_t profile_2_bsta_disallowed : 1;
+            uint8_t reserved : 6;
+            #else
+            #error "Bitfield macros are not defined"
+            #endif
+            void struct_swap(){
+            }
+            void struct_init(){
+            }
+        } __attribute__((packed)) sFlags;
+        
+        const eTlvTypeMap& type();
+        const uint16_t& length();
+        sMacAddr& bssid();
+        sFlags& flags();
+        void class_swap() override;
+        bool finalize() override;
+        static size_t get_initial_size();
+
+    private:
+        bool init();
+        eTlvTypeMap* m_type = nullptr;
+        uint16_t* m_length = nullptr;
+        sMacAddr* m_bssid = nullptr;
+        sFlags* m_flags = nullptr;
+};
+
+}; // close namespace: wfa_map
+
+#endif //_TLVF/WFA_MAP_TLVBACKHAULBSSCONFIGURATION_H_
