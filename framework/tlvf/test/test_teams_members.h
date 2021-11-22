38bf9668ae21f976d53ce958a45f01d9a8dd061c
anantkumar <cchavda@maxlinear.com>
2021-11-22 17:39:50 +0200

    tlvf: Add build-parse test api for teams_members
    Signed-off-by: anantkumar <cchavda@maxlinear.com>

---
+++ framework/tlvf/test/test_teams_members.h
@@ -0,0 +1,14 @@
+#ifndef _TEST_TEAMS_MEMBERS_H_
+#define _TEST_TEAMS_MEMBERS_H_
+#include "tlvf/CmduMessageRx.h"
+#include "tlvf/CmduMessageTx.h"
+#include "tlvf/tlvftypes.h"
+#include "tlvf/wfa_map/tlvTeamsMembers.h"
+#include <mapf/common/logger.h>
+
+using namespace ieee1905_1;
+using namespace wfa_map;
+
+uint8_t test_teams_members_build(uint8_t *tx_buffer, size_t len);
+void test_teams_members_parse(uint8_t *tx_buffer, size_t len);
+#endif //_TEST_TEAMS_MEMBERS_H_
