c83ec80c2496c367055a869bcb3ae6356d2bd730
Frederik Van Bogaert <frederik.vanbogaert@mind.be>
2021-12-02 10:53:16 +0100

    Add docgen to gitlab CI: initial attempt
    Signed-off-by: Frederik Van Bogaert <frederik.vanbogaert@mind.be>

--- tools/nbapi-docgen/parse_api.sh
+++ tools/nbapi-docgen/parse_api.sh
@@ -1,50 +1,50 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ $(basename $(pwd)) != nbapi-docgen ]; then
+if [ "$(basename "$(pwd)")" != "nbapi-docgen" ]; then
     echo "Please execute this file from its own directory"
     exit
 fi
 
-# Dependency: yajl
 # Make a build dir
-mkdir -p build
+mkdir -p build/usr/lib
 # amx components look for input variables (include files, libs) in a "staging dir" ...
-export STAGINGDIR=$(cd build && pwd)
+STAGINGDIR="$(cd build && pwd)"
 # ... and they put their files in a DEST dir. We need those to be the same
-export DEST=$STAGINGDIR
+DEST="$STAGINGDIR"
 # amx components try to install libs to machine-dependent dirs, but then don't include those directories in their linker paths ...
-export MACHINE= 
+MACHINE=
+# This is needed so that the amx libraries are found during execution of amxo-cg and amxo-xml-to
+LD_LIBRARY_PATH="${STAGINGDIR}/usr/lib"
+
+export STAGINGDIR DEST MACHINE LD_LIBRARY_PATH
 
 for COMPONENT in libamxc libamxj libamxp libamxd libamxo amxo-cg amxo-xml-to; do
-    if [ -d $COMPONENT ]; then 
-        #rm -rf $COMPONENT;
-        (cd $COMPONENT && git pull)
-    else
-        #mkdir $COMPONENT; 
+    if [ ! -d $COMPONENT ]; then
         if [ ${COMPONENT:0:3} == "lib" ]; then
-            git clone --progress --single-branch git@gitlab.com:prpl-foundation/components/ambiorix/libraries/$COMPONENT $COMPONENT
+            git clone --single-branch https://gitlab.com/prpl-foundation/components/ambiorix/libraries/$COMPONENT $COMPONENT
         else
-            git clone --progress --single-branch git@gitlab.com:prpl-foundation/components/ambiorix/applications/$COMPONENT $COMPONENT
+            git clone --single-branch https://gitlab.com/prpl-foundation/components/ambiorix/applications/$COMPONENT $COMPONENT
         fi
     fi
-    pushd $COMPONENT;
+    pushd $COMPONENT || exit 1
     if [ -f ../patches/${COMPONENT}.patch ]; then
         echo "*** running git apply ***"
         git apply ../patches/${COMPONENT}.patch;
     fi
     make && make install;
-    popd;
+    popd || exit 1
 done
 
-export LD_LIBRARY_PATH=${STAGINGDIR}/usr/lib
 ./build/usr/bin/amxo-cg -G xml ../../controller/nbapi/odl/controller.odl
 if [ ! -r controller.odl.xml ]; then
-    echo -e "\033[1;31mXML generation failed -- ODL syntax issue?\033[0m"
-    exit
+    echo -e "\\033[1;31mXML generation failed -- ODL syntax issue?\\033[0m"
+    exit 1
 fi
+# install conversion files
+ln -s "$(pwd)/build/etc/amx" /etc/amx
 mkdir -p build/html
-pushd build/html
+pushd build/html || exit 1
 ../usr/bin/amxo-xml-to -x html ../../controller.odl.xml
-popd
+popd || exit 1
 zip -r ppm_controller_nbapi_docs.zip build/html
 
