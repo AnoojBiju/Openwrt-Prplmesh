a2d2c1f29e4c69eb8789821263259d7b24eca560
Frederik Van Bogaert <frederik.vanbogaert@mind.be>
2021-12-02 18:00:59 +0100

    Use ambiorix tools from the prplmesh builder
    Signed-off-by: Frederik Van Bogaert <frederik.vanbogaert@mind.be>

--- tools/nbapi-docgen/parse_api.sh
+++ tools/nbapi-docgen/parse_api.sh
@@ -1,46 +1,20 @@
 #!/bin/bash
 
-if [ "$(basename "$(pwd)")" != "nbapi-docgen" ]; then
-    echo "Please execute this file from its own directory"
-    exit
-fi
+scriptdir="$(cd "${0%/*}" || exit 1; pwd)"
+rootdir="${scriptdir%/*/*}"
 
-# Make a build dir
-mkdir -p build/usr/lib
-# amx components look for input variables (include files, libs) in a "staging dir" ...
-STAGINGDIR="$(cd build && pwd)"
-# ... and they put their files in a DEST dir. We need those to be the same
-DEST="$STAGINGDIR"
-# amx components try to install libs to machine-dependent dirs, but then don't include those directories in their linker paths ...
-MACHINE=
-# This is needed so that the amx libraries are found during execution of amxo-cg and amxo-xml-to
-LD_LIBRARY_PATH="${STAGINGDIR}/usr/lib"
+[ -z "$VERSION" ] && VERSION="custom-$(date -Iminutes)"
 
-export STAGINGDIR DEST MACHINE LD_LIBRARY_PATH
-
-for COMPONENT in libamxc libamxj libamxp libamxd libamxo amxo-cg amxo-xml-to; do
-    if [ ! -d $COMPONENT ]; then
-        if [ ${COMPONENT:0:3} == "lib" ]; then
-            git clone --single-branch https://gitlab.com/prpl-foundation/components/ambiorix/libraries/$COMPONENT $COMPONENT
-        else
-            git clone --single-branch https://gitlab.com/prpl-foundation/components/ambiorix/applications/$COMPONENT $COMPONENT
-        fi
-    fi
-    pushd $COMPONENT || exit 1
-    make && make install;
-    popd || exit 1
-done
-
-./build/usr/bin/amxo-cg -G xml ../../controller/nbapi/odl/controller.odl
+amxo-cg -G xml "${rootdir}/controller/nbapi/odl/controller.odl"
 if [ ! -r controller.odl.xml ]; then
     echo -e "\\033[1;31mXML generation failed -- ODL syntax issue?\\033[0m"
     exit 1
 fi
-# install conversion files
-ln -s "$(pwd)/build/etc/amx" /etc/amx
-mkdir -p build/html
-pushd build/html || exit 1
-../usr/bin/amxo-xml-to -x html ../../controller.odl.xml
-popd || exit 1
-zip -r ppm_controller_nbapi_docs.zip build/html
 
+mkdir -p "${rootdir}/build/html"
+amxo-xml-to -x html\
+                  -o output-dir="${rootdir}/build/html"\
+                  -o title="prplMesh"\
+                  -o sub-title="Northbound API"\
+                  -o version="$VERSION"\
+                  controller.odl.xml
