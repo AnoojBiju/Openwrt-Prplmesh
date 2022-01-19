a24f79e65d27819824bbd104fb14b11e115f50c1
Frederik Van Bogaert <frederik.vanbogaert@mind.be>
2022-01-19 15:23:28 +0100

    Try prplOS with SAH components for prplMesh build
    Since the SAH packages are now part of feed-prpl (different versions)
    we should not add the "sah" profile anymore.
    
    Signed-off-by: Frederik Van Bogaert <frederik.vanbogaert@mind.be>
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -185,7 +185,7 @@ main() {
 VERBOSE=false
 IMAGE_ONLY=false
 OPENWRT_REPOSITORY='https://gitlab.com/prpl-foundation/prplos/prplos.git'
-OPENWRT_VERSION='8a79f25586d69a8e181db8bd5bff3b24fac9ef05'
+OPENWRT_VERSION='fdbd47fadd0601680422e8feacf482b9261eacc4'
 PRPLMESH_VARIANT="-nl80211"
 DOCKER_TARGET_STAGE="prplmesh-builder"
 SHELL_ONLY=false
