e3452feb4a865c42bdf77f89d02ff01ad1c879f5
Ahmed Amine HAOUES <ahmed-amine.haoues_ext@softathome.com>
2022-02-19 23:35:06 +0100

    bwl: nl80211: create derivated class wpa_ctrl_socket_event
    New feature:
    - Add new class to manage wpa_ctrl socket for receiving events from
    hostapd/wpa_supplicant deamons
    - This class derivate from base class wpa_ctrl_socket
    - It implements connection/disconnection
    as socket open+attach / detach+close
    - It defines methods to check pending data and read it
    - It defines methods to update a file descriptor list with the socket fd
    to be monitored by the event loop
    
    https://jira.prplfoundation.org/browse/PPM-1714
    
    Signed-off-by: Ahmed Amine HAOUES <ahmed-amine.haoues_ext@softathome.com>

--- common/beerocks/bwl/nl80211/wpa_ctrl_client.cpp
+++ common/beerocks/bwl/nl80211/wpa_ctrl_client.cpp
@@ -93,5 +93,147 @@ bool wpa_ctrl_socket_cmd::request(const std::string &cmd, char *buffer, size_t b
     return true;
 }
 
+wpa_ctrl_socket_event::wpa_ctrl_socket_event(const std::string &path, std::vector<int> &event_fds)
+    : wpa_ctrl_socket(path), m_attached(false), m_event_fds(event_fds)
+{
+}
+
+wpa_ctrl_socket_event::~wpa_ctrl_socket_event() { close(); }
+
+bool wpa_ctrl_socket_event::open()
+{
+    if (wpa_ctrl_socket::open()) {
+        add_event_fd(fd());
+        return true;
+    }
+    return false;
+}
+
+bool wpa_ctrl_socket_event::connect() { return (open() && attach()); }
+
+bool wpa_ctrl_socket_event::disconnect(bool force)
+{
+    if (detach() || force) {
+        m_attached = false;
+        del_event_fd(fd());
+        wpa_ctrl_socket::close();
+        return true;
+    }
+    return false;
+}
+
+void wpa_ctrl_socket_event::close() { disconnect(true); }
+
+bool wpa_ctrl_socket_event::attach()
+{
+    if (!m_ctx) {
+        LOG(ERROR) << "Control socket not available!";
+        return false;
+    }
+    if (m_attached) {
+        return true;
+    }
+
+    // Attach to the control interface for events receiving
+    int result;
+    int try_cnt = 0;
+    do {
+        result = wpa_ctrl_attach(m_ctx);
+
+        // return values: 0 on success, -1 on failure, -2 on timeout
+    } while (result == -2 && ++try_cnt < WPA_CTRL_READ_RETRY_MAX);
+    if (result != 0) {
+        LOG(DEBUG) << "wpa_ctrl_attach() failed, ctrl_iface_path: " << path() << " res: " << result;
+        return false;
+    }
+    m_attached = true;
+    LOG(DEBUG) << "attached for event over " << path();
+    return true;
+}
+
+bool wpa_ctrl_socket_event::detach()
+{
+    if (!m_ctx) {
+        return true;
+    }
+    if (!m_attached) {
+        return true;
+    }
+
+    // Detach to the control interface for events receiving
+    int result;
+    int try_cnt = 0;
+    do {
+        result = wpa_ctrl_detach(m_ctx);
+
+        // return values: 0 on success, -1 on failure, -2 on timeout
+    } while (result == -2 && ++try_cnt < WPA_CTRL_READ_RETRY_MAX);
+    if (result != 0) {
+        LOG(DEBUG) << "wpa_ctrl_detach() failed, ctrl_iface_path: " << path() << " res: " << result;
+        return false;
+    }
+    m_attached = false;
+    LOG(DEBUG) << "detached for event over " << path();
+    return true;
+}
+
+bool wpa_ctrl_socket_event::pending() { return (m_ctx && m_attached && wpa_ctrl_pending(m_ctx)); }
+
+bool wpa_ctrl_socket_event::receive(char *buffer, size_t buff_size)
+{
+    if (!m_ctx) {
+        LOG(ERROR) << "Control socket not available!";
+        return false;
+    }
+    auto buff_size_copy = buff_size;
+    int result          = wpa_ctrl_recv(m_ctx, buffer, &buff_size_copy);
+    if (result < 0) {
+        LOG(ERROR) << "wpa_ctrl_recv() failed!";
+        return false;
+    }
+    if (buff_size_copy >= buff_size) {
+        LOG(ERROR) << "wpa_ctrl_recv returned reply of size " << buff_size_copy;
+        return false;
+    }
+
+    // the wpa_ctrl does not put null terminator at the and of the string
+    buffer[buff_size_copy] = 0;
+    return true;
+}
+
+void wpa_ctrl_socket_event::add_event_fd(int fd)
+{
+    if (fd <= 0) {
+        return;
+    }
+    auto itReplace = m_event_fds.end();
+    for (auto it = std::begin(m_event_fds); it != std::end(m_event_fds); ++it) {
+        if (*it == fd) {
+            return;
+        }
+        if (*it == -1) {
+            itReplace = it;
+        }
+    }
+
+    // replace first invalid fd (-1) or append the new fd value
+    if (itReplace != m_event_fds.end()) {
+        *itReplace = fd;
+    } else {
+        m_event_fds.push_back(fd);
+    }
+}
+
+void wpa_ctrl_socket_event::del_event_fd(int fd)
+{
+    if (fd <= 0) {
+        return;
+    }
+    auto it = std::find(m_event_fds.begin(), m_event_fds.end(), fd);
+    if (it != m_event_fds.end()) {
+        m_event_fds.erase(it);
+    }
+}
+
 } // namespace nl80211
 } // namespace bwl
--- common/beerocks/bwl/nl80211/wpa_ctrl_client.h
+++ common/beerocks/bwl/nl80211/wpa_ctrl_client.h
@@ -174,6 +174,134 @@ public:
     bool request(const std::string &cmd, char *buffer, size_t buff_size);
 };
 
+/**
+ * @brief WPA Ctrl Event socket Class.
+ *
+ * This class derives from Base wpa_ctrl_socket class, and it is exclusively used for
+ * receiving unsolicited events from hostapd/wpa_supplicant daemon
+ */
+class wpa_ctrl_socket_event : public wpa_ctrl_socket {
+public:
+    /**
+     * @brief Constructor wpa_ctrl event socket object.
+     *
+     * @param[in] path The WPA Ctrl socket file path
+     * @param[in] event_fds The list of file descriptors to be updated
+     * for event monitoring
+     */
+    wpa_ctrl_socket_event(const std::string &path, std::vector<int> &event_fds);
+
+    /**
+     * @brief Custom destructor of wpa_ctrl event socket object.
+     * It enforces socket disconnection.
+     */
+    virtual ~wpa_ctrl_socket_event();
+
+    /**
+     * @brief Opens and attach event socket to hostapd/wpa_supplicant deamon.
+     *
+     * @return True if socket is open and attached and false otherwise.
+     */
+    virtual bool connect() override;
+
+    /**
+     * @brief Detach and close event socket to hostapd/wpa_supplicant deamon.
+     *
+     * @param[in] force Enforce socket closing
+     *
+     * @return True if socket is detached and close (unless closing is enforced)
+     * and false otherwise.
+     */
+    virtual bool disconnect(bool force = false) override;
+
+    /**
+     * @brief Checks whether the socket has pending data to be read.
+     *
+     * @return True if socket was attached and has pending data.
+     */
+    bool pending();
+
+    /**
+     * @brief Read available data on wpa_ctrl socket.
+     *
+     * @param[in/out] buffer Buffer data to be filled wit received data.
+     * @param[in] buff_size Maximum data size to be saved in the buffer.
+     *
+     * @return True on success and false if:
+     * - socket is not opened.
+     * - or reading has failed or time outed.
+     * - or received data size exceeds the buffer size .
+     */
+    bool receive(char *buffer, size_t buff_size);
+
+private:
+    /**
+     * This boolean indicates if wpa_ctrl socket is actually attached
+     * (subscribed) for receiving unsolicited events from daemon.
+     */
+    bool m_attached;
+
+    /**
+     * Reference to event queues list , with all monitored file descriptors.
+     * This list is updated upon opening or closing eventing socket.
+     */
+    std::vector<int> &m_event_fds;
+
+    /**
+     * @brief Adds wpa_ctrl event socket to monitored event queues list.
+     * If the file descriptor value is new (not found), it is set at first empty entry
+     * (i.e equals -1) , or appended to the list.
+     *
+     * @param[in] fd File descriptor to be added in the list.
+     *
+     * @return void
+     */
+    void add_event_fd(int fd);
+
+    /**
+     * @brief Removes wpa_ctrl event socket from monitored event queues list.
+     *
+     * @param[in] fd File descriptor to be removed in the list.
+     *
+     * @return void
+     */
+    void del_event_fd(int fd);
+
+    /**
+     * @brief Opens wpa_ctrl event socket
+     * and add the new file descriptor to event monitoring FDs queue.
+     *
+     * @return True if socket is open, false otherwise.
+     */
+    virtual bool open() override;
+
+    /**
+     * @brief Closes wpa_ctrl event socket
+     * and remove the relative file descriptor from event monitoring FDs queue.
+     *
+     * @return void.
+     */
+    virtual void close() override;
+
+    /**
+     * @brief Attach the wpa_ctrl socket (i.e subscribe client)
+     * to be notified with events from hostapd/wpa_supplicant daemon.
+     *
+     * @return True on success and when already attached.
+     * false otherwise.
+     */
+    bool attach();
+
+    /**
+     * @brief Detach the wpa_ctrl socket (i.e unsubscribe client)
+     * to stop receiving events from hostapd/wpa_supplicant daemon.
+     *
+     * @return True on success and when detached or closed.
+     * false otherwise.
+     */
+    bool detach();
+};
+
 } // namespace nl80211
 } // namespace bwl
 
