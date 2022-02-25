df735fb3435e9a94ce0a89e51893cbfc09dcb846
anantkumar <cchavda@maxlinear.com>
2022-02-25 12:04:52 +0200

    bwl: dwpald client connection to dwpald server
    dwpald_connect is used to register new client to dwpald server.
    client connection is per process.
    
    fronthaul has ap_manager and monitor which respecively ap_wlan_hal and
    mon_wlan_hal instance, but as process we can connect once only, and
    order of instance createion is ap_wlan_hal then mon_wlan_hal, we use
    dwpald_connect in ap_wlan_hal instance creation and no need to do again
    from mon_wlan_hal as it is in same fronthual process.
    
    backhaul manager has sta_wlan_hal instance so use dwpald_connect in
    sta_wlan_hal instace.
    
    For each client connection , client must call dwpald_start_listener in
    order to communicate with server, this API also to be used once per process.
    So, dwpald_start_listener is called from ap_wlan_hal and sta_wlan_hal.
    
    With this commit, dwpald client is set to communicate with dwpald
    server.
    
    Following commit to do attach for hostap/driver/kernel events to dwpald
    server.
    
    bwl: dwpald_attach implmentation
    
    dwpald_attach is been called to register to hostap and nl event handler
    to dwpald.
    
    dwpald_hostap_attach as can be called only once, been kept in
    ap_wlan_hal_dwpald and sta_wlan_hal_dwpald.
    
    As monitor is part of fronthaul process which already has
    ap_wlan_hal_dwpal instance, in order to register event handler use
    dwpald_hostap_attach_with_id and dwpald_nl_drv_attach_with_id.
    
    dwpald provides event filter ie prplMesh is going to get receive event handler
    only for registered events.
    
    PPM-1947
    
    Signed-off-by: anantkumar <cchavda@maxlinear.com>

--- common/beerocks/bwl/dwpald/base_wlan_hal_dwpald.h
+++ common/beerocks/bwl/dwpald/base_wlan_hal_dwpald.h
@@ -19,6 +19,7 @@
 
 extern "C" {
 #include <dwpal.h>
+#include <dwpald_client.h>
 }
 
 #include <chrono>
@@ -86,6 +87,7 @@ protected:
     // Process dwpal event
     virtual bool process_dwpal_event(char *buffer, int bufLen, const std::string &opcode) = 0;
     virtual bool process_dwpal_nl_event(struct nl_msg *msg, void *arg = nullptr)          = 0;
+    virtual bool dwpald_attach(char *ifname)                                              = 0;
 
     bool set(const std::string &param, const std::string &value,
              int vap_id = beerocks::IFACE_RADIO_ID);
