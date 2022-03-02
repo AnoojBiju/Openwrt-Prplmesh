25713fe66672fd386c8a08b017a8a36867b9635c
Cihangir Odabas <codabas@assia-inc.com>
2022-03-02 11:54:49 +0300

    controller: update bss clearing with new keep new method
    Clearing all bsses leads to unwanted results,
    like leaving zombie datamodels, etc.
    
    So, only remove unreported bsses from the radio object with
    keep_new_prepare() and keep_new_remove_old() methods.
    
    https://jira.prplfoundation.org/browse/PPM-1762
    --
    Signed-off-by: Cihangir Odabas <codabas@assia-inc.com>

--- controller/src/beerocks/master/controller.cpp
+++ controller/src/beerocks/master/controller.cpp
@@ -3007,7 +3007,8 @@ bool Controller::handle_cmdu_control_message(
             LOG(ERROR) << "No radio found for radio_uid " << radio_mac << " on " << src_mac;
             break;
         }
-        radio->bsses.clear();
+
+        radio->bsses.keep_new_prepare();
 
         std::unordered_map<int8_t, sVapElement> vaps_info;
         std::string vaps_list;
@@ -3042,6 +3043,20 @@ bool Controller::handle_cmdu_control_message(
         }
 
         database.set_hostap_vap_list(radio_mac, vaps_info);
+
+        auto removed = radio->bsses.keep_new_remove_old();
+        for (const auto &bss : removed) {
+
+            // Remove all clients from that vap
+            auto client_list =
+                database.get_node_children(tlvf::mac_to_string(bss->bssid), beerocks::TYPE_CLIENT);
+            for (auto &client : client_list) {
+                son_actions::handle_dead_node(client, true, database, cmdu_tx, tasks);
+            }
+
+            // Remove the vap from DB
+            database.remove_vap(*radio, *bss);
+        }
 
         // update bml listeners
         bml_task::connection_change_event new_event;
