b30be8720c5b03bf144a56cac5589b32488913d5
anantkumar <cchavda@maxlinear.com>
2022-05-18 12:41:59 +0300

    controller: check whether to add gateware or ire node
    currently when autoconfig search message is received with profile2 tlv,
    node is been added as ire.
    
    But if agent al_mac is same as local_bridge_mac then node is to be
    added as gateway node.
    
    Signed-off-by: anantkumar <cchavda@maxlinear.com>

--- controller/src/beerocks/master/controller.cpp
+++ controller/src/beerocks/master/controller.cpp
@@ -703,8 +703,12 @@ bool Controller::handle_cmdu_1905_autoconfiguration_search(const sMacAddr &src_m
     LOG(DEBUG) << "sending autoconfig response message";
 
     if (tlvProfile2MultiApProfileAgent) {
-
-        auto agent = database.add_node_ire(al_mac);
+        std::shared_ptr<Agent> agent;
+        if (database.get_local_bridge_mac() == al_mac) {
+            agent = database.add_node_gateway(al_mac);
+        } else {
+            agent = database.add_node_ire(al_mac);
+        }
         if (!agent) {
             LOG(ERROR) << "Failed adding agent: " << al_mac;
             return false;
