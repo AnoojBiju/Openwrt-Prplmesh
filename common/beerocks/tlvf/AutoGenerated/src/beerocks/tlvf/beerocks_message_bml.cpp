9d0080547981ccb8e7594a061f7d61dc2c54ee5f
Itay Elenzweig <ielenzweig@maxlinear.com>
2022-06-01 14:31:14 +0300

    yaml: Add Get/Set selection channel-pool CMDUs
    To support the Auto-Channel-Selection feature as part of PPM-1973 we
    need to be able to change the default channel pool used in the ACS scan.
    
    Add Request & Response CMDUs to Get the Channel Pool.
    Add Request & Response CMDUs to Set the Channel Pool.
    
    Signed-off-by: Itay Elenzweig <ielenzweig@maxlinear.com>

--- common/beerocks/tlvf/AutoGenerated/src/beerocks/tlvf/beerocks_message_bml.cpp
+++ common/beerocks/tlvf/AutoGenerated/src/beerocks/tlvf/beerocks_message_bml.cpp
@@ -8056,4 +8056,356 @@ bool cACTION_BML_GET_CLIENT_ROAMING_11K_SUPPORT_RESPONSE::init()
     return true;
 }
 
+cACTION_BML_SET_SELECTION_CHANNEL_POOL_REQUEST::cACTION_BML_SET_SELECTION_CHANNEL_POOL_REQUEST(uint8_t* buff, size_t buff_len, bool parse) :
+    BaseClass(buff, buff_len, parse) {
+    m_init_succeeded = init();
+}
+cACTION_BML_SET_SELECTION_CHANNEL_POOL_REQUEST::cACTION_BML_SET_SELECTION_CHANNEL_POOL_REQUEST(std::shared_ptr<BaseClass> base, bool parse) :
+BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse){
+    m_init_succeeded = init();
+}
+cACTION_BML_SET_SELECTION_CHANNEL_POOL_REQUEST::~cACTION_BML_SET_SELECTION_CHANNEL_POOL_REQUEST() {
+}
+sMacAddr& cACTION_BML_SET_SELECTION_CHANNEL_POOL_REQUEST::radio_mac() {
+    return (sMacAddr&)(*m_radio_mac);
+}
+
+int8_t& cACTION_BML_SET_SELECTION_CHANNEL_POOL_REQUEST::channel_pool_size() {
+    return (int8_t&)(*m_channel_pool_size);
+}
+
+uint8_t* cACTION_BML_SET_SELECTION_CHANNEL_POOL_REQUEST::channel_pool(size_t idx) {
+    if ( (m_channel_pool_idx__ == 0) || (m_channel_pool_idx__ <= idx) ) {
+        TLVF_LOG(ERROR) << "Requested index is greater than the number of available entries";
+        return nullptr;
+    }
+    return &(m_channel_pool[idx]);
+}
+
+bool cACTION_BML_SET_SELECTION_CHANNEL_POOL_REQUEST::set_channel_pool(const void* buffer, size_t size) {
+    if (buffer == nullptr) {
+        TLVF_LOG(WARNING) << "set_channel_pool received a null pointer.";
+        return false;
+    }
+    if (size > beerocks::message::SUPPORTED_CHANNELS_LENGTH) {
+        TLVF_LOG(ERROR) << "Received buffer size is smaller than buffer length";
+        return false;
+    }
+    std::copy_n(reinterpret_cast<const uint8_t *>(buffer), size, m_channel_pool);
+    return true;
+}
+void cACTION_BML_SET_SELECTION_CHANNEL_POOL_REQUEST::class_swap()
+{
+    tlvf_swap(8*sizeof(eActionOp_BML), reinterpret_cast<uint8_t*>(m_action_op));
+    m_radio_mac->struct_swap();
+}
+
+bool cACTION_BML_SET_SELECTION_CHANNEL_POOL_REQUEST::finalize()
+{
+    if (m_parse__) {
+        TLVF_LOG(DEBUG) << "finalize() called but m_parse__ is set";
+        return true;
+    }
+    if (m_finalized__) {
+        TLVF_LOG(DEBUG) << "finalize() called for already finalized class";
+        return true;
+    }
+    if (!isPostInitSucceeded()) {
+        TLVF_LOG(ERROR) << "post init check failed";
+        return false;
+    }
+    if (m_inner__) {
+        if (!m_inner__->finalize()) {
+            TLVF_LOG(ERROR) << "m_inner__->finalize() failed";
+            return false;
+        }
+        auto tailroom = m_inner__->getMessageBuffLength() - m_inner__->getMessageLength();
+        m_buff_ptr__ -= tailroom;
+    }
+    class_swap();
+    m_finalized__ = true;
+    return true;
+}
+
+size_t cACTION_BML_SET_SELECTION_CHANNEL_POOL_REQUEST::get_initial_size()
+{
+    size_t class_size = 0;
+    class_size += sizeof(sMacAddr); // radio_mac
+    class_size += sizeof(int8_t); // channel_pool_size
+    class_size += beerocks::message::SUPPORTED_CHANNELS_LENGTH * sizeof(uint8_t); // channel_pool
+    return class_size;
+}
+
+bool cACTION_BML_SET_SELECTION_CHANNEL_POOL_REQUEST::init()
+{
+    if (getBuffRemainingBytes() < get_initial_size()) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    m_radio_mac = reinterpret_cast<sMacAddr*>(m_buff_ptr__);
+    if (!buffPtrIncrementSafe(sizeof(sMacAddr))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(sMacAddr) << ") Failed!";
+        return false;
+    }
+    if (!m_parse__) { m_radio_mac->struct_init(); }
+    m_channel_pool_size = reinterpret_cast<int8_t*>(m_buff_ptr__);
+    if (!m_parse__) *m_channel_pool_size = 0x0;
+    if (!buffPtrIncrementSafe(sizeof(int8_t))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(int8_t) << ") Failed!";
+        return false;
+    }
+    m_channel_pool = reinterpret_cast<uint8_t*>(m_buff_ptr__);
+    if (!buffPtrIncrementSafe(sizeof(uint8_t) * (beerocks::message::SUPPORTED_CHANNELS_LENGTH))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(uint8_t) * (beerocks::message::SUPPORTED_CHANNELS_LENGTH) << ") Failed!";
+        return false;
+    }
+    m_channel_pool_idx__  = beerocks::message::SUPPORTED_CHANNELS_LENGTH;
+    if (m_parse__) { class_swap(); }
+    return true;
+}
+
+cACTION_BML_SET_SELECTION_CHANNEL_POOL_RESPONSE::cACTION_BML_SET_SELECTION_CHANNEL_POOL_RESPONSE(uint8_t* buff, size_t buff_len, bool parse) :
+    BaseClass(buff, buff_len, parse) {
+    m_init_succeeded = init();
+}
+cACTION_BML_SET_SELECTION_CHANNEL_POOL_RESPONSE::cACTION_BML_SET_SELECTION_CHANNEL_POOL_RESPONSE(std::shared_ptr<BaseClass> base, bool parse) :
+BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse){
+    m_init_succeeded = init();
+}
+cACTION_BML_SET_SELECTION_CHANNEL_POOL_RESPONSE::~cACTION_BML_SET_SELECTION_CHANNEL_POOL_RESPONSE() {
+}
+uint8_t& cACTION_BML_SET_SELECTION_CHANNEL_POOL_RESPONSE::success() {
+    return (uint8_t&)(*m_success);
+}
+
+void cACTION_BML_SET_SELECTION_CHANNEL_POOL_RESPONSE::class_swap()
+{
+    tlvf_swap(8*sizeof(eActionOp_BML), reinterpret_cast<uint8_t*>(m_action_op));
+}
+
+bool cACTION_BML_SET_SELECTION_CHANNEL_POOL_RESPONSE::finalize()
+{
+    if (m_parse__) {
+        TLVF_LOG(DEBUG) << "finalize() called but m_parse__ is set";
+        return true;
+    }
+    if (m_finalized__) {
+        TLVF_LOG(DEBUG) << "finalize() called for already finalized class";
+        return true;
+    }
+    if (!isPostInitSucceeded()) {
+        TLVF_LOG(ERROR) << "post init check failed";
+        return false;
+    }
+    if (m_inner__) {
+        if (!m_inner__->finalize()) {
+            TLVF_LOG(ERROR) << "m_inner__->finalize() failed";
+            return false;
+        }
+        auto tailroom = m_inner__->getMessageBuffLength() - m_inner__->getMessageLength();
+        m_buff_ptr__ -= tailroom;
+    }
+    class_swap();
+    m_finalized__ = true;
+    return true;
+}
+
+size_t cACTION_BML_SET_SELECTION_CHANNEL_POOL_RESPONSE::get_initial_size()
+{
+    size_t class_size = 0;
+    class_size += sizeof(uint8_t); // success
+    return class_size;
+}
+
+bool cACTION_BML_SET_SELECTION_CHANNEL_POOL_RESPONSE::init()
+{
+    if (getBuffRemainingBytes() < get_initial_size()) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    m_success = reinterpret_cast<uint8_t*>(m_buff_ptr__);
+    if (!buffPtrIncrementSafe(sizeof(uint8_t))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(uint8_t) << ") Failed!";
+        return false;
+    }
+    if (m_parse__) { class_swap(); }
+    return true;
+}
+
+cACTION_BML_GET_SELECTION_CHANNEL_POOL_REQUEST::cACTION_BML_GET_SELECTION_CHANNEL_POOL_REQUEST(uint8_t* buff, size_t buff_len, bool parse) :
+    BaseClass(buff, buff_len, parse) {
+    m_init_succeeded = init();
+}
+cACTION_BML_GET_SELECTION_CHANNEL_POOL_REQUEST::cACTION_BML_GET_SELECTION_CHANNEL_POOL_REQUEST(std::shared_ptr<BaseClass> base, bool parse) :
+BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse){
+    m_init_succeeded = init();
+}
+cACTION_BML_GET_SELECTION_CHANNEL_POOL_REQUEST::~cACTION_BML_GET_SELECTION_CHANNEL_POOL_REQUEST() {
+}
+sMacAddr& cACTION_BML_GET_SELECTION_CHANNEL_POOL_REQUEST::radio_mac() {
+    return (sMacAddr&)(*m_radio_mac);
+}
+
+void cACTION_BML_GET_SELECTION_CHANNEL_POOL_REQUEST::class_swap()
+{
+    tlvf_swap(8*sizeof(eActionOp_BML), reinterpret_cast<uint8_t*>(m_action_op));
+    m_radio_mac->struct_swap();
+}
+
+bool cACTION_BML_GET_SELECTION_CHANNEL_POOL_REQUEST::finalize()
+{
+    if (m_parse__) {
+        TLVF_LOG(DEBUG) << "finalize() called but m_parse__ is set";
+        return true;
+    }
+    if (m_finalized__) {
+        TLVF_LOG(DEBUG) << "finalize() called for already finalized class";
+        return true;
+    }
+    if (!isPostInitSucceeded()) {
+        TLVF_LOG(ERROR) << "post init check failed";
+        return false;
+    }
+    if (m_inner__) {
+        if (!m_inner__->finalize()) {
+            TLVF_LOG(ERROR) << "m_inner__->finalize() failed";
+            return false;
+        }
+        auto tailroom = m_inner__->getMessageBuffLength() - m_inner__->getMessageLength();
+        m_buff_ptr__ -= tailroom;
+    }
+    class_swap();
+    m_finalized__ = true;
+    return true;
+}
+
+size_t cACTION_BML_GET_SELECTION_CHANNEL_POOL_REQUEST::get_initial_size()
+{
+    size_t class_size = 0;
+    class_size += sizeof(sMacAddr); // radio_mac
+    return class_size;
+}
+
+bool cACTION_BML_GET_SELECTION_CHANNEL_POOL_REQUEST::init()
+{
+    if (getBuffRemainingBytes() < get_initial_size()) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    m_radio_mac = reinterpret_cast<sMacAddr*>(m_buff_ptr__);
+    if (!buffPtrIncrementSafe(sizeof(sMacAddr))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(sMacAddr) << ") Failed!";
+        return false;
+    }
+    if (!m_parse__) { m_radio_mac->struct_init(); }
+    if (m_parse__) { class_swap(); }
+    return true;
+}
+
+cACTION_BML_GET_SELECTION_CHANNEL_POOL_RESPONSE::cACTION_BML_GET_SELECTION_CHANNEL_POOL_RESPONSE(uint8_t* buff, size_t buff_len, bool parse) :
+    BaseClass(buff, buff_len, parse) {
+    m_init_succeeded = init();
+}
+cACTION_BML_GET_SELECTION_CHANNEL_POOL_RESPONSE::cACTION_BML_GET_SELECTION_CHANNEL_POOL_RESPONSE(std::shared_ptr<BaseClass> base, bool parse) :
+BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse){
+    m_init_succeeded = init();
+}
+cACTION_BML_GET_SELECTION_CHANNEL_POOL_RESPONSE::~cACTION_BML_GET_SELECTION_CHANNEL_POOL_RESPONSE() {
+}
+uint8_t& cACTION_BML_GET_SELECTION_CHANNEL_POOL_RESPONSE::success() {
+    return (uint8_t&)(*m_success);
+}
+
+int8_t& cACTION_BML_GET_SELECTION_CHANNEL_POOL_RESPONSE::channel_pool_size() {
+    return (int8_t&)(*m_channel_pool_size);
+}
+
+uint8_t* cACTION_BML_GET_SELECTION_CHANNEL_POOL_RESPONSE::channel_pool(size_t idx) {
+    if ( (m_channel_pool_idx__ == 0) || (m_channel_pool_idx__ <= idx) ) {
+        TLVF_LOG(ERROR) << "Requested index is greater than the number of available entries";
+        return nullptr;
+    }
+    return &(m_channel_pool[idx]);
+}
+
+bool cACTION_BML_GET_SELECTION_CHANNEL_POOL_RESPONSE::set_channel_pool(const void* buffer, size_t size) {
+    if (buffer == nullptr) {
+        TLVF_LOG(WARNING) << "set_channel_pool received a null pointer.";
+        return false;
+    }
+    if (size > beerocks::message::SUPPORTED_CHANNELS_LENGTH) {
+        TLVF_LOG(ERROR) << "Received buffer size is smaller than buffer length";
+        return false;
+    }
+    std::copy_n(reinterpret_cast<const uint8_t *>(buffer), size, m_channel_pool);
+    return true;
+}
+void cACTION_BML_GET_SELECTION_CHANNEL_POOL_RESPONSE::class_swap()
+{
+    tlvf_swap(8*sizeof(eActionOp_BML), reinterpret_cast<uint8_t*>(m_action_op));
+}
+
+bool cACTION_BML_GET_SELECTION_CHANNEL_POOL_RESPONSE::finalize()
+{
+    if (m_parse__) {
+        TLVF_LOG(DEBUG) << "finalize() called but m_parse__ is set";
+        return true;
+    }
+    if (m_finalized__) {
+        TLVF_LOG(DEBUG) << "finalize() called for already finalized class";
+        return true;
+    }
+    if (!isPostInitSucceeded()) {
+        TLVF_LOG(ERROR) << "post init check failed";
+        return false;
+    }
+    if (m_inner__) {
+        if (!m_inner__->finalize()) {
+            TLVF_LOG(ERROR) << "m_inner__->finalize() failed";
+            return false;
+        }
+        auto tailroom = m_inner__->getMessageBuffLength() - m_inner__->getMessageLength();
+        m_buff_ptr__ -= tailroom;
+    }
+    class_swap();
+    m_finalized__ = true;
+    return true;
+}
+
+size_t cACTION_BML_GET_SELECTION_CHANNEL_POOL_RESPONSE::get_initial_size()
+{
+    size_t class_size = 0;
+    class_size += sizeof(uint8_t); // success
+    class_size += sizeof(int8_t); // channel_pool_size
+    class_size += beerocks::message::SUPPORTED_CHANNELS_LENGTH * sizeof(uint8_t); // channel_pool
+    return class_size;
+}
+
+bool cACTION_BML_GET_SELECTION_CHANNEL_POOL_RESPONSE::init()
+{
+    if (getBuffRemainingBytes() < get_initial_size()) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    m_success = reinterpret_cast<uint8_t*>(m_buff_ptr__);
+    if (!buffPtrIncrementSafe(sizeof(uint8_t))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(uint8_t) << ") Failed!";
+        return false;
+    }
+    m_channel_pool_size = reinterpret_cast<int8_t*>(m_buff_ptr__);
+    if (!m_parse__) *m_channel_pool_size = 0x0;
+    if (!buffPtrIncrementSafe(sizeof(int8_t))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(int8_t) << ") Failed!";
+        return false;
+    }
+    m_channel_pool = reinterpret_cast<uint8_t*>(m_buff_ptr__);
+    if (!buffPtrIncrementSafe(sizeof(uint8_t) * (beerocks::message::SUPPORTED_CHANNELS_LENGTH))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(uint8_t) * (beerocks::message::SUPPORTED_CHANNELS_LENGTH) << ") Failed!";
+        return false;
+    }
+    m_channel_pool_idx__  = beerocks::message::SUPPORTED_CHANNELS_LENGTH;
+    if (m_parse__) { class_swap(); }
+    return true;
+}
+
 
