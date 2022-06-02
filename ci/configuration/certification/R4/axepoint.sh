81532b2e503f8b4cf229eef5e153e8d067eb9051
Raphaël Mélotte <raphael.melotte@mind.be>
2022-06-02 14:26:25 +0200

    ci: configuration: disable the TR-181 DHCP clients
    As described in PPM-2094, the DHCP clients are enabled by default on
    the devices, and on the Turris Omnia (which uses the WAN port, on
    which the DHCP clients run by default) they caused a race condition
    with the IP that we try to set manually.
    
    Disable the new TR-181 DHCP clients on the devices like we used to do
    in UCI previously. Using ubus to set "Enable" to "false" on the DHCP
    clients doesn't work (the clients still assign IPs), so stop and
    disable them completely instead.
    
    On the Turris Omnia (which is currently the only device where we use
    the WAN port for control), we also need to always set the IP instead
    of just adding it if there is none. This is because in case the DHCP
    client had the time to already assign an IP, we need to change its
    value.
    
    Fixes PPM-2094
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- ci/configuration/certification/R4/axepoint.sh
+++ ci/configuration/certification/R4/axepoint.sh
@@ -5,6 +5,12 @@ set -e
 # Start with a new log file:
 rm -f /var/log/messages ; syslog-ng-ctl reload || true
 
+# Stop and disable the DHCP clients:
+/etc/init.d/tr181-dhcpv4client stop
+rm -f /etc/rc.d/S27tr181-dhcpv4client
+/etc/init.d/tr181-dhcpv6client stop
+rm -f /etc/rc.d/S25tr181-dhcpv6client
+
 # Set the LAN bridge IP:
 ubus wait_for IP.Interface
 ubus call "IP.Interface" _set '{ "rel_path": ".[Name == \"br-lan\"].IPv4Address.[Alias == \"lan\"].", "parameters": { "IPAddress": "192.165.100.173" } }'
