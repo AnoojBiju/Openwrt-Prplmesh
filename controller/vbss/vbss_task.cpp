774b9b163ab80c0be8f34c9bf0186b9525cd8c10
Benjamin Carlson <b.carlson@cablelabs.com>
2022-07-12 19:17:11 -0700

    vbss: vbss_task: Add additional response cases
    https://jira.prplfoundation.org/browse/PPM-2179
    
    Signed-off-by: Benjamin Carlson <b.carlson@cablelabs.com>

--- controller/vbss/vbss_task.cpp
+++ controller/vbss/vbss_task.cpp
@@ -9,27 +9,68 @@ bool vbss_task::handle_ieee1905_1_msg(const sMacAddr &src_mac, ieee1905_1::CmduM
     switch (cmdu_rx.getMessageType()) {
     case ieee1905_1::eMessageType::AP_CAPABILITY_REPORT_MESSAGE:
         // Virtual BSS Capabilities Response
-        // AP Radio VBSS Capabilities TLV
         break;
     case ieee1905_1::eMessageType::BSS_CONFIGURATION_RESULT_MESSAGE:
         // Virtual BSS Response
-        // Virtual BSS Event TLV
         break;
     case ieee1905_1::eMessageType::CLIENT_CAPABILITY_REPORT_MESSAGE:
         // This type is definitely not right
         // Client Security Context Response
-        // Client Info TLV
-        // Client Security Context TLV
         break;
     case ieee1905_1::eMessageType::CHANNEL_SELECTION_RESPONSE_MESSAGE:
         // Trigger Channel Switch Announcement Response
-        // Client Info TLV
-        // Trigger Channel Switch Announcement TLV
         break;
     case ieee1905_1::eMessageType::BSS_CONFIGURATION_RESPONSE_MESSAGE:
         // This type is definitely not right
         // Virtual BSS Move Preperation Response
-        // Client Info TLV
+        break;
+    case ieee1905_1::eMessageType::FAILED_CONNECTION_MESSAGE:
+        // This type is definitely not right
+        // Virtual BSS Move Cancel Response
+        break;
+    case ieee1905_1::eMessageType::TOPOLOGY_RESPONSE_MESSAGE:
+        //VBSS Configuration Report TLV
+        break;
+    case ieee1905_1::eMessageType::AP_AUTOCONFIGURATION_WSC_MESSAGE:
+        //AP Radio VBSS Capabilities TLV
+        break;
+    case ieee1905_1::eMessageType::BSS_CONFIGURATION_REQUEST_MESSAGE:
+        //AP Radio VBSS Capabilities TLV
         break;
     }
 }
+/*
+ Virtual BSS Capabilities Response
+    - AP Radio VBSS Capabilities TLV
+
+ Virtual BSS Response
+    - Virtual BSS Event TLV
+
+ Client Security Context Response
+    - Client Info TLV
+    - Client Security Context TLV
+
+ Trigger Channel Switch Announcement Response
+    - Client Info TLV
+    - Trigger Channel Switch Announcement TLV
+
+ Virtual VBSS Move Preperation Response
+    - Client Info TLV
+
+ Virtual BSS Move Cancel Response
+    - Client Info TLV
+
+-----------------------------------------------
+
+ 1905 Topology Response Message
+    - VBSS Configuration Report TLV
+    - ...
+
+ 1905 AP-Autoconfiguration WSC message
+    - AP Radio VBSS Capabilities TLV
+    - ...
+
+ BSS Configuration Request
+    - AP Radio VBSS Capabilities TLV
+    - ...
+*/
