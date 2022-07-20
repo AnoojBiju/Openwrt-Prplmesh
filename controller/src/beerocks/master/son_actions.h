b71da121ad4c53ec3021ec5e93c88231f48c9ef1
Yoni Abecassis <yabecassis@maxlinear.com>
2022-07-20 10:52:02 +0300

    controller: add source identifier to client disconnect task
    Following the previous commit, where a client-disconnect-source identifier
    was added to client disconnect request TLVF, add client disconnect source
    identifier to disconnect client method.
    
    Signed-off-by: Yoni Abecassis <yabecassis@maxlinear.com>

--- controller/src/beerocks/master/son_actions.h
+++ controller/src/beerocks/master/son_actions.h
@@ -38,7 +38,8 @@ public:
                          bool steer_restricted = false);
     static void disconnect_client(db &database, ieee1905_1::CmduMessageTx &cmdu_tx,
                                   const std::string &client_mac, const std::string &bssid,
-                                  eDisconnectType type, uint32_t reason);
+                                  eDisconnectType type, uint32_t reason,
+                                  eClientDisconnectSource src = eClient_Disconnect_Source_Ignore);
     static bool set_hostap_active(db &database, task_pool &tasks, std::string hostap_mac,
                                   bool active);
     static void send_cli_debug_message(db &database, ieee1905_1::CmduMessageTx &cmdu_tx,
