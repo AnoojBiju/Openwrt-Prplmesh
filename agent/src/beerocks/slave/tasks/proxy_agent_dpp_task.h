f5f677d740490418554441c4ce79f1b94b553d8a
hksrirama <hksrirama@maxlinear.com>
2022-07-28 11:47:49 +0300

    agent : add proxy_agent_dpp_task
    currently, there is no task to handle 1905 DPP messages.
    
    proxy_agent_dpp_task is added to handle 1905 DPP cmdu messages
    to and from controller and ap_manager.
    
    PPM-2158
    
    Signed-off-by: hksrirama <hksrirama@maxlinear.com>

---
+++ agent/src/beerocks/slave/tasks/proxy_agent_dpp_task.h
@@ -0,0 +1,43 @@
+#ifndef _PROXY_AGENT_DPP_TASK_H_
+#define _PROXY_AGENT_DPP_TASK_H_
+
+#include "task.h"
+#include <tlvf/CmduMessageTx.h>
+
+namespace beerocks {
+
+// Forward declaration for Agent context saving
+class slave_thread;
+
+class ProxyAgentDppTask : public Task {
+public:
+    ProxyAgentDppTask(slave_thread &btl_ctx, ieee1905_1::CmduMessageTx &cmdu_tx);
+    ~ProxyAgentDppTask() {}
+
+    bool handle_cmdu(ieee1905_1::CmduMessageRx &cmdu_rx, uint32_t iface_index,
+                     const sMacAddr &dst_mac, const sMacAddr &src_mac, int fd,
+                     std::shared_ptr<beerocks_header> beerocks_header) override;
+
+private:
+    slave_thread &m_btl_ctx;
+    ieee1905_1::CmduMessageTx &m_cmdu_tx;
+
+    /**
+     * @brief Parse DPP CCE Indication message.
+     *
+     * @param cmdu_rx Received CMDU.
+     * @return true on success, otherwise false.
+     */
+    void handle_dpp_cce_indication(ieee1905_1::CmduMessageRx &cmdu_rx);
+
+    /**
+     * @brief Parse Chirp Notification Message.
+     *
+     * @param cmdu_rx Received CMDU.
+     * @return true on success, otherwise false.
+     */
+    void handle_chirp_notification(ieee1905_1::CmduMessageRx &cmdu_rx);
+};
+} // namespace beerocks
+
+#endif // _PROXY_AGENT_DPP_TASK_H_
