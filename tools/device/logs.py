ba066240fcd80d2398f198916edfd74eb5617cff
Raphaël Mélotte <raphael.melotte@mind.be>
2022-08-12 15:59:10 +0200

    tools: device: logs: add dm-WiFi.txt
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- tools/device/logs.py
+++ tools/device/logs.py
@@ -27,7 +27,8 @@ LOG_COMMANDS = \
         LogCommand('ubus call Bridging _get \'{ "depth": "10" }\'', "dm-Bridge.txt"),
         LogCommand('ubus call Ethernet _get \'{ "depth": "10" }\'', "dm-Ethernet.txt"),
         LogCommand('ubus call DHCPv4 _get \'{ "depth": "10" }\'', "dm-DHCPv4.txt"),
-        LogCommand('ubus call DHCPv6 _get \'{ "depth": "10" }\'', "dm-DHCPv6.txt")
+        LogCommand('ubus call DHCPv6 _get \'{ "depth": "10" }\'', "dm-DHCPv6.txt"),
+        LogCommand('ubus call WiFi _get \'{ "depth": "10" }\'', "dm-WiFi.txt")
     ]
 
 
