0fb832ce2a2e8e0d84cf2f400afe889e487aa01b
hksrirama <hksrirama@maxlinear.com>
2022-08-23 12:50:18 +0300

    agent: forward DPP authentication confirm message
    Curently, there is no implementation to handle DPP Authentication
    confirm message in proxy_agent_dpp_task.
    
    Added code in proxy_agent_dpp_task to handle DPP Authentication
    confirm message and forward Proxied Encap DPP message to the ap_manager.
    
    PPM-2162
    
    Signed-off-by: hksrirama <hksrirama@maxlinear.com>

--- agent/src/beerocks/slave/tasks/proxy_agent_dpp_task.cpp
+++ agent/src/beerocks/slave/tasks/proxy_agent_dpp_task.cpp
@@ -25,7 +25,7 @@ bool ProxyAgentDppTask::handle_cmdu(ieee1905_1::CmduMessageRx &cmdu_rx, uint32_t
         return true;
     }
     case ieee1905_1::eMessageType::PROXIED_ENCAP_DPP_MESSAGE: {
-        handle_proxied_encap_dpp(cmdu_rx);
+        handle_proxied_encap_dpp(fd, src_mac, cmdu_rx);
         return true;
     }
     default: {
@@ -62,7 +62,8 @@ void ProxyAgentDppTask::handle_chirp_notification(ieee1905_1::CmduMessageRx &cmd
     m_btl_ctx.forward_cmdu_to_controller(cmdu_rx);
 }
 
-void ProxyAgentDppTask::handle_proxied_encap_dpp(ieee1905_1::CmduMessageRx &cmdu_rx)
+void ProxyAgentDppTask::handle_proxied_encap_dpp(int fd, const sMacAddr &src_mac,
+                                                 ieee1905_1::CmduMessageRx &cmdu_rx)
 {
     // cmdu message received from ap_manager
     auto encap_1905_dpp_tlv = cmdu_rx.getClass<wfa_map::tlv1905EncapDpp>();
@@ -70,7 +71,44 @@ void ProxyAgentDppTask::handle_proxied_encap_dpp(ieee1905_1::CmduMessageRx &cmdu
         LOG(ERROR) << "Proxied encap dpp message doesn't contain 1905 encap dpp tlv";
         return;
     }
-    m_btl_ctx.forward_cmdu_to_controller(cmdu_rx);
+
+    auto db = AgentDB::get();
+    if (src_mac == db->controller_info.bridge_mac) {
+        // TO DO: save DPP authentication request frame.
+        if (encap_1905_dpp_tlv->frame_type() == 0) {
+            // TO DO: handle DPP authentication request message
+            // forward proxy_encap_dpp messages originating from controller to ap_manager.
+            for (auto radio : db->get_radios_list()) {
+                auto ap_manager_fd = m_btl_ctx.get_ap_manager_fd(radio->front.iface_name);
+                if (!m_btl_ctx.forward_cmdu_to_uds(ap_manager_fd, cmdu_rx)) {
+                    LOG(ERROR) << "Failed to forward message to ap_manager";
+                    return;
+                }
+            }
+            return;
+        }
+        if (active_onboarding_ap_manager_fd == beerocks::net::FileDescriptor::invalid_descriptor) {
+            LOG(ERROR) << "Invalid ap_manager file descriptor" << active_onboarding_ap_manager_fd;
+            return;
+        }
+        if (!m_btl_ctx.forward_cmdu_to_uds(active_onboarding_ap_manager_fd, cmdu_rx)) {
+            LOG(ERROR) << "Failed to forward message to ap_manager";
+            return;
+        }
+        return;
+    }
+
+    // forward proxy encap dpp cmdu message originating from ap_manager to controller.
+    if (src_mac != db->controller_info.bridge_mac && src_mac == net::network_utils::ZERO_MAC) {
+        // save ap_manager fd that sent DPP Authentication Response.
+        if (encap_1905_dpp_tlv->frame_type() == 1) {
+            active_onboarding_ap_manager_fd = fd;
+        }
+        m_btl_ctx.forward_cmdu_to_controller(cmdu_rx);
+        return;
+    }
+    // To DO: To invalidate active_onboarding_ap_manager_fd file descriptor.
+    return;
 }
 
 } // namespace beerocks
