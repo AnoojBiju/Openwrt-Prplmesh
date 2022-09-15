43f5329ac50b10f62e67c34626a4b3064a2b0328
Tucker Polomik <t.polomik@cablelabs.com>
2022-09-15 11:30:13 -0600

    framework: tlvf: add tlvUnassociatedStaLinkMetricsQuery
    Also adds auto-generated files.
    
    Signed-off-by: Tucker Polomik <t.polomik@cablelabs.com>

--- framework/tlvf/src/src/CmduMessageRx.cpp
+++ framework/tlvf/src/src/CmduMessageRx.cpp
@@ -122,6 +122,7 @@
 #include <tlvf/wfa_map/tlvTunnelledData.h>
 #include <tlvf/wfa_map/tlvTunnelledProtocolType.h>
 #include <tlvf/wfa_map/tlvTunnelledSourceInfo.h>
+#include <tlvf/wfa_map/tlvUnassociatedStaLinkMetricsQuery.h>
 #include <tlvf/wfa_map/tlvVbssConfigurationReport.h>
 #include <tlvf/wfa_map/tlvVirtualBssCreation.h>
 #include <tlvf/wfa_map/tlvVirtualBssDestruction.h>
@@ -324,8 +325,7 @@ std::shared_ptr<BaseClass> CmduMessageRx::parseNextTlv(wfa_map::eTlvTypeMap tlv_
         return msg.addClass<wfa_map::tlvAssociatedStaLinkMetrics>();
     }
     case (wfa_map::eTlvTypeMap::TLV_UNASSOCIATED_STA_LINK_METRICS_QUERY): {
-        LOG(DEBUG) << "TLV_UNASSOCIATED_STA_LINK_METRICS_QUERY not supported";
-        return msg.addClass<ieee1905_1::tlvUnknown>();
+        return msg.addClass<wfa_map::tlvUnassociatedStaLinkMetricsQuery>();
     }
     case (wfa_map::eTlvTypeMap::TLV_UNASSOCIATED_STA_LINK_METRICS_RESPONSE): {
         LOG(DEBUG) << "TLV_UNASSOCIATED_STA_LINK_METRICS_RESPONSE not supported";
