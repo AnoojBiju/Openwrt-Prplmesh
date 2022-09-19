0b7a4df1dfe7ef3ef1f9aaa5b37c59028433dbf3
Raphaël Mélotte <raphael.melotte@mind.be>
2022-09-19 09:54:23 +0200

    ci: configuration: keep tr181-firewall enabled for remaining devices
    Commit ecab7162cc7090c2f3a04e4df5e35110fd8f5af6 switched to keeping
    the tr181-firewall service running, but using iptables to accept
    anything on INPUT. However some devices were left behind, so apply the
    same changes to them now.
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- ci/configuration/certification/R2/turris-omnia.sh
+++ ci/configuration/certification/R2/turris-omnia.sh
@@ -35,9 +35,11 @@ ubus call "IP.Interface" _set '{ "rel_path": ".[Name == \"br-lan\"].IPv4Address.
 # Wired backhaul interface:
 uci set prplmesh.config.backhaul_wire_iface='lan0'
 
-# Stop and disable the firewall:
-/etc/init.d/tr181-firewall stop
-rm -f /etc/rc.d/S22tr181-firewall
+# For now there is no way to disable the firewall (see PCF-590).
+# Instead, wait for it in the datamodel, then set the whole INPUT
+# chain to ACCEPT:
+ubus wait_for Firewall
+iptables -P INPUT ACCEPT
 
 # Required for config_load:
 . /lib/functions/system.sh
