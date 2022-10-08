05adbf4c24f2a718cfa6703817d633d319ef9180
Ahmed Amine HAOUES <ahmed-amine.haoues_ext@softathome.com>
2022-10-08 16:37:58 +0200

    framework: wbapi: add utils to query whm dm obj
    - add methods to format search path of whm datamodel objects paths
    - add wrapper methods to get datamodel object and typed parameters
    
    https://jira.prplfoundation.org/browse/PPM-2220
    
    Signed-off-by: Ahmed Amine HAOUES <ahmed-amine.haoues_ext@softathome.com>

--- framework/platform/wbapi/include/wbapi_utils.h
+++ framework/platform/wbapi/include/wbapi_utils.h
@@ -9,6 +9,7 @@
 #ifndef _WBAPI_UTILS_H_
 #define _WBAPI_UTILS_H_
 
+#include "ambiorix_client.h"
 #include <bcl/beerocks_defines.h>
 #include <easylogging++.h>
 #include <tlvf/WSC/eWscAuth.h>
@@ -19,7 +20,42 @@ namespace wbapi {
 
 class wbapi_utils {
 
+protected:
+    /**
+     * @brief virtual abstract method to get access to ambiorix client context.
+     * optional suffix.
+     *
+     * @return shared pointer of ambiorix client.
+     */
+    virtual const std::unique_ptr<beerocks::wbapi::AmbiorixClient> &get_amx_cli() = 0;
+
+    /**
+     * @brief request full object content of a provided path.
+     *
+     * @param[out] result: pointer to amxc_var_t, filled with object content.
+     *                     (To be cleared after usage)
+     * @param[in] obj_path: object datamodel path.
+     * @return true on success, false otherwise.
+     */
+    bool get_object(amxc_var_t *&result, const std::string &obj_path);
+
+    /**
+     * @brief request parameter value of a provided parameter path
+     * and converts it to basic types through template specialization.
+     *
+     * @param[out] result: reference to typed result, filled after conversion
+     *                     with the parameter value
+     * @param[in] obj_path: parent object path.
+     * @param[in] param_name: parameter name.
+     * @return true on success, false otherwise.
+     */
+    template <typename T>
+    bool get_param(T &result, const std::string &obj_path, const std::string &param_name);
+
 public:
+    wbapi_utils()          = default;
+    virtual ~wbapi_utils() = default;
+
     /**
      * @brief Converts a string-based bandwith to beerocks::eWiFiBandwidth.
      */
@@ -52,8 +88,141 @@ public:
 
     /**
      * @brief get amxc var object id from the object path.
+     * eg: "RootObj.TemplateObj.id" or "RootObj.TemplateObj.id."
+     *
+     * @return instance id , greater than 0 if successful, 0 otherwise
      */
     static int get_object_id(const std::string &object_path);
+
+    /**
+     * @brief return path of WiFi root object.
+     */
+    static std::string search_path_wifi();
+
+    /**
+     * @brief return path of Radio template object.
+     */
+    static std::string search_path_radio();
+
+    /**
+     * @brief return path of SSID template object.
+     */
+    static std::string search_path_ssid();
+
+    /**
+     * @brief return path of AccessPoint template object.
+     */
+    static std::string search_path_ap();
+
+    /**
+     * @brief return path of EndPoint template object.
+     */
+    static std::string search_path_ep();
+
+    /**
+     * @brief return search path of Radio object instance's Name.
+     */
+    static std::string search_path_radio_iface();
+
+    /**
+     * @brief return search path of Radio object instance by Name.
+     */
+    static std::string search_path_radio_by_iface(const std::string &rad_ifname);
+
+    /**
+     * @brief return search path of SSID object instance by interface name.
+     */
+    static std::string search_path_ssid_by_iface(const std::string &ssid_ifname);
+
+    /**
+     * @brief return search path of AccessPoint object instance by interface name.
+     */
+    static std::string search_path_ap_by_iface(const std::string &vap_ifname);
+
+    /**
+     * @brief return search path of AccessPoint object instances by radio reference.
+     */
+    static std::string search_path_ap_by_radRef(const std::string &radioRef);
+
+    /**
+     * @brief return search path of all AccessPoint object instance's interfaces
+     * filtered by radio reference.
+     */
+    static std::string search_path_ap_iface_by_radRef(const std::string &radioRef);
+
+    /**
+     * @brief return search path of associated device object instance
+     * filter by MAC address.
+     */
+    static std::string search_path_assocDev_by_mac(const std::string &vap_ifname,
+                                                   const std::string &mac);
+
+    /**
+     * @brief return search path of EndPoint object instance by interface name.
+     */
+    static std::string search_path_ep_by_iface(const std::string &ep_ifname);
+
+    /**
+     * @brief return search path of EndPoint profile object instance by endpoint interface name.
+     */
+    static std::string search_path_ep_profiles_by_iface(const std::string &ep_ifname);
+
+    /**
+     * @brief return search path of EndPoint EndPoint profile object instance by index
+     */
+    static std::string search_path_ep_profile_by_id(const std::string &ep_ifname,
+                                                    uint32_t profile_id);
+
+    /**
+     * @brief return radio instance path from radio reference param of an upper layer object
+     */
+    static std::string get_path_radio_reference(const amxc_var_t *obj);
+
+    /**
+     * @brief return ssid instance path from ssid reference param of an upper layer object
+     */
+    static std::string get_path_ssid_reference(const amxc_var_t *obj);
+
+    /**
+     * @brief return interface name param path of radio object instance
+     */
+    static std::string get_path_radio_iface(const std::string &radio_path);
+
+    /**
+     * @brief return interface name param path of AccessPoint object instance
+     */
+    static std::string get_path_ap_iface(const std::string &ap_path);
+
+    /**
+     * @brief return accesspoint object instance path from path of child AssociatedDevice object
+     * instance
+     */
+    static std::string get_path_ap_iface_of_assocDev(const std::string &assocDev_path);
+
+    /**
+     * @brief get interface name of AccessPoint object data
+     */
+    static std::string get_ap_iface(const amxc_var_t *obj);
+
+    /**
+     * @brief get interface name of SSID object data
+     */
+    static std::string get_ssid_iface(const amxc_var_t *obj);
+
+    /**
+     * @brief get interface name of EndPoint object data
+     */
+    static std::string get_ep_iface(const amxc_var_t *obj);
+
+private:
+    /**
+     * @brief build string path from static char buf and optional suffix.
+     *
+     * @param[in] buf: base string content.
+     * @param[in] pad: optional suffix.
+     * @return string with concatenated values.
+     */
+    static std::string get_string(const char *buf, const char *pad = "");
 };
 
 } // namespace wbapi
--- framework/platform/wbapi/wbapi_utils.cpp
+++ framework/platform/wbapi/wbapi_utils.cpp
@@ -32,8 +32,10 @@ beerocks::eFreqType wbapi_utils::band_to_freq(const std::string &band)
         return beerocks::eFreqType::FREQ_24G;
     } else if (band == "5GHz") {
         return beerocks::eFreqType::FREQ_5G;
+    } else if (band == "6GHz") {
+        return beerocks::eFreqType::FREQ_6G;
     } else {
-        LOG(ERROR) << "not Supported FreqBand value";
+        LOG(ERROR) << "not Supported FreqBand value: " << band;
         return beerocks::eFreqType::FREQ_UNKNOWN;
     }
 }
@@ -86,18 +88,201 @@ WSC::eWscEncr wbapi_utils::encryption_type_from_string(const std::string &encryp
 
 int wbapi_utils::get_object_id(const std::string &object_path)
 {
-    size_t pos_start = 0;
-    size_t pos_end;
-    std::string delimiter = ".";
-    size_t delim_len      = delimiter.length();
-    std::string token;
-
-    while ((pos_end = object_path.find(delimiter, pos_start)) != std::string::npos) {
-        token     = object_path.substr(pos_start, pos_end - pos_start);
-        pos_start = pos_end + delim_len;
+    auto str = object_path;
+    if (str.back() == '.') {
+        str.pop_back();
     }
-
+    auto pos = str.rfind('.');
+    if (pos == std::string::npos) {
+        return 0;
+    }
+    auto token = str.substr(pos + 1);
+    if (token.find_first_not_of("0123456789") != std::string::npos) {
+        return 0;
+    }
     return stoi(token);
+}
+
+std::string wbapi_utils::get_string(const char *buf, const char *pad)
+{
+    if (!buf || !buf[0]) {
+        return "";
+    }
+    auto ret = std::string(buf);
+    if (pad && pad[0]) {
+        return ret + pad;
+    }
+    return ret;
+}
+
+std::string wbapi_utils::search_path_wifi() { return std::string("WiFi."); }
+
+std::string wbapi_utils::search_path_radio() { return search_path_wifi() + "Radio."; }
+
+std::string wbapi_utils::search_path_ssid() { return search_path_wifi() + "SSID."; }
+
+std::string wbapi_utils::search_path_ap() { return search_path_wifi() + "AccessPoint."; }
+
+std::string wbapi_utils::search_path_ep() { return search_path_wifi() + "EndPoint."; }
+
+std::string wbapi_utils::search_path_radio_iface() { return search_path_radio() + "*.Name"; }
+
+std::string wbapi_utils::search_path_radio_by_iface(const std::string &rad_ifname)
+{
+    return search_path_radio() + "[Name == '" + rad_ifname + "'].";
+}
+
+std::string wbapi_utils::search_path_ssid_by_iface(const std::string &ssid_ifname)
+{
+    return search_path_ssid() + "[Name == '" + ssid_ifname + "'].";
+}
+
+std::string wbapi_utils::search_path_ap_by_iface(const std::string &vap_ifname)
+{
+    return search_path_ap() + "[Alias == '" + vap_ifname + "'].";
+}
+
+std::string wbapi_utils::search_path_ap_by_radRef(const std::string &radioRef)
+{
+    return search_path_ap() + "[RadioReference == '" + radioRef + "'].";
+}
+
+std::string wbapi_utils::search_path_ap_iface_by_radRef(const std::string &radioRef)
+{
+    return search_path_ap_by_radRef(radioRef) + "Alias";
+}
+
+std::string wbapi_utils::search_path_assocDev_by_mac(const std::string &vap_ifname,
+                                                     const std::string &mac)
+{
+    return search_path_ap_by_iface(vap_ifname) + "AssociatedDevice." + "[MACAddress == '" + mac +
+           "'].";
+}
+
+std::string wbapi_utils::search_path_ep_by_iface(const std::string &ep_ifname)
+{
+    return search_path_ep() + "[IntfName == '" + ep_ifname + "'].";
+}
+
+std::string wbapi_utils::search_path_ep_profiles_by_iface(const std::string &ep_ifname)
+{
+    return search_path_ep_by_iface(ep_ifname) + "Profile.";
+}
+
+std::string wbapi_utils::search_path_ep_profile_by_id(const std::string &ep_ifname,
+                                                      uint32_t profile_id)
+{
+    return search_path_ep_profiles_by_iface(ep_ifname) + std::to_string(profile_id) + ".";
+}
+
+std::string wbapi_utils::get_path_radio_reference(const amxc_var_t *obj)
+{
+    return get_string(GET_CHAR(obj, "RadioReference"), ".");
+}
+
+std::string wbapi_utils::get_path_ssid_reference(const amxc_var_t *obj)
+{
+    return get_string(GET_CHAR(obj, "SSIDReference"), ".");
+}
+
+std::string wbapi_utils::get_path_radio_iface(const std::string &radio_path)
+{
+    return get_string(radio_path.c_str(), "Name");
+}
+
+std::string wbapi_utils::get_path_ap_iface(const std::string &ap_path)
+{
+    return get_string(ap_path.c_str(), "Alias");
+}
+
+std::string wbapi_utils::get_path_ap_iface_of_assocDev(const std::string &assocDev_path)
+{
+    auto pos = assocDev_path.rfind("AssociatedDevice");
+    if (pos == std::string::npos) {
+        return "";
+    }
+    return get_path_ap_iface(assocDev_path.substr(0, pos));
+}
+
+std::string wbapi_utils::wbapi_utils::get_ap_iface(const amxc_var_t *obj)
+{
+    return get_string(GET_CHAR(obj, "Alias"));
+}
+
+std::string wbapi_utils::get_ssid_iface(const amxc_var_t *obj)
+{
+    return get_string(GET_CHAR(obj, "Alias"));
+}
+
+std::string wbapi_utils::get_ep_iface(const amxc_var_t *obj)
+{
+    return get_string(GET_CHAR(obj, "IntfName"));
+}
+
+bool wbapi_utils::get_object(amxc_var_t *&result, const std::string &obj_path)
+{
+    result = get_amx_cli()->get_object(obj_path);
+    return (result != nullptr);
+}
+
+template <>
+bool wbapi_utils::get_param<>(amxc_var_t *&result, const std::string &obj_path,
+                              const std::string &param_name)
+{
+    amxc_var_t *entry      = nullptr;
+    std::string param_path = obj_path + param_name;
+    if (get_object(entry, param_path)) {
+        result = amxc_var_get_first(entry);
+        amxc_var_take_it(result);
+        amxc_var_delete(&entry);
+        return true;
+    }
+    return false;
+}
+
+template <>
+bool wbapi_utils::get_param<>(std::string &result, const std::string &obj_path,
+                              const std::string &param_name)
+{
+    amxc_var_t *pRes = nullptr;
+    if (get_param<>(pRes, obj_path, param_name)) {
+        result = amxc_var_constcast(cstring_t, pRes);
+        amxc_var_delete(&pRes);
+        return true;
+    }
+    return false;
+}
+
+template <>
+bool wbapi_utils::get_param<>(bool &result, const std::string &obj_path,
+                              const std::string &param_name)
+{
+    amxc_var_t *pRes = nullptr;
+    if (get_param<>(pRes, obj_path, param_name)) {
+        result = amxc_var_dyncast(bool, pRes);
+        amxc_var_delete(&pRes);
+        return true;
+    }
+    return false;
+}
+
+template <>
+bool wbapi_utils::get_param<>(int &result, const std::string &obj_path,
+                              const std::string &param_name)
+{
+    amxc_var_t *pRes = nullptr;
+    if (get_param<>(pRes, obj_path, param_name)) {
+        result = amxc_var_dyncast(int32_t, pRes);
+        amxc_var_delete(&pRes);
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+bool wbapi_utils::get_param(T &result, const std::string &obj_path, const std::string &param_name)
+{
+    return false;
 }
 
 } // namespace wbapi
