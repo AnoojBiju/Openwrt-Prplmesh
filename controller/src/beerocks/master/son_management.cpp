60de4beabec36839a64fb13a6dd7ca4da5eb008d
Itay Elenzweig <ielenzweig@maxlinear.com>
2022-10-23 11:23:21 +0300

    0007

--- controller/src/beerocks/master/son_management.cpp
+++ controller/src/beerocks/master/son_management.cpp
@@ -1670,6 +1670,12 @@ void son_management::handle_bml_message(int sd, std::shared_ptr<beerocks_header>
                   << ", bandwidth=" << request->bandwidth()
                   << ", csa_count=" << request->csa_count();
 
+        if (!database.get_agent_by_radio_uid(request->radio_mac())) {
+            response->code() = uint8_t(eChannelSwitchStatus::ERROR);
+            controller_ctx->send_cmdu(sd, cmdu_tx);
+            return;
+        }
+
         bool need_new_preference = false;
         uint8_t operating_class  = 0;
         if (request->channel() != 0) {
