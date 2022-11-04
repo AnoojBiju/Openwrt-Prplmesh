e940227cd3f849ad92d2d61856911c0fa96b0807
Raphaël Mélotte <raphael.melotte@mind.be>
2022-11-04 12:31:34 +0100

    bwl: nl80211: implement add_key()
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- common/beerocks/bwl/nl80211/ap_wlan_hal_nl80211.cpp
+++ common/beerocks/bwl/nl80211/ap_wlan_hal_nl80211.cpp
@@ -1561,8 +1561,7 @@ bool ap_wlan_hal_nl80211::remove_bss(std::string &ifname)
 
 bool ap_wlan_hal_nl80211::add_key(const sKeyInfo &key_info)
 {
-    LOG(TRACE) << __func__ << " - NOT IMPLEMENTED!";
-    return false;
+    return m_nl80211_client->add_key(get_iface_name(), key_info);
 }
 
 } // namespace nl80211
