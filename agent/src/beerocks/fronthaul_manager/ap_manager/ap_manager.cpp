c5463787a2d6ea27bc00808bcc8ddab21e607d71
badhri_s <bsrinivasan@maxlinear.com>
2022-11-09 23:44:07 +0530

    ap_manager: handle Interface_* events
    Add fix to handle the new dwpald events
    INTERFACE_CONNECTED_OK and INTERFACE_RECONNECTED_OK
    which tells about the connection status between the
    hostapd and dwpal daemon.
    
    PPM-2344
    
    Signed-off-by: badhri_s <bsrinivasan@maxlinear.com>

--- agent/src/beerocks/fronthaul_manager/ap_manager/ap_manager.cpp
+++ agent/src/beerocks/fronthaul_manager/ap_manager/ap_manager.cpp
@@ -1671,6 +1671,8 @@ bool ApManager::hal_event_handler(bwl::base_wlan_hal::hal_event_ptr_t event_ptr)
         handle_hostapd_attached();
     } break;
 
+    case Event::Interface_Connected_OK:
+    case Event::Interface_Reconnected_OK:
     case Event::AP_Enabled: {
         if (!data) {
             LOG(ERROR) << "AP_Enabled without data!";
@@ -2045,6 +2047,7 @@ bool ApManager::hal_event_handler(bwl::base_wlan_hal::hal_event_ptr_t event_ptr)
     } break;
 
     // AP/Interface Disabled
+    case Event::Interface_Disconnected:
     case Event::AP_Disabled: {
         if (!data) {
             LOG(ERROR) << "AP_Disabled without data!";
