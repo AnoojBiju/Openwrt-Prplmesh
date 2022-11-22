172dadfcea716bc1733772be6e3a36698cce9a79
Marouan MCHARFI <marouan.mcharfi_ext@softathome.com>
2022-11-22 16:30:15 +0100

    ci: add certification test for pwhm
    Add certification test using pwhm for GLinet
    
    Signed-off-by: Marouan MCHARFI <marouan.mcharfi_ext@softathome.com>

---
+++ ci/configuration/certification/R1/whm/glinet-b1300.sh
@@ -0,0 +1,131 @@
+#!/bin/sh
+
+# We need to source some files which are only available on prplWrt
+# devices, so prevent shellcheck from trying to read them:
+# shellcheck disable=SC1091
+
+set -e
+
+# Start with a new log file:
+rm -f /var/log/messages && syslog-ng-ctl reload
+
+data_overlay_not_initialized()
+{
+  grep -q overlayfs:/tmp/root /proc/mounts || test -f /tmp/.switch_jffs2 || pgrep 'mount_root done'
+}
+
+if data_overlay_not_initialized; then
+  logger -t prplmesh -p daemon.info "Waiting for data overlay initialization..."
+  while data_overlay_not_initialized; do
+    sleep 2
+  done
+  logger -t prplmesh -p daemon.info "Data overlay is initialized."
+fi
+sleep 2
+
+ubus wait_for DHCPv4
+ubus wait_for DHCPv6
+ubus wait_for IP.Interface
+
+# Stop and disable the DHCP clients and servers:
+ubus call DHCPv4.Client.1 _set '{"parameters": { "Enable": False }}'
+ubus call DHCPv6.Client.1 _set '{"parameters": { "Enable": False }}'
+ubus call DHCPv4.Server _set '{"parameters": { "Enable": False }}'
+ubus call DHCPv6.Server _set '{"parameters": { "Enable": False }}'
+
+# Since for the GL-inet eth0 (LAN) is always detected as UP, eth1 (WAN) will be configured as a backhaul interface
+# This allows prplMesh to detect the state of the backhaul interface
+# WAN and LAN interfaces are effectively switched
+
+# Remove the physical LAN interface from the LAN-bridge; it will become the control interface
+# Also add the WAN interface to the LAN-bridge
+# Set the LAN bridge IP:
+ubus call "IP.Interface" _set '{ "rel_path": ".[Name == \"br-lan\"].IPv4Address.[Alias == \"lan\"].", "parameters": { "IPAddress": "192.165.100.172" } }'
+
+# Move the WAN port into the LAN bridge if it's not there yet (to use it for data):
+ubus wait_for Bridging.Bridge
+ubus call "Bridging.Bridge" _get '{ "rel_path": ".[Alias == \"lan\"].Port.[Name == \"eth1\"]." }' || {
+    echo "Adding WAN interface to LAN-bridge"
+    ubus call "Bridging.Bridge" _add '{ "rel_path": ".[Alias == \"lan\"].Port.",  "parameters": { "Name": "eth1", "Alias": "ETH1", "Enable": true } }'
+}
+
+# One of the LAN ports is used for control. Create a section for it:
+uci set network.cert=interface
+# Setting ifname is not supported in the current version of the TR-181
+# IP manager (v1.11.1), set it in UCI instead:
+uci set network.cert.ifname='eth0'
+
+# Remove the control interface from the LAN bridge if it's not already the case:
+ubus wait_for Bridging.Bridge
+ubus call "Bridging.Bridge" _get '{ "rel_path": ".[Alias == \"lan\"].Port.[Name == \"eth0\"]." }' && {
+    echo "Removing LAN interface from LAN-bridge"
+    echo "It will be used as control interface"
+    ubus call "Bridging.Bridge" _del '{ "rel_path": ".[Alias == \"lan\"].Port.[Name == \"eth0\"]." }'
+    uci changes; sleep 5
+}
+
+# To set the IP on the control interface, we first need to find the
+# corresponding Ethernet.Interface:
+ETH_IF="$(ubus call Ethernet.Interface _list | jsonfilter -e '@.instances[@.name="ETH0"].index')"
+# Then if there is no corresponding Ethernet.Link yet, we need to add
+# one:
+ubus call Ethernet.Link _get '{ "rel_path": ".[Name == \"eth0\"]." }' || {
+    echo "Adding Ethernet Link for LAN interface"
+    ETH_LINK="$(ubus call Ethernet.Link _add "{ \"parameters\": { \"Name\": \"eth0\", \"Alias\": \"eth0\",\"LowerLayers\": \"Device.Ethernet.Interface.$ETH_IF.\", \"Enable\": true } }" | jsonfilter -e '@.index')"
+}
+# We can now create an IP.Interface if there is none yet:
+ubus call IP.Interface _get '{ "rel_path": ".[Name == \"eth0\"]." }' || {
+    echo "Adding IP.Interface"
+    ubus call IP.Interface _add "{ \"parameters\": { \"Name\": \"eth0\", \"UCISectionNameIPv4\": \"cert\", \"Alias\": \"eth0\", \"LowerLayers\": \"Device.Ethernet.Link.$ETH_LINK.\", \"Enable\": true } }"
+}
+# We can now add the IP address if there is none yet:
+ubus call IP.Interface _get '{ "rel_path": ".[Name == \"eth0\"].IPv4Address.[Alias == \"eth0\"]." }' || {
+    echo "Adding IP address $IP"
+    ubus call "IP.Interface" _add '{ "rel_path": ".[Name == \"eth0\"].IPv4Address.", "parameters": { "IPAddress": "192.168.250.172", "SubnetMask": "255.255.255.0", "AddressingType": "Static", "Alias": "eth0", "Enable" : true } }'
+}
+# Finally, we can enable it:
+ubus call "IP.Interface" _set '{ "rel_path": ".[Name == \"eth0\"].", "parameters": { "IPv4Enable": true } }'
+
+# Wired backhaul interface:
+# Set the WAN interface as backhaul interface
+uci set prplmesh.config.backhaul_wire_iface='eth1'
+
+# Stop and disable the firewall:
+/etc/init.d/tr181-firewall stop
+rm -f /etc/rc.d/S22tr181-firewall
+
+# Required for config_load:
+. /lib/functions/system.sh
+# Required for config_foreach:
+. /lib/functions.sh
+
+# add private vaps to lan to workaround Netmodel missing wlan mib
+# this must be reverted once Netmodel version is integrated
+brctl addif br-lan wlan0 > /dev/null 2>&1 || true
+brctl addif br-lan wlan1 > /dev/null 2>&1 || true
+
+# configure private vaps
+ubus call "WiFi.SSID.1" _set '{ "parameters": { "SSID": "prplmesh" } }'
+ubus call "WiFi.SSID.2" _set '{ "parameters": { "SSID": "prplmesh" } }'
+ubus call "WiFi.AccessPoint.1.Security" _set '{ "parameters": { "KeyPassPhrase": "prplmesh_pass" } }'
+ubus call "WiFi.AccessPoint.2.Security" _set '{ "parameters": { "KeyPassPhrase": "prplmesh_pass" } }'
+ubus call "WiFi.AccessPoint.1.Security" _set '{ "parameters": { "ModeEnabled": "WPA2-Personal" } }'
+ubus call "WiFi.AccessPoint.2.Security" _set '{ "parameters": { "ModeEnabled": "WPA2-Personal" } }'
+ubus call "WiFi.AccessPoint.1.WPS" _set '{ "parameters": { "ConfigMethodsEnabled": "PushButton" } }'
+ubus call "WiFi.AccessPoint.2.WPS" _set '{ "parameters": { "ConfigMethodsEnabled": "PushButton" } }'
+
+# Make sure specific channels are configured. If channel is set to 0,
+# ACS will be configured. If ACS is configured hostapd will refuse to
+# switch channels when we ask it to. Channels 1 and 48 were chosen
+# because they are NOT used in the WFA certification tests (this
+# allows to verify that the device actually switches channel as part
+# of the test).
+# See also PPM-1928.
+ubus call "WiFi.Radio.1" _set '{ "parameters": { "Channel": "1" } }'
+ubus call "WiFi.Radio.2" _set '{ "parameters": { "Channel": "48" } }'
+
+# secondary vaps and backhaul are not supported yet (WIP)
+
+# Try to work around PCF-681: if we don't have a connectivity, restart
+# tr181-bridging
+ping 192.168.1.2 -c 3 || /etc/init.d/tr181-bridging restart
