b3a551c2576d2605fd9d73f756bb2d2d1fd1064a
wiswis15 <wissem.golli_ext@softathome.com>
2022-11-30 09:54:45 +0100

    framework: small update for the telemetry definition.
    Closes PPM-2323
    
    Signed-off-by: wiswis15 <wissem.golli_ext@softathome.com>

--- framework/tlvf/AutoGenerated/src/tlvf/wfa_map/tlvUnassociatedStaLinkMetricsQuery.cpp
+++ framework/tlvf/AutoGenerated/src/tlvf/wfa_map/tlvUnassociatedStaLinkMetricsQuery.cpp
@@ -41,42 +41,74 @@ uint8_t& tlvUnassociatedStaLinkMetricsQuery::channel_list_length() {
     return (uint8_t&)(*m_channel_list_length);
 }
 
-std::tuple<bool, tlvUnassociatedStaLinkMetricsQuery::sChannelPrameters&> tlvUnassociatedStaLinkMetricsQuery::channel_list(size_t idx) {
+std::tuple<bool, cChannelParameters&> tlvUnassociatedStaLinkMetricsQuery::channel_list(size_t idx) {
     bool ret_success = ( (m_channel_list_idx__ > 0) && (m_channel_list_idx__ > idx) );
     size_t ret_idx = ret_success ? idx : 0;
     if (!ret_success) {
         TLVF_LOG(ERROR) << "Requested index is greater than the number of available entries";
     }
-    return std::forward_as_tuple(ret_success, m_channel_list[ret_idx]);
+    return std::forward_as_tuple(ret_success, *(m_channel_list_vector[ret_idx]));
 }
 
-bool tlvUnassociatedStaLinkMetricsQuery::alloc_channel_list(size_t count) {
-    if (m_lock_order_counter__ > 0) {;
+std::shared_ptr<cChannelParameters> tlvUnassociatedStaLinkMetricsQuery::create_channel_list() {
+    if (m_lock_order_counter__ > 0) {
         TLVF_LOG(ERROR) << "Out of order allocation for variable length list channel_list, abort!";
-        return false;
+        return nullptr;
     }
-    size_t len = sizeof(sChannelPrameters) * count;
-    if(getBuffRemainingBytes() < len )  {
-        TLVF_LOG(ERROR) << "Not enough available space on buffer - can't allocate";
-        return false;
+    size_t len = cChannelParameters::get_initial_size();
+    if (m_lock_allocation__) {
+        TLVF_LOG(ERROR) << "Can't create new element before adding the previous one";
+        return nullptr;
+    }
+    if (getBuffRemainingBytes() < len) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer";
+        return nullptr;
     }
     m_lock_order_counter__ = 0;
-    uint8_t *src = (uint8_t *)&m_channel_list[*m_channel_list_length];
-    uint8_t *dst = src + len;
+    m_lock_allocation__ = true;
+    uint8_t *src = (uint8_t *)m_channel_list;
+    if (m_channel_list_idx__ > 0) {
+        src = (uint8_t *)m_channel_list_vector[m_channel_list_idx__ - 1]->getBuffPtr();
+    }
     if (!m_parse__) {
+        uint8_t *dst = src + len;
         size_t move_length = getBuffRemainingBytes(src) - len;
         std::copy_n(src, move_length, dst);
     }
-    m_channel_list_idx__ += count;
-    *m_channel_list_length += count;
+    return std::make_shared<cChannelParameters>(src, getBuffRemainingBytes(src), m_parse__);
+}
+
+bool tlvUnassociatedStaLinkMetricsQuery::add_channel_list(std::shared_ptr<cChannelParameters> ptr) {
+    if (ptr == nullptr) {
+        TLVF_LOG(ERROR) << "Received entry is nullptr";
+        return false;
+    }
+    if (m_lock_allocation__ == false) {
+        TLVF_LOG(ERROR) << "No call to create_channel_list was called before add_channel_list";
+        return false;
+    }
+    uint8_t *src = (uint8_t *)m_channel_list;
+    if (m_channel_list_idx__ > 0) {
+        src = (uint8_t *)m_channel_list_vector[m_channel_list_idx__ - 1]->getBuffPtr();
+    }
+    if (ptr->getStartBuffPtr() != src) {
+        TLVF_LOG(ERROR) << "Received entry pointer is different than expected (expecting the same pointer returned from add method)";
+        return false;
+    }
+    if (ptr->getLen() > getBuffRemainingBytes(ptr->getStartBuffPtr())) {;
+        TLVF_LOG(ERROR) << "Not enough available space on buffer";
+        return false;
+    }
+    m_channel_list_idx__++;
+    if (!m_parse__) { (*m_channel_list_length)++; }
+    size_t len = ptr->getLen();
+    m_channel_list_vector.push_back(ptr);
     if (!buffPtrIncrementSafe(len)) {
         LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << len << ") Failed!";
         return false;
     }
-    if(m_length){ (*m_length) += len; }
-    if (!m_parse__) { 
-        for (size_t i = m_channel_list_idx__ - count; i < m_channel_list_idx__; i++) { m_channel_list[i].struct_init(); }
-    }
+    if(!m_parse__ && m_length){ (*m_length) += len; }
+    m_lock_allocation__ = false;
     return true;
 }
 
@@ -84,7 +116,7 @@ void tlvUnassociatedStaLinkMetricsQuery::class_swap()
 {
     tlvf_swap(16, reinterpret_cast<uint8_t*>(m_length));
     for (size_t i = 0; i < m_channel_list_idx__; i++){
-        m_channel_list[i].struct_swap();
+        std::get<1>(channel_list(i)).class_swap();
     }
 }
 
@@ -157,12 +189,21 @@ bool tlvUnassociatedStaLinkMetricsQuery::init()
         return false;
     }
     if(m_length && !m_parse__){ (*m_length) += sizeof(uint8_t); }
-    m_channel_list = reinterpret_cast<sChannelPrameters*>(m_buff_ptr__);
+    m_channel_list = reinterpret_cast<cChannelParameters*>(m_buff_ptr__);
     uint8_t channel_list_length = *m_channel_list_length;
-    m_channel_list_idx__ = channel_list_length;
-    if (!buffPtrIncrementSafe(sizeof(sChannelPrameters) * (channel_list_length))) {
-        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(sChannelPrameters) * (channel_list_length) << ") Failed!";
-        return false;
+    m_channel_list_idx__ = 0;
+    for (size_t i = 0; i < channel_list_length; i++) {
+        auto channel_list = create_channel_list();
+        if (!channel_list || !channel_list->isInitialized()) {
+            TLVF_LOG(ERROR) << "create_channel_list() failed";
+            return false;
+        }
+        if (!add_channel_list(channel_list)) {
+            TLVF_LOG(ERROR) << "add_channel_list() failed";
+            return false;
+        }
+        // swap back since channel_list will be swapped as part of the whole class swap
+        channel_list->class_swap();
     }
     if (m_parse__) { class_swap(); }
     if (m_parse__) {
@@ -171,6 +212,132 @@ bool tlvUnassociatedStaLinkMetricsQuery::init()
             return false;
         }
     }
+    return true;
+}
+
+cChannelParameters::cChannelParameters(uint8_t* buff, size_t buff_len, bool parse) :
+    BaseClass(buff, buff_len, parse) {
+    m_init_succeeded = init();
+}
+cChannelParameters::cChannelParameters(std::shared_ptr<BaseClass> base, bool parse) :
+BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse){
+    m_init_succeeded = init();
+}
+cChannelParameters::~cChannelParameters() {
+}
+uint8_t& cChannelParameters::channel_number() {
+    return (uint8_t&)(*m_channel_number);
+}
+
+uint8_t& cChannelParameters::sta_list_length() {
+    return (uint8_t&)(*m_sta_list_length);
+}
+
+std::tuple<bool, sMacAddr&> cChannelParameters::sta_list(size_t idx) {
+    bool ret_success = ( (m_sta_list_idx__ > 0) && (m_sta_list_idx__ > idx) );
+    size_t ret_idx = ret_success ? idx : 0;
+    if (!ret_success) {
+        TLVF_LOG(ERROR) << "Requested index is greater than the number of available entries";
+    }
+    return std::forward_as_tuple(ret_success, m_sta_list[ret_idx]);
+}
+
+bool cChannelParameters::alloc_sta_list(size_t count) {
+    if (m_lock_order_counter__ > 0) {;
+        TLVF_LOG(ERROR) << "Out of order allocation for variable length list sta_list, abort!";
+        return false;
+    }
+    size_t len = sizeof(sMacAddr) * count;
+    if(getBuffRemainingBytes() < len )  {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer - can't allocate";
+        return false;
+    }
+    m_lock_order_counter__ = 0;
+    uint8_t *src = (uint8_t *)&m_sta_list[*m_sta_list_length];
+    uint8_t *dst = src + len;
+    if (!m_parse__) {
+        size_t move_length = getBuffRemainingBytes(src) - len;
+        std::copy_n(src, move_length, dst);
+    }
+    m_sta_list_idx__ += count;
+    *m_sta_list_length += count;
+    if (!buffPtrIncrementSafe(len)) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << len << ") Failed!";
+        return false;
+    }
+    if (!m_parse__) { 
+        for (size_t i = m_sta_list_idx__ - count; i < m_sta_list_idx__; i++) { m_sta_list[i].struct_init(); }
+    }
+    return true;
+}
+
+void cChannelParameters::class_swap()
+{
+    for (size_t i = 0; i < m_sta_list_idx__; i++){
+        m_sta_list[i].struct_swap();
+    }
+}
+
+bool cChannelParameters::finalize()
+{
+    if (m_parse__) {
+        TLVF_LOG(DEBUG) << "finalize() called but m_parse__ is set";
+        return true;
+    }
+    if (m_finalized__) {
+        TLVF_LOG(DEBUG) << "finalize() called for already finalized class";
+        return true;
+    }
+    if (!isPostInitSucceeded()) {
+        TLVF_LOG(ERROR) << "post init check failed";
+        return false;
+    }
+    if (m_inner__) {
+        if (!m_inner__->finalize()) {
+            TLVF_LOG(ERROR) << "m_inner__->finalize() failed";
+            return false;
+        }
+        auto tailroom = m_inner__->getMessageBuffLength() - m_inner__->getMessageLength();
+        m_buff_ptr__ -= tailroom;
+    }
+    class_swap();
+    m_finalized__ = true;
+    return true;
+}
+
+size_t cChannelParameters::get_initial_size()
+{
+    size_t class_size = 0;
+    class_size += sizeof(uint8_t); // channel_number
+    class_size += sizeof(uint8_t); // sta_list_length
+    return class_size;
+}
+
+bool cChannelParameters::init()
+{
+    if (getBuffRemainingBytes() < get_initial_size()) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    m_channel_number = reinterpret_cast<uint8_t*>(m_buff_ptr__);
+    if (!buffPtrIncrementSafe(sizeof(uint8_t))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(uint8_t) << ") Failed!";
+        return false;
+    }
+    m_sta_list_length = reinterpret_cast<uint8_t*>(m_buff_ptr__);
+    if (!m_parse__) *m_sta_list_length = 0;
+    if (!buffPtrIncrementSafe(sizeof(uint8_t))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(uint8_t) << ") Failed!";
+        return false;
+    }
+    m_sta_list = reinterpret_cast<sMacAddr*>(m_buff_ptr__);
+    uint8_t sta_list_length = *m_sta_list_length;
+    m_sta_list_idx__ = sta_list_length;
+    if (!buffPtrIncrementSafe(sizeof(sMacAddr) * (sta_list_length))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(sMacAddr) * (sta_list_length) << ") Failed!";
+        return false;
+    }
+    if (m_parse__) { class_swap(); }
     return true;
 }
 
