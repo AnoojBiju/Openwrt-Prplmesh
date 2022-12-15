52783da86a865cf2e9facd152f58006687f06103
ddking_97 <d.puz@inango-systems.com>
2022-12-15 09:47:12 +0200

    DO NOT MERGE
    This is not strictly-speaking a "bug" in prplMesh,
    but rather a usability issue or wrong architecture.
    
    We want to remove backhaul_wire_iface and start to
    use any interface for backhaul.
    
    This commit is mainly for debugging and some
    architectural decisions.
    
    Signed-off-by: ddking_97 <d.puz@inango-systems.com>

--- agent/src/beerocks/slave/beerocks_agent_main.cpp
+++ agent/src/beerocks/slave/beerocks_agent_main.cpp
@@ -383,6 +383,8 @@ static int run_beerocks_slave(beerocks::config_file::sConfigSlave &beerocks_slav
 
         db->init_data_model(amb_dm_obj);
 
+        // Read the backhole interface from the configuration file.
+        // Need to fix
         if (!beerocks::bpl::bpl_cfg_get_backhaul_wire_iface(db->ethernet.wan.iface_name)) {
             LOG(ERROR) << "Failed reading 'backhaul_wire_iface'";
             return false;
