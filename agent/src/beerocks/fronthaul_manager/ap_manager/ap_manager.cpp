5f9a6448cdf20ca7dfb0255d741518fa79b9e6e3
RagavanR <rramasamy@maxlinear.com>
2022-12-27 09:42:50 +0530

    agent: add wifi6_capability params in Wi-Fi6 Capabilities TLV
    wifi6 Capabilities parameters are copied to structure 'radio'
    and fill in Wi-Fi6 Capabilities TLV in AP Capability Report.
    
    Closes PPM-2288
    
    Signed-off-by: RagavanR <rramasamy@maxlinear.com>

--- agent/src/beerocks/fronthaul_manager/ap_manager/ap_manager.cpp
+++ agent/src/beerocks/fronthaul_manager/ap_manager/ap_manager.cpp
@@ -2669,8 +2669,9 @@ void ApManager::handle_hostapd_attached()
     notification->params().vht_capability = ap_wlan_hal->get_radio_info().vht_capability;
     std::copy_n(ap_wlan_hal->get_radio_info().vht_mcs_set.data(),
                 beerocks::message::VHT_MCS_SET_SIZE, notification->params().vht_mcs_set);
-    notification->params().he_supported  = ap_wlan_hal->get_radio_info().he_supported;
-    notification->params().he_capability = ap_wlan_hal->get_radio_info().he_capability;
+    notification->params().he_supported     = ap_wlan_hal->get_radio_info().he_supported;
+    notification->params().he_capability    = ap_wlan_hal->get_radio_info().he_capability;
+    notification->params().wifi6_capability = ap_wlan_hal->get_radio_info().wifi6_capability;
     std::copy_n(ap_wlan_hal->get_radio_info().he_mcs_set.data(), beerocks::message::HE_MCS_SET_SIZE,
                 notification->params().he_mcs_set);
 
@@ -2700,6 +2701,8 @@ void ApManager::handle_hostapd_attached()
     LOG(INFO) << " vht_capability = " << std::hex << ap_wlan_hal->get_radio_info().vht_capability;
     LOG(INFO) << " he_supported = " << ap_wlan_hal->get_radio_info().he_supported;
     LOG(INFO) << " he_capability = " << std::hex << ap_wlan_hal->get_radio_info().he_capability;
+    LOG(INFO) << " wifi6_capability = " << std::hex
+              << ap_wlan_hal->get_radio_info().wifi6_capability;
     LOG(INFO) << " zwdfs = " << m_ap_support_zwdfs;
 
     copy_vaps_info(ap_wlan_hal, notification->vap_list().vaps);
