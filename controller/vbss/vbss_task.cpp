100a484f373f72bcfd9b4551a1bdb50ec1803804
Zack F <z.foreman@cablelabs.com>
2023-02-01 20:25:53 -0500

    Handling of the vbss destruction message primarily for accounting purposes.

--- controller/vbss/vbss_task.cpp
+++ controller/vbss/vbss_task.cpp
@@ -432,7 +432,8 @@ bool vbss_task::handle_vbss_event_response(const sMacAddr &src_mac,
         if (cntrCtx) {
             return cntrCtx->handle_vbss_creation(ruid, vbssid);
         }
-        return true;
+        LOG(ERROR) << "Failed to find controller context";
+        return false;
     }
 
     auto existing_destruction = active_destruction_events.get(vbssid);
@@ -444,9 +445,12 @@ bool vbss_task::handle_vbss_event_response(const sMacAddr &src_mac,
                        << "! Destroy event failed";
             return false;
         }
-        //TODO: Add VBSSes to DM
-
-        return true;
+        auto cntrxCtx = m_database.get_controller_ctx();
+        if (cntrxCtx) {
+            return cntrxCtx->handle_vbss_destruction(vbssid);
+        }
+        LOG(ERROR) << "Failed to find controller context";
+        return false;
     }
 
     LOG(DEBUG) << "Checking existing moves with creation state";
