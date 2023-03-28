9e739975a83e6cabcdff6327b8503eb2d6da88ba
Maarten De Decker <maarten.dedecker@mind.be>
2023-03-28 13:48:13 +0200

    R1: test whm on turris
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

---
+++ ci/configuration/certification/R1/whm/turris-omnia.sh
@@ -0,0 +1,117 @@
+#!/bin/sh
+
+# We need to source some files which are only available on prplWrt
+# devices, so prevent shellcheck from trying to read them:
+# shellcheck disable=SC1091
+
+set -e
+
+# Start with a new log file:
+rm -f /var/log/messages && syslog-ng-ctl reload
+
+data_overlay_not_initialized()
+{
+  grep -q overlayfs:/tmp/root /proc/mounts || test -f /tmp/.switch_jffs2 || pgrep 'mount_root done'
+}
+
+if data_overlay_not_initialized; then
+  logger -t prplmesh -p daemon.info "Waiting for data overlay initialization..."
+  while data_overlay_not_initialized; do
+    sleep 2
+  done
+  logger -t prplmesh -p daemon.info "Data overlay is initialized."
+fi
+sleep 2
+
+# Stop and disable the miniupnpd as it is logging a lot
+/etc/init.d/tr181-upnp stop
+rm -f /etc/rc.d/*tr181-upnp
+pkill -9 miniupnpd
+
+# disable firewall
+/etc/init.d/tr181-firewall stop ; sleep 2 ; /etc/init.d/tr181-firewall stop
+rm -f /etc/rc.d/*tr181-firewall
+
+ubus wait_for IP.Interface
+
+# Stop and disable the DHCP clients:
+ubus call DHCPv4.Client _set '{ "rel_path": "[Alias == \"wan\"].", "parameters":{"Enable":false}}'
+ubus call DHCPv6.Client _set '{ "rel_path": "[Alias == \"wan\"].", "parameters":{"Enable":false}}'
+/etc/init.d/tr181-dhcpv4client stop
+rm -f /etc/rc.d/*tr181-dhcpv4client
+/etc/init.d/tr181-dhcpv6client stop
+rm -f /etc/rc.d/*tr181-dhcpv6client
+
+# Save the IP settings persistently (PPM-2351):
+sed -ri 's/(dm-save.*) = false/\1 = true/g' /etc/amx/ip-manager/ip-manager.odl
+/etc/init.d/ip-manager restart
+sleep 12
+
+# We use WAN for the control interface.
+# Add the IP address if there is none yet:
+ubus call IP.Interface _get '{ "rel_path": ".[Alias == \"wan\"].IPv4Address.[Alias == \"wan\"]." }' || {
+    echo "Adding IP address $IP"
+    ubus call "IP.Interface" _add '{ "rel_path": ".[Alias == \"wan\"].IPv4Address.", "parameters": { "Alias": "wan", "AddressingType": "Static" } }'
+}
+# Configure it:
+ubus call "IP.Interface" _set '{ "rel_path": ".[Alias == \"wan\"].IPv4Address.1", "parameters": { "IPAddress": "192.168.250.190", "SubnetMask": "255.255.255.0", "AddressingType": "Static", "Enable" : true } }'
+# Enable it:
+ubus call "IP.Interface" _set '{ "rel_path": ".[Alias == \"wan\"].", "parameters": { "IPv4Enable": true } }'
+
+# Set the LAN bridge IP:
+ubus call "IP.Interface" _set '{ "rel_path": ".[Name == \"br-lan\"].IPv4Address.[Alias == \"lan\"].", "parameters": { "IPAddress": "192.165.100.190" } }'
+
+# Wired backhaul interface:
+uci set prplmesh.config.backhaul_wire_iface='lan0'
+uci commit
+
+# enable Wi-Fi radios
+ubus call "WiFi.Radio.1" _set '{ "parameters": { "Enable": "true" } }'
+ubus call "WiFi.Radio.2" _set '{ "parameters": { "Enable": "true" } }'
+
+# all pwhm default configuration can be found in /etc/amx/wld/wld_defaults.odl.uc
+
+# add private vaps to lan to workaround Netmodel missing wlan mib
+# this must be reverted once Netmodel version is integrated
+brctl addif br-lan wlan0 > /dev/null 2>&1 || true
+brctl addif br-lan wlan1 > /dev/null 2>&1 || true
+
+# configure private vaps
+ubus call "WiFi.SSID.1" _set '{ "parameters": { "SSID": "prplmesh" } }'
+ubus call "WiFi.SSID.2" _set '{ "parameters": { "SSID": "prplmesh" } }'
+ubus call "WiFi.AccessPoint.1.Security" _set '{ "parameters": { "KeyPassPhrase": "prplmesh_pass" } }'
+ubus call "WiFi.AccessPoint.2.Security" _set '{ "parameters": { "KeyPassPhrase": "prplmesh_pass" } }'
+ubus call "WiFi.AccessPoint.1.Security" _set '{ "parameters": { "ModeEnabled": "WPA2-Personal" } }'
+ubus call "WiFi.AccessPoint.2.Security" _set '{ "parameters": { "ModeEnabled": "WPA2-Personal" } }'
+ubus call "WiFi.AccessPoint.1.WPS" _set '{ "parameters": { "ConfigMethodsEnabled": "PushButton" } }'
+ubus call "WiFi.AccessPoint.2.WPS" _set '{ "parameters": { "ConfigMethodsEnabled": "PushButton" } }'
+
+# Make sure specific channels are configured. If channel is set to 0,
+# ACS will be configured. If ACS is configured hostapd will refuse to
+# switch channels when we ask it to. Channels 1 and 48 were chosen
+# because they are NOT used in the WFA certification tests (this
+# allows to verify that the device actually switches channel as part
+# of the test).
+# See also PPM-1928.
+ubus call "WiFi.Radio.1" _set '{ "parameters": { "Channel": "1" } }'
+ubus call "WiFi.Radio.2" _set '{ "parameters": { "Channel": "48" } }'
+
+sleep 10
+
+# Try to work around PCF-681: if we don't have a connectivity, restart
+# tr181-bridging
+# Check the status of the LAN bridge
+ip a l br-lan |grep "state UP" >/dev/null || (echo "LAN Bridge DOWN, restarting bridge manager" && /etc/init.d/tr181-bridging restart && sleep 15)
+
+# If we still can't ping the UCC, restart the IP manager
+ping -i 1 -c 2 192.168.250.199 || (/etc/init.d/ip-manager restart && sleep 12)
+
+# Restart the ssh server
+# (necessary when prplOS will be upstepped for this platform)
+# /etc/init.d/ssh-server restart
+# sleep 5
+
+# Start an ssh server on the control interfce
+# The ssh server that is already running will only accept connections from 
+# the IP interface that was configured with the IP-Manager
+dropbear -F -T 10 -p192.168.250.190:22 &
