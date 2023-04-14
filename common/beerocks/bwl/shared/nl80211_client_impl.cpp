AnoojBiju/Openwrt-Prplmesh

commit ff9b6d5b9bb86359fde74ec4fd7d4691ac54765b

agent: send delba for all TIDs instead of only one
Some specific stations (e.g. in the case that has been recently
tested, older Android phones) negociate block acks for TIDs other than
0 (for example TID 2).

When this happens, sending a DELBA for TID 0 won't help. Combined with
the fact that mobile phones can have pretty aggressive IP connectivity
checks, this leads to the phone disconnecting when we try to move it.

To fix it, send the delba for every TID.

Fixes PPM-2517

Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

common/beerocks/bwl/shared/nl80211_client_impl.cpp
@@ -1394,9 +1394,11 @@ bool nl80211_client_impl::send_delba(const std::string &interface_name, const sM
         return false;
     }
 
-    // Send a delba with reason code 0x25:
-    //    delba_entry << "0 1 37" << std::endl;
-    delba_entry << "0 1 1" << std::endl;
+    // Send a delba with reason code 1 (unknown) for all TIDs:
+    for (uint8_t tid = 0; tid < 16; tid++) {
+        delba_entry << std::to_string(tid) + " 1 1" << std::endl;
+    }
+
     delba_entry.close();
 
     if (delba_entry.fail()) {

commit c50157efd82b3cdd1a16edeecafd8d20abb0c281

agent: bwl: fix sending multiple delbas
It turns out that the newline causes the writes to fail with "Invalid
argument".

This fixes the regression introduced by
ff9b6d5b9bb86359fde74ec4fd7d4691ac54765b ("agent: send delba for all
TIDs instead of only one") where the creation consistently failed
because of the failing delba.

Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

common/beerocks/bwl/shared/nl80211_client_impl.cpp
@@ -1396,7 +1396,7 @@ bool nl80211_client_impl::send_delba(const std::string &interface_name, const sM
 
     // Send a delba with reason code 1 (unknown) for all TIDs:
     for (uint8_t tid = 0; tid < 16; tid++) {
-        delba_entry << std::to_string(tid) + " 1 1" << std::endl;
+        delba_entry << std::to_string(tid) + " 1 1";
     }
 
     delba_entry.close();