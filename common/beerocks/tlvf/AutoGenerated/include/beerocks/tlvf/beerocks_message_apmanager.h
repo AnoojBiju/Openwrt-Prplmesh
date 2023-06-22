f97bba08314a7280498e7f9d3a853bac47085b4f
ddking_97 <d.puz@inango-systems.com>
2023-06-22 23:23:16 +0300

    tlvf: add spatial reuse parameters to apmanager messages.
    Add spatial reuse parameters to ACTION_APMANAGER_HOSTAP_CSA_NOTIFICATION and
    ACTION_APMANAGER_HOSTAP_SPATIAL_REUSE_REPORT_NOTIFICATION to be able to send
    them from ap_manager to son_slave_thread.
    
    It was also found that ACTION_APMANAGER_SET_MAP_CONTROLLER_PROFILE and
    ACTION_APMANAGER_UNASSOC_STA_LINK_METRICS_RESPONSE have the same value of 73.
    Each action should have a unique identifier to ensure proper identification
    and handling of the messages. Having two actions with the same value could
    lead to confusion and incorrect processing of the messages.
    ACTION_APMANAGER_UNASSOC_STA_LINK_METRICS_RESPONSE was updated whith new number.
    
    Signed-off-by: ddking_97 <d.puz@inango-systems.com>

--- common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_apmanager.h
+++ common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_apmanager.h
@@ -633,6 +633,27 @@ class cACTION_APMANAGER_HOSTAP_SERVICE_PRIO_CONFIG : public BaseClass
         sServicePrioConfig* m_cs_params = nullptr;
 };
 
+class cACTION_APMANAGER_HOSTAP_SPATIAL_REUSE_REPORT_NOTIFICATION : public BaseClass
+{
+    public:
+        cACTION_APMANAGER_HOSTAP_SPATIAL_REUSE_REPORT_NOTIFICATION(uint8_t* buff, size_t buff_len, bool parse = false);
+        explicit cACTION_APMANAGER_HOSTAP_SPATIAL_REUSE_REPORT_NOTIFICATION(std::shared_ptr<BaseClass> base, bool parse = false);
+        ~cACTION_APMANAGER_HOSTAP_SPATIAL_REUSE_REPORT_NOTIFICATION();
+
+        static eActionOp_APMANAGER get_action_op(){
+            return (eActionOp_APMANAGER)(ACTION_APMANAGER_HOSTAP_SPATIAL_REUSE_REPORT_NOTIFICATION);
+        }
+        sSpatialReuseParams& sr_params();
+        void class_swap() override;
+        bool finalize() override;
+        static size_t get_initial_size();
+
+    private:
+        bool init();
+        eActionOp_APMANAGER* m_action_op = nullptr;
+        sSpatialReuseParams* m_sr_params = nullptr;
+};
+
 class cACTION_APMANAGER_CLIENT_ASSOCIATED_NOTIFICATION : public BaseClass
 {
     public:
