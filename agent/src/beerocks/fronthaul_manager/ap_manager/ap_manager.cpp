7d180c9b600f82984de340a39c83bae45bd3cb57
ddking_97 <d.puz@inango-systems.com>
2023-07-03 10:13:20 +0300

    agent: handle spatial reuse params at the ap manager
    Add AP manager logic for bss coloring (include tlv changes)
    Add changes tp son slave thread for inter-thread communication
    of bss coloring parameters.
    
    Signed-off-by: ddking_97 <d.puz@inango-systems.com>

--- agent/src/beerocks/fronthaul_manager/ap_manager/ap_manager.cpp
+++ agent/src/beerocks/fronthaul_manager/ap_manager/ap_manager.cpp
@@ -859,7 +859,7 @@ void ApManager::handle_virtual_bss_request(ieee1905_1::CmduMessageRx &cmdu_rx)
                 .key_idx = 0,
                 .mac     = virtual_bss_creation_tlv->client_mac(),
                 .key     = {virtual_bss_creation_tlv->ptk(),
-                        virtual_bss_creation_tlv->ptk() + virtual_bss_creation_tlv->key_length()},
+                            virtual_bss_creation_tlv->ptk() + virtual_bss_creation_tlv->key_length()},
                 .key_seq = pw_key_seq,
 
                 // TODO: PPM-2368: We need to know the pairwise cipher. For now, use CCMP
@@ -883,7 +883,7 @@ void ApManager::handle_virtual_bss_request(ieee1905_1::CmduMessageRx &cmdu_rx)
                 .key_idx = 1,
                 .mac     = beerocks::net::network_utils::ZERO_MAC,
                 .key     = {virtual_bss_creation_tlv->gtk(),
-                        virtual_bss_creation_tlv->gtk() + virtual_bss_creation_tlv->key_length()},
+                            virtual_bss_creation_tlv->gtk() + virtual_bss_creation_tlv->key_length()},
                 .key_seq = group_key_seq,
 
                 // TODO: PPM-2368: We need to know the groupwise cipher. For now, use CCMP
