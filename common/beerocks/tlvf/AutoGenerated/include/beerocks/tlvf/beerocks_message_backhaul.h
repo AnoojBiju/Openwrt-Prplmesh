bb58aaf90fe69a9d80ab67e96b9130c5fbeedac2
ddking_97 <d.puz@inango-systems.com>
2023-07-18 16:34:33 +0300

    Create variable for indication of spatial reuse params presence
    Signed-off-by: ddking_97 <d.puz@inango-systems.com>

--- common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_backhaul.h
+++ common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_backhaul.h
@@ -412,6 +412,7 @@ class cACTION_BACKHAUL_HOSTAP_CHANNEL_SWITCH_ACS_START : public BaseClass
         int8_t& tx_limit();
         uint8_t& tx_limit_valid();
         sSpatialReuseParams& sr_params();
+        uint8_t& spatial_reuse_valid();
         void class_swap() override;
         bool finalize() override;
         static size_t get_initial_size();
@@ -423,6 +424,7 @@ class cACTION_BACKHAUL_HOSTAP_CHANNEL_SWITCH_ACS_START : public BaseClass
         int8_t* m_tx_limit = nullptr;
         uint8_t* m_tx_limit_valid = nullptr;
         sSpatialReuseParams* m_sr_params = nullptr;
+        uint8_t* m_spatial_reuse_valid = nullptr;
 };
 
 class cACTION_BACKHAUL_HOSTAP_CANCEL_ACTIVE_CAC_REQUEST : public BaseClass
