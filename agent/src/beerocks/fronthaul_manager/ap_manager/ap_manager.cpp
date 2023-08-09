8c0a2cd5caa98b0354179cfa26a4e3a973307ba9
Nitish Suresh <nsuresh@maxlinear.com>
2023-08-09 19:03:33 +0530

    Prevent channel switch if requested channel is 0
    Add clang fix.
    
    Signed-off-by: Nitish Suresh <nsuresh@maxlinear.com>

--- agent/src/beerocks/fronthaul_manager/ap_manager/ap_manager.cpp
+++ agent/src/beerocks/fronthaul_manager/ap_manager/ap_manager.cpp
@@ -1336,9 +1336,10 @@ void ApManager::handle_cmdu(ieee1905_1::CmduMessageRx &cmdu_rx)
             }
         }
 
-        if (ap_wlan_hal->get_radio_info().channel == request->cs_params().channel &&
-            utils::convert_bandwidth_to_enum(ap_wlan_hal->get_radio_info().bandwidth) ==
-                request->cs_params().bandwidth) {
+        if ((!request->cs_params().channel) ||
+            (ap_wlan_hal->get_radio_info().channel == request->cs_params().channel &&
+             utils::convert_bandwidth_to_enum(ap_wlan_hal->get_radio_info().bandwidth) ==
+                 request->cs_params().bandwidth)) {
             // No need to switch channels
             LOG(INFO) << "No need to switch channels as current channel and requested channels are "
                          "the same.";
