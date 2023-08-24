f7c52d0988300d2205ad7e7643206cb6898a2aad
Maarten De Decker <maarten.dedecker@mind.be>
2023-08-25 01:16:02 +0200

    ci: configuration: certification: legacy configuration fixes Starts the SSH server correctly for non-WHM configuration. Fixes an issue where setting the WAN IP fails due to no delay after an IP manager restart.
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

--- ci/configuration/certification/R2/turris-omnia.sh
+++ ci/configuration/certification/R2/turris-omnia.sh
@@ -31,7 +31,7 @@ rm -f /etc/rc.d/S25tr181-dhcpv6client
 
 # Save the IP settings persistently (PPM-2351):
 sed -ri 's/(dm-save.*) = false/\1 = true/g' /etc/amx/ip-manager/ip-manager.odl
-/etc/init.d/ip-manager restart
+/etc/init.d/ip-manager restart && sleep 15
 
 ubus wait_for IP.Interface
 
@@ -174,3 +174,23 @@ config_foreach set_channel wifi-device
 uci commit
 /etc/init.d/system restart
 /etc/init.d/network restart
+
+sleep 10
+
+# Try to work around PCF-681: if we don't have a connectivity, restart
+# tr181-bridging
+# Check the status of the LAN bridge
+ip a |grep "br-lan:" |grep "state UP" >/dev/null || (echo "LAN Bridge DOWN, restarting bridge manager" && /etc/init.d/tr181-bridging restart && sleep 15)
+
+# If we still can't ping the UCC, restart the IP manager
+ping -i 1 -c 2 192.168.250.199 || (/etc/init.d/ip-manager restart && sleep 15)
+ping -i 1 -c 2 192.168.250.199 || (/etc/init.d/ip-manager restart && sleep 15)
+
+# Add iptables rule to rc.local to allow SSH access after reboot
+BOOTSCRIPT="/etc/rc.local"
+IPTABLES_CMD="sleep 20 && iptables -P INPUT ACCEPT && dropbear -F -T 10 -p192.168.250.170:22 &"
+if ! grep -q "$IPTABLES_CMD" "$BOOTSCRIPT"; then { head -n -2 "$BOOTSCRIPT"; echo "$IPTABLES_CMD"; tail -2 "$BOOTSCRIPT"; } >> btscript.tmp; mv btscript.tmp "$BOOTSCRIPT"; fi
+
+# Restart the ssh server
+# /etc/init.d/ssh-server restart
+dropbear -F -T 10 -p192.168.250.170:22 &
