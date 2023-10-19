ee4aa3bda2913fe3ee8dc21e87433cd6a780efc2
bsrinivasan <bsrinivasan@maxlinear.com>
2023-10-19 11:06:55 +0530

    bwl:mon_wlan_hal_dwpal: Distinguish Channel Scan in certification mode
    Add a code in channel_scan_trigger function such that if
    we are running certification mode then customise some of
    the background scan params to perform a faster scan
    in order to pass certification.
    
    PPM-2625
    
    Signed-off-by: bsrinivasan <bsrinivasan@maxlinear.com>

--- common/beerocks/bwl/dwpal/mon_wlan_hal_dwpal.cpp
+++ common/beerocks/bwl/dwpal/mon_wlan_hal_dwpal.cpp
@@ -1051,14 +1051,15 @@ bool mon_wlan_hal_dwpal::sta_link_measurements_11k_request(const std::string &va
 }
 
 bool mon_wlan_hal_dwpal::channel_scan_trigger(int dwell_time_msec,
-                                              const std::vector<unsigned int> &channel_pool)
+                                              const std::vector<unsigned int> &channel_pool,
+                                              bool cert_mode)
 {
     LOG(DEBUG) << "Channel scan trigger received on interface=" << m_radio_info.iface_name;
 
     //build background scan parameters
-    ScanParams channel_scan_params = {0};
+    scan_params channel_scan_params = {0};
     sScanCfgParamsBG params_bg; //background scan param
-    size_t bg_size = ScanCfgParams_size_invalid;
+    size_t bg_size = ScanCfgParamsBG_size;
 
     // get original background scan params
     if (!dwpal_get_scan_params_bg(params_bg, bg_size)) {
@@ -1072,8 +1073,30 @@ bool mon_wlan_hal_dwpal::channel_scan_trigger(int dwell_time_msec,
         dwell_time_msec = params_bg.window_slice + 1;
     }
 
-    if (params_bg.active_dwell_time != dwell_time_msec ||
-        params_bg.passive_dwell_time != dwell_time_msec) {
+    if (cert_mode) {
+        params_bg.active_dwell_time    = dwell_time_msec;
+        params_bg.passive_dwell_time   = dwell_time_msec;
+        params_bg.break_time           = BREAK_TIME_FOR_CERT;
+        params_bg.break_time_busy      = BREAK_TIME_BUSY_FOR_CERT;
+        params_bg.window_slice         = WINDOW_SLICE_FOR_CERT;
+        params_bg.window_slice_overlap = 0;
+        LOG(DEBUG) << "Certification mode:" << std::endl
+                   << " passive_dwell_time: " << params_bg.passive_dwell_time << std::endl
+                   << " active_dwell_time: " << params_bg.active_dwell_time << std::endl
+                   << " num_probe_reqs: " << params_bg.num_probe_reqs << std::endl
+                   << " probe_reqs_interval: " << params_bg.probe_reqs_interval << std::endl
+                   << " num_chans_in_chunk: " << params_bg.num_chans_in_chunk << std::endl
+                   << " Break_time: " << params_bg.break_time << std::endl
+                   << " Break_time_busy: " << params_bg.break_time_busy << std::endl
+                   << " window_slice: " << params_bg.window_slice << std::endl
+                   << " window_slice_overlap: " << params_bg.window_slice_overlap << std::endl
+                   << " cts_to_self_duration: " << params_bg.cts_to_self_duration << std::endl;
+        if (!dwpal_set_scan_params_bg(params_bg, bg_size)) {
+            LOG(ERROR) << "Failed setting new scan parameters";
+            return false;
+        }
+    } else if (params_bg.active_dwell_time != dwell_time_msec ||
+               params_bg.passive_dwell_time != dwell_time_msec) {
         params_bg.active_dwell_time  = dwell_time_msec;
         params_bg.passive_dwell_time = dwell_time_msec;
         LOG(DEBUG) << "Setting NEW scan params, updating default dwell_time from "
@@ -1095,15 +1118,16 @@ bool mon_wlan_hal_dwpal::channel_scan_trigger(int dwell_time_msec,
     // must as single wifi won't allow scan on ap without this flag
     channel_scan_params.ap_force = 1;
 
-    int cmd_res = 0;
-    auto ret    = dwpal_driver_nl_scan_trigger_sync(get_dwpal_nl_ctx(),
-                                                 (char *)m_radio_info.iface_name.c_str(), &cmd_res,
-                                                 &channel_scan_params);
-    if (ret != DWPAL_SUCCESS && cmd_res != 0) {
+    int cmd_res                     = 0;
+    m_scan_was_triggered_internally = true;
+    auto ret = dwpald_ieee80211_scan_trigger((char *)m_radio_info.iface_name.c_str(),
+                                             &channel_scan_params, &cmd_res);
+    if (ret != DWPALD_SUCCESS && cmd_res != 0) {
         LOG(ERROR) << " scan trigger failed! Abort scan";
+        m_scan_was_triggered_internally = false;
         return false;
     }
-    m_scan_was_triggered_internally = true;
+
     LOG(DEBUG) << "Scan trigger request sent";
 
     return true;
