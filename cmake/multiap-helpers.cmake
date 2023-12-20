6c4544a6a3123befff46045b7cc605ba41fb0c91
Maarten De Decker <maarten.dedecker@mind.be>
2023-12-20 02:00:30 +0100

    Prepare release 4.2.1
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

--- cmake/multiap-helpers.cmake
+++ cmake/multiap-helpers.cmake
@@ -43,7 +43,7 @@ if (NOT BUILD_DATE)
 endif()
 
 if (NOT PRPLMESH_VERSION)
-    set(prplmesh_VERSION "4.2.0")
+    set(prplmesh_VERSION "4.2.1")
 else()
     set(prplmesh_VERSION ${PRPLMESH_VERSION})
 endif()
