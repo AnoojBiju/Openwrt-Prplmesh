71f2b226a6d1ecf3debe4eda06b41295e30335be
bsrinivasan <bsrinivasan@maxlinear.com>
2024-01-12 15:25:11 +0800

    tlvf: AutoGenerated: Add the auto generated code
    Added the auto generated code for the newly
    created vendor specific message given by the
    yaml of the previous commit.
    
    PPM-2691
    
    Signed-off-by: bsrinivasan <bsrinivasan@maxlinear.com>

--- common/beerocks/tlvf/AutoGenerated/src/beerocks/tlvf/beerocks_message_backhaul.cpp
+++ common/beerocks/tlvf/AutoGenerated/src/beerocks/tlvf/beerocks_message_backhaul.cpp
@@ -2317,6 +2317,76 @@ bool cACTION_BACKHAUL_HOSTAP_SPATIAL_REUSE_REPORT_NOTIFICATION::init()
     return true;
 }
 
+cACTION_BACKHAUL_TRIGGER_ON_BOOT_SCAN::cACTION_BACKHAUL_TRIGGER_ON_BOOT_SCAN(uint8_t* buff, size_t buff_len, bool parse) :
+    BaseClass(buff, buff_len, parse) {
+    m_init_succeeded = init();
+}
+cACTION_BACKHAUL_TRIGGER_ON_BOOT_SCAN::cACTION_BACKHAUL_TRIGGER_ON_BOOT_SCAN(std::shared_ptr<BaseClass> base, bool parse) :
+BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse){
+    m_init_succeeded = init();
+}
+cACTION_BACKHAUL_TRIGGER_ON_BOOT_SCAN::~cACTION_BACKHAUL_TRIGGER_ON_BOOT_SCAN() {
+}
+sMacAddr& cACTION_BACKHAUL_TRIGGER_ON_BOOT_SCAN::radio_mac() {
+    return (sMacAddr&)(*m_radio_mac);
+}
+
+void cACTION_BACKHAUL_TRIGGER_ON_BOOT_SCAN::class_swap()
+{
+    tlvf_swap(8*sizeof(eActionOp_BACKHAUL), reinterpret_cast<uint8_t*>(m_action_op));
+    m_radio_mac->struct_swap();
+}
+
+bool cACTION_BACKHAUL_TRIGGER_ON_BOOT_SCAN::finalize()
+{
+    if (m_parse__) {
+        TLVF_LOG(DEBUG) << "finalize() called but m_parse__ is set";
+        return true;
+    }
+    if (m_finalized__) {
+        TLVF_LOG(DEBUG) << "finalize() called for already finalized class";
+        return true;
+    }
+    if (!isPostInitSucceeded()) {
+        TLVF_LOG(ERROR) << "post init check failed";
+        return false;
+    }
+    if (m_inner__) {
+        if (!m_inner__->finalize()) {
+            TLVF_LOG(ERROR) << "m_inner__->finalize() failed";
+            return false;
+        }
+        auto tailroom = m_inner__->getMessageBuffLength() - m_inner__->getMessageLength();
+        m_buff_ptr__ -= tailroom;
+    }
+    class_swap();
+    m_finalized__ = true;
+    return true;
+}
+
+size_t cACTION_BACKHAUL_TRIGGER_ON_BOOT_SCAN::get_initial_size()
+{
+    size_t class_size = 0;
+    class_size += sizeof(sMacAddr); // radio_mac
+    return class_size;
+}
+
+bool cACTION_BACKHAUL_TRIGGER_ON_BOOT_SCAN::init()
+{
+    if (getBuffRemainingBytes() < get_initial_size()) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    m_radio_mac = reinterpret_cast<sMacAddr*>(m_buff_ptr__);
+    if (!buffPtrIncrementSafe(sizeof(sMacAddr))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(sMacAddr) << ") Failed!";
+        return false;
+    }
+    if (!m_parse__) { m_radio_mac->struct_init(); }
+    if (m_parse__) { class_swap(); }
+    return true;
+}
+
 cACTION_BACKHAUL_CHANNEL_SCAN_TRIGGER_SCAN_REQUEST::cACTION_BACKHAUL_CHANNEL_SCAN_TRIGGER_SCAN_REQUEST(uint8_t* buff, size_t buff_len, bool parse) :
     BaseClass(buff, buff_len, parse) {
     m_init_succeeded = init();
