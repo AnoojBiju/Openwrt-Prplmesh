a8187524290083dfb4ecd9dd5a6c1e80f7c617a0
Maarten De Decker <maarten.dedecker@mind.be>
2024-03-14 17:28:02 +0100

    TEST: increase delay after bringing up eth interface PPM-2684
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -170,9 +170,9 @@ main() {
         OPENWRT_TOOLCHAIN_VERSION='833823239904b80e15dd4286a3891c68055bb0a0'
         OPENWRT_VERSION='833823239904b80e15dd4286a3891c68055bb0a0'
     elif [[ "urx_osp" == "$TARGET_DEVICE" ]] ; then
-        dbg "OSP platform, build on prplos UPDK 9.1.50 + pWHM 5.42.5"
-        OPENWRT_TOOLCHAIN_VERSION='00c70dbd3c6ba0b5d6d4fbf6e3f909046ab58304'
-        OPENWRT_VERSION='00c70dbd3c6ba0b5d6d4fbf6e3f909046ab58304'
+        dbg "OSP platform, build on prplos UPDK 9.1.50 + pWHM 5.42.9"
+        OPENWRT_TOOLCHAIN_VERSION='a1b4ee5cb12720570dceb9572f0e886f492ece6b'
+        OPENWRT_VERSION='a1b4ee5cb12720570dceb9572f0e886f492ece6b'
     else
         dbg "Building on prplOS-next"
     fi
