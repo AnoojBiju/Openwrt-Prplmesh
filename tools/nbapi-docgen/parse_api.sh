445f8a58db505680dba2852e27bc54ee81081592
Frederik Van Bogaert <frederik.vanbogaert@mind.be>
2024-04-11 14:15:45 +0200

    ci:nbapi-docgen: generate agent datamodel documentation
    See PPM-2394
    
    We have automatic generation of controller data models in our CI,
    	but not of the agent DM.
    This MR makes sure to also parse the Agent DM.
    
    Note that this does not include the pWHM. pWHM documentation generation
    	needs to be added once part of the CI moves to prplOS/RDK.
    This is because pWHM is a separate project,
    	and pulling it in here would cause a lot of overhead that
    	we would anyway need to undo later.
    
    Signed-off-by: Frederik Van Bogaert <frederik.vanbogaert@mind.be>

--- tools/nbapi-docgen/parse_api.sh
+++ tools/nbapi-docgen/parse_api.sh
@@ -3,22 +3,29 @@
 scriptdir="$(cd "${0%/*}" || exit 1; pwd)"
 rootdir="${scriptdir%/*/*}"
 
-[ -z "$VERSION" ] && VERSION="custom-$(date -Iminutes)"
+PPM_VERSION=$(egrep -o "prplmesh_VERSION \"[0-9]\.[0-9]\.[0-9]\"" ${rootdir}/cmake/multiap-helpers.cmake | cut -d\" -f2)
 
-mkdir -p "${rootdir}/build" && pushd "${rootdir}/build" || exit 1
-amxo-cg -G xml "${rootdir}/build/controller/nbapi/odl/controller.odl"
-popd || exit 1
+[ -z "$VERSION" ] && VERSION="${PPM_VERSION}-custom-$(date -Iminutes)"
+
+mkdir -p "${rootdir}/build"
+amxo-cg -G xml,"${rootdir}/build" "${rootdir}/build/install/config/odl/controller.odl" "${rootdir}/build/install/config/odl/agent.odl" "${rootdir}/build/install/config/odl/"
+
 if [ ! -r "${rootdir}/build/controller.odl.xml" ]; then
-    echo -e "\\033[1;31mXML generation failed -- ODL syntax issue?\\033[0m"
+    echo -e "\\033[1;31mXML generation failed (controller DM) -- ODL syntax issue?\\033[0m"
+    exit 1
+fi
+if [ ! -r "${rootdir}/build/agent.odl.xml" ]; then
+    echo -e "\\033[1;31mXML generation failed (agent DM) -- ODL syntax issue?\\033[0m"
     exit 1
 fi
 
-mkdir -p "${rootdir}/build/html/controller"
+mkdir -p "${rootdir}/build/html/prplMesh"
 amxo-xml-to -x html\
-                  -o output-dir="${rootdir}/build/html/controller"\
+                  -o output-dir="${rootdir}/build/html/prplMesh"\
                   -o title="prplMesh"\
                   -o sub-title="Northbound API"\
                   -o version="$VERSION"\
                   -o stylesheet="prpl_style.css"\
                   -o copyrights="Prpl"\
-                  "${rootdir}/build/controller.odl.xml"
+                  "${rootdir}/build/controller.odl.xml" "${rootdir}/build/agent.odl.xml"
+
