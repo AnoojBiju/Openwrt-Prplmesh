3732f7488e0054118812e9f67329eb16a99f212c
Pooja <ppoovendran@maxlinear.com>
2024-07-04 17:16:02 +0800

    tlvf: handle tlvTIDtoLinkMappingPolicy in CmduMessageRx
    Handle the TID to Link Mapping Policy TLV in parseNextTlv()
    
    https://prplfoundationcloud.atlassian.net/browse/PPM-2914
    
    Signed-off-by: Pooja <ppoovendran@maxlinear.com>

--- framework/tlvf/src/src/CmduMessageRx.cpp
+++ framework/tlvf/src/src/CmduMessageRx.cpp
@@ -123,6 +123,7 @@
 #include <tlvf/wfa_map/tlvSteeringPolicy.h>
 #include <tlvf/wfa_map/tlvSteeringRequest.h>
 #include <tlvf/wfa_map/tlvSupportedService.h>
+#include <tlvf/wfa_map/tlvTIDtoLinkMappingPolicy.h>
 #include <tlvf/wfa_map/tlvTimestamp.h>
 #include <tlvf/wfa_map/tlvTransmitPowerLimit.h>
 #include <tlvf/wfa_map/tlvTriggerChannelSwitchAnnouncement.h>
@@ -583,9 +584,11 @@ std::shared_ptr<BaseClass> CmduMessageRx::parseNextTlv(wfa_map::eTlvTypeMap tlv_
     case (wfa_map::eTlvTypeMap::TLV_MLD_STRUCTURE): {
         return msg.addClass<wfa_map::tlvMldStructure>();
     }
+    case (wfa_map::eTlvTypeMap::TLV_TID_TO_LINK_MAPPING_POLICY): {
+        return msg.addClass<wfa_map::tlvTIDtoLinkMappingPolicy>();
+    }
     case (wfa_map::eTlvTypeMap::TLV_EHT_OPERATIONS): {
         return msg.addClass<wfa_map::tlvEHTOperations>();
-    }
     }
     LOG(FATAL) << "Unknown TLV type: " << unsigned(tlv_type);
     return msg.addClass<tlvUnknown>();
