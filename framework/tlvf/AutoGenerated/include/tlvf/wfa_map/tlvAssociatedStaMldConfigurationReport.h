d37ee986c7acbfd66c1ea72ded490a519560be21
HENRIQUES Nicolas <prplmesh.opensources@sagemcom.com>
2024-07-08 15:10:35 +0200

    tlvf: associated STA mld configuration refacto
    Signed-off-by: HENRIQUES Nicolas <prplmesh.opensources@sagemcom.com>

--- framework/tlvf/AutoGenerated/include/tlvf/wfa_map/tlvAssociatedStaMldConfigurationReport.h
+++ framework/tlvf/AutoGenerated/include/tlvf/wfa_map/tlvAssociatedStaMldConfigurationReport.h
@@ -28,7 +28,7 @@
 
 namespace wfa_map {
 
-class cAffliatedsta;
+class cAffiliatedSta;
 
 class tlvAssociatedStaMldConfigurationReport : public BaseClass
 {
@@ -37,18 +37,18 @@ class tlvAssociatedStaMldConfigurationReport : public BaseClass
         explicit tlvAssociatedStaMldConfigurationReport(std::shared_ptr<BaseClass> base, bool parse = false);
         ~tlvAssociatedStaMldConfigurationReport();
 
-        typedef struct sFlags {
+        typedef struct sModes {
             #if defined(__LITTLE_ENDIAN_BITFIELD)
             uint8_t reserved : 4;
-            uint8_t emlmr_mode : 1;
-            uint8_t emlsr_mode : 1;
-            uint8_t nstr_mode : 1;
-            uint8_t str_mode : 1;
+            uint8_t emlmr : 1;
+            uint8_t emlsr : 1;
+            uint8_t nstr : 1;
+            uint8_t str : 1;
             #elif defined(__BIG_ENDIAN_BITFIELD)
-            uint8_t str_mode : 1;
-            uint8_t nstr_mode : 1;
-            uint8_t emlsr_mode : 1;
-            uint8_t emlmr_mode : 1;
+            uint8_t str : 1;
+            uint8_t nstr : 1;
+            uint8_t emlsr : 1;
+            uint8_t emlmr : 1;
             uint8_t reserved : 4;
             #else
             #error "Bitfield macros are not defined"
@@ -57,19 +57,19 @@ class tlvAssociatedStaMldConfigurationReport : public BaseClass
             }
             void struct_init(){
             }
-        } __attribute__((packed)) sFlags;
+        } __attribute__((packed)) sModes;
         
         const eTlvTypeMap& type();
         const uint16_t& length();
         sMacAddr& sta_mld_mac_addr();
         sMacAddr& ap_mld_mac_addr();
-        sFlags& flags();
+        sModes& modes();
         uint8_t* reserved(size_t idx = 0);
         bool set_reserved(const void* buffer, size_t size);
-        uint8_t& num_affliated_sta_length();
-        std::tuple<bool, cAffliatedsta&> affliated_sta(size_t idx);
-        std::shared_ptr<cAffliatedsta> create_affliated_sta();
-        bool add_affliated_sta(std::shared_ptr<cAffliatedsta> ptr);
+        uint8_t& num_affiliated_sta();
+        std::tuple<bool, cAffiliatedSta&> affiliated_sta(size_t idx);
+        std::shared_ptr<cAffiliatedSta> create_affiliated_sta();
+        bool add_affiliated_sta(std::shared_ptr<cAffiliatedSta> ptr);
         void class_swap() override;
         bool finalize() override;
         static size_t get_initial_size();
@@ -80,26 +80,26 @@ class tlvAssociatedStaMldConfigurationReport : public BaseClass
         uint16_t* m_length = nullptr;
         sMacAddr* m_sta_mld_mac_addr = nullptr;
         sMacAddr* m_ap_mld_mac_addr = nullptr;
-        sFlags* m_flags = nullptr;
+        sModes* m_modes = nullptr;
         uint8_t* m_reserved = nullptr;
         size_t m_reserved_idx__ = 0;
         int m_lock_order_counter__ = 0;
-        uint8_t* m_num_affliated_sta_length = nullptr;
-        cAffliatedsta* m_affliated_sta = nullptr;
-        size_t m_affliated_sta_idx__ = 0;
-        std::vector<std::shared_ptr<cAffliatedsta>> m_affliated_sta_vector;
+        uint8_t* m_num_affiliated_sta = nullptr;
+        cAffiliatedSta* m_affiliated_sta = nullptr;
+        size_t m_affiliated_sta_idx__ = 0;
+        std::vector<std::shared_ptr<cAffiliatedSta>> m_affiliated_sta_vector;
         bool m_lock_allocation__ = false;
 };
 
-class cAffliatedsta : public BaseClass
+class cAffiliatedSta : public BaseClass
 {
     public:
-        cAffliatedsta(uint8_t* buff, size_t buff_len, bool parse = false);
-        explicit cAffliatedsta(std::shared_ptr<BaseClass> base, bool parse = false);
-        ~cAffliatedsta();
+        cAffiliatedSta(uint8_t* buff, size_t buff_len, bool parse = false);
+        explicit cAffiliatedSta(std::shared_ptr<BaseClass> base, bool parse = false);
+        ~cAffiliatedSta();
 
         sMacAddr& bssid();
-        sMacAddr& affliated_sta_mac_addr();
+        sMacAddr& affiliated_sta_mac_addr();
         uint8_t* reserved(size_t idx = 0);
         bool set_reserved(const void* buffer, size_t size);
         void class_swap() override;
@@ -109,7 +109,7 @@ class cAffliatedsta : public BaseClass
     private:
         bool init();
         sMacAddr* m_bssid = nullptr;
-        sMacAddr* m_affliated_sta_mac_addr = nullptr;
+        sMacAddr* m_affiliated_sta_mac_addr = nullptr;
         uint8_t* m_reserved = nullptr;
         size_t m_reserved_idx__ = 0;
         int m_lock_order_counter__ = 0;
--- framework/tlvf/AutoGenerated/src/tlvf/wfa_map/tlvAssociatedStaMldConfigurationReport.cpp
+++ framework/tlvf/AutoGenerated/src/tlvf/wfa_map/tlvAssociatedStaMldConfigurationReport.cpp
@@ -41,8 +41,8 @@ sMacAddr& tlvAssociatedStaMldConfigurationReport::ap_mld_mac_addr() {
     return (sMacAddr&)(*m_ap_mld_mac_addr);
 }
 
-tlvAssociatedStaMldConfigurationReport::sFlags& tlvAssociatedStaMldConfigurationReport::flags() {
-    return (sFlags&)(*m_flags);
+tlvAssociatedStaMldConfigurationReport::sModes& tlvAssociatedStaMldConfigurationReport::modes() {
+    return (sModes&)(*m_modes);
 }
 
 uint8_t* tlvAssociatedStaMldConfigurationReport::reserved(size_t idx) {
@@ -65,25 +65,25 @@ bool tlvAssociatedStaMldConfigurationReport::set_reserved(const void* buffer, si
     std::copy_n(reinterpret_cast<const uint8_t *>(buffer), size, m_reserved);
     return true;
 }
-uint8_t& tlvAssociatedStaMldConfigurationReport::num_affliated_sta_length() {
-    return (uint8_t&)(*m_num_affliated_sta_length);
+uint8_t& tlvAssociatedStaMldConfigurationReport::num_affiliated_sta() {
+    return (uint8_t&)(*m_num_affiliated_sta);
 }
 
-std::tuple<bool, cAffliatedsta&> tlvAssociatedStaMldConfigurationReport::affliated_sta(size_t idx) {
-    bool ret_success = ( (m_affliated_sta_idx__ > 0) && (m_affliated_sta_idx__ > idx) );
+std::tuple<bool, cAffiliatedSta&> tlvAssociatedStaMldConfigurationReport::affiliated_sta(size_t idx) {
+    bool ret_success = ( (m_affiliated_sta_idx__ > 0) && (m_affiliated_sta_idx__ > idx) );
     size_t ret_idx = ret_success ? idx : 0;
     if (!ret_success) {
         TLVF_LOG(ERROR) << "Requested index is greater than the number of available entries";
     }
-    return std::forward_as_tuple(ret_success, *(m_affliated_sta_vector[ret_idx]));
+    return std::forward_as_tuple(ret_success, *(m_affiliated_sta_vector[ret_idx]));
 }
 
-std::shared_ptr<cAffliatedsta> tlvAssociatedStaMldConfigurationReport::create_affliated_sta() {
+std::shared_ptr<cAffiliatedSta> tlvAssociatedStaMldConfigurationReport::create_affiliated_sta() {
     if (m_lock_order_counter__ > 0) {
-        TLVF_LOG(ERROR) << "Out of order allocation for variable length list affliated_sta, abort!";
+        TLVF_LOG(ERROR) << "Out of order allocation for variable length list affiliated_sta, abort!";
         return nullptr;
     }
-    size_t len = cAffliatedsta::get_initial_size();
+    size_t len = cAffiliatedSta::get_initial_size();
     if (m_lock_allocation__) {
         TLVF_LOG(ERROR) << "Can't create new element before adding the previous one";
         return nullptr;
@@ -94,30 +94,30 @@ std::shared_ptr<cAffliatedsta> tlvAssociatedStaMldConfigurationReport::create_af
     }
     m_lock_order_counter__ = 0;
     m_lock_allocation__ = true;
-    uint8_t *src = (uint8_t *)m_affliated_sta;
-    if (m_affliated_sta_idx__ > 0) {
-        src = (uint8_t *)m_affliated_sta_vector[m_affliated_sta_idx__ - 1]->getBuffPtr();
+    uint8_t *src = (uint8_t *)m_affiliated_sta;
+    if (m_affiliated_sta_idx__ > 0) {
+        src = (uint8_t *)m_affiliated_sta_vector[m_affiliated_sta_idx__ - 1]->getBuffPtr();
     }
     if (!m_parse__) {
         uint8_t *dst = src + len;
         size_t move_length = getBuffRemainingBytes(src) - len;
         std::copy_n(src, move_length, dst);
     }
-    return std::make_shared<cAffliatedsta>(src, getBuffRemainingBytes(src), m_parse__);
+    return std::make_shared<cAffiliatedSta>(src, getBuffRemainingBytes(src), m_parse__);
 }
 
-bool tlvAssociatedStaMldConfigurationReport::add_affliated_sta(std::shared_ptr<cAffliatedsta> ptr) {
+bool tlvAssociatedStaMldConfigurationReport::add_affiliated_sta(std::shared_ptr<cAffiliatedSta> ptr) {
     if (ptr == nullptr) {
         TLVF_LOG(ERROR) << "Received entry is nullptr";
         return false;
     }
     if (m_lock_allocation__ == false) {
-        TLVF_LOG(ERROR) << "No call to create_affliated_sta was called before add_affliated_sta";
+        TLVF_LOG(ERROR) << "No call to create_affiliated_sta was called before add_affiliated_sta";
         return false;
     }
-    uint8_t *src = (uint8_t *)m_affliated_sta;
-    if (m_affliated_sta_idx__ > 0) {
-        src = (uint8_t *)m_affliated_sta_vector[m_affliated_sta_idx__ - 1]->getBuffPtr();
+    uint8_t *src = (uint8_t *)m_affiliated_sta;
+    if (m_affiliated_sta_idx__ > 0) {
+        src = (uint8_t *)m_affiliated_sta_vector[m_affiliated_sta_idx__ - 1]->getBuffPtr();
     }
     if (ptr->getStartBuffPtr() != src) {
         TLVF_LOG(ERROR) << "Received entry pointer is different than expected (expecting the same pointer returned from add method)";
@@ -127,10 +127,10 @@ bool tlvAssociatedStaMldConfigurationReport::add_affliated_sta(std::shared_ptr<c
         TLVF_LOG(ERROR) << "Not enough available space on buffer";
         return false;
     }
-    m_affliated_sta_idx__++;
-    if (!m_parse__) { (*m_num_affliated_sta_length)++; }
+    m_affiliated_sta_idx__++;
+    if (!m_parse__) { (*m_num_affiliated_sta)++; }
     size_t len = ptr->getLen();
-    m_affliated_sta_vector.push_back(ptr);
+    m_affiliated_sta_vector.push_back(ptr);
     if (!buffPtrIncrementSafe(len)) {
         LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << len << ") Failed!";
         return false;
@@ -145,9 +145,9 @@ void tlvAssociatedStaMldConfigurationReport::class_swap()
     tlvf_swap(16, reinterpret_cast<uint8_t*>(m_length));
     m_sta_mld_mac_addr->struct_swap();
     m_ap_mld_mac_addr->struct_swap();
-    m_flags->struct_swap();
-    for (size_t i = 0; i < m_affliated_sta_idx__; i++){
-        std::get<1>(affliated_sta(i)).class_swap();
+    m_modes->struct_swap();
+    for (size_t i = 0; i < m_affiliated_sta_idx__; i++){
+        std::get<1>(affiliated_sta(i)).class_swap();
     }
 }
 
@@ -186,9 +186,9 @@ size_t tlvAssociatedStaMldConfigurationReport::get_initial_size()
     class_size += sizeof(uint16_t); // length
     class_size += sizeof(sMacAddr); // sta_mld_mac_addr
     class_size += sizeof(sMacAddr); // ap_mld_mac_addr
-    class_size += sizeof(sFlags); // flags
+    class_size += sizeof(sModes); // modes
     class_size += 18 * sizeof(uint8_t); // reserved
-    class_size += sizeof(uint8_t); // num_affliated_sta_length
+    class_size += sizeof(uint8_t); // num_affiliated_sta
     return class_size;
 }
 
@@ -224,13 +224,13 @@ bool tlvAssociatedStaMldConfigurationReport::init()
     }
     if(m_length && !m_parse__){ (*m_length) += sizeof(sMacAddr); }
     if (!m_parse__) { m_ap_mld_mac_addr->struct_init(); }
-    m_flags = reinterpret_cast<sFlags*>(m_buff_ptr__);
-    if (!buffPtrIncrementSafe(sizeof(sFlags))) {
-        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(sFlags) << ") Failed!";
+    m_modes = reinterpret_cast<sModes*>(m_buff_ptr__);
+    if (!buffPtrIncrementSafe(sizeof(sModes))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(sModes) << ") Failed!";
         return false;
     }
-    if(m_length && !m_parse__){ (*m_length) += sizeof(sFlags); }
-    if (!m_parse__) { m_flags->struct_init(); }
+    if(m_length && !m_parse__){ (*m_length) += sizeof(sModes); }
+    if (!m_parse__) { m_modes->struct_init(); }
     m_reserved = reinterpret_cast<uint8_t*>(m_buff_ptr__);
     if (!buffPtrIncrementSafe(sizeof(uint8_t) * (18))) {
         LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(uint8_t) * (18) << ") Failed!";
@@ -240,28 +240,28 @@ bool tlvAssociatedStaMldConfigurationReport::init()
     if (!m_parse__) {
         if (m_length) { (*m_length) += (sizeof(uint8_t) * 18); }
     }
-    m_num_affliated_sta_length = reinterpret_cast<uint8_t*>(m_buff_ptr__);
-    if (!m_parse__) *m_num_affliated_sta_length = 0;
+    m_num_affiliated_sta = reinterpret_cast<uint8_t*>(m_buff_ptr__);
+    if (!m_parse__) *m_num_affiliated_sta = 0;
     if (!buffPtrIncrementSafe(sizeof(uint8_t))) {
         LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(uint8_t) << ") Failed!";
         return false;
     }
     if(m_length && !m_parse__){ (*m_length) += sizeof(uint8_t); }
-    m_affliated_sta = reinterpret_cast<cAffliatedsta*>(m_buff_ptr__);
-    uint8_t num_affliated_sta_length = *m_num_affliated_sta_length;
-    m_affliated_sta_idx__ = 0;
-    for (size_t i = 0; i < num_affliated_sta_length; i++) {
-        auto affliated_sta = create_affliated_sta();
-        if (!affliated_sta || !affliated_sta->isInitialized()) {
-            TLVF_LOG(ERROR) << "create_affliated_sta() failed";
+    m_affiliated_sta = reinterpret_cast<cAffiliatedSta*>(m_buff_ptr__);
+    uint8_t num_affiliated_sta = *m_num_affiliated_sta;
+    m_affiliated_sta_idx__ = 0;
+    for (size_t i = 0; i < num_affiliated_sta; i++) {
+        auto affiliated_sta = create_affiliated_sta();
+        if (!affiliated_sta || !affiliated_sta->isInitialized()) {
+            TLVF_LOG(ERROR) << "create_affiliated_sta() failed";
             return false;
         }
-        if (!add_affliated_sta(affliated_sta)) {
-            TLVF_LOG(ERROR) << "add_affliated_sta() failed";
+        if (!add_affiliated_sta(affiliated_sta)) {
+            TLVF_LOG(ERROR) << "add_affiliated_sta() failed";
             return false;
         }
-        // swap back since affliated_sta will be swapped as part of the whole class swap
-        affliated_sta->class_swap();
+        // swap back since affiliated_sta will be swapped as part of the whole class swap
+        affiliated_sta->class_swap();
     }
     if (m_parse__) { class_swap(); }
     if (m_parse__) {
@@ -273,25 +273,25 @@ bool tlvAssociatedStaMldConfigurationReport::init()
     return true;
 }
 
-cAffliatedsta::cAffliatedsta(uint8_t* buff, size_t buff_len, bool parse) :
+cAffiliatedSta::cAffiliatedSta(uint8_t* buff, size_t buff_len, bool parse) :
     BaseClass(buff, buff_len, parse) {
     m_init_succeeded = init();
 }
-cAffliatedsta::cAffliatedsta(std::shared_ptr<BaseClass> base, bool parse) :
+cAffiliatedSta::cAffiliatedSta(std::shared_ptr<BaseClass> base, bool parse) :
 BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse){
     m_init_succeeded = init();
 }
-cAffliatedsta::~cAffliatedsta() {
+cAffiliatedSta::~cAffiliatedSta() {
 }
-sMacAddr& cAffliatedsta::bssid() {
+sMacAddr& cAffiliatedSta::bssid() {
     return (sMacAddr&)(*m_bssid);
 }
 
-sMacAddr& cAffliatedsta::affliated_sta_mac_addr() {
-    return (sMacAddr&)(*m_affliated_sta_mac_addr);
+sMacAddr& cAffiliatedSta::affiliated_sta_mac_addr() {
+    return (sMacAddr&)(*m_affiliated_sta_mac_addr);
 }
 
-uint8_t* cAffliatedsta::reserved(size_t idx) {
+uint8_t* cAffiliatedSta::reserved(size_t idx) {
     if ( (m_reserved_idx__ == 0) || (m_reserved_idx__ <= idx) ) {
         TLVF_LOG(ERROR) << "Requested index is greater than the number of available entries";
         return nullptr;
@@ -299,7 +299,7 @@ uint8_t* cAffliatedsta::reserved(size_t idx) {
     return &(m_reserved[idx]);
 }
 
-bool cAffliatedsta::set_reserved(const void* buffer, size_t size) {
+bool cAffiliatedSta::set_reserved(const void* buffer, size_t size) {
     if (buffer == nullptr) {
         TLVF_LOG(WARNING) << "set_reserved received a null pointer.";
         return false;
@@ -311,13 +311,13 @@ bool cAffliatedsta::set_reserved(const void* buffer, size_t size) {
     std::copy_n(reinterpret_cast<const uint8_t *>(buffer), size, m_reserved);
     return true;
 }
-void cAffliatedsta::class_swap()
+void cAffiliatedSta::class_swap()
 {
     m_bssid->struct_swap();
-    m_affliated_sta_mac_addr->struct_swap();
+    m_affiliated_sta_mac_addr->struct_swap();
 }
 
-bool cAffliatedsta::finalize()
+bool cAffiliatedSta::finalize()
 {
     if (m_parse__) {
         TLVF_LOG(DEBUG) << "finalize() called but m_parse__ is set";
@@ -344,16 +344,16 @@ bool cAffliatedsta::finalize()
     return true;
 }
 
-size_t cAffliatedsta::get_initial_size()
+size_t cAffiliatedSta::get_initial_size()
 {
     size_t class_size = 0;
     class_size += sizeof(sMacAddr); // bssid
-    class_size += sizeof(sMacAddr); // affliated_sta_mac_addr
+    class_size += sizeof(sMacAddr); // affiliated_sta_mac_addr
     class_size += 19 * sizeof(uint8_t); // reserved
     return class_size;
 }
 
-bool cAffliatedsta::init()
+bool cAffiliatedSta::init()
 {
     if (getBuffRemainingBytes() < get_initial_size()) {
         TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
@@ -365,12 +365,12 @@ bool cAffliatedsta::init()
         return false;
     }
     if (!m_parse__) { m_bssid->struct_init(); }
-    m_affliated_sta_mac_addr = reinterpret_cast<sMacAddr*>(m_buff_ptr__);
+    m_affiliated_sta_mac_addr = reinterpret_cast<sMacAddr*>(m_buff_ptr__);
     if (!buffPtrIncrementSafe(sizeof(sMacAddr))) {
         LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(sMacAddr) << ") Failed!";
         return false;
     }
-    if (!m_parse__) { m_affliated_sta_mac_addr->struct_init(); }
+    if (!m_parse__) { m_affiliated_sta_mac_addr->struct_init(); }
     m_reserved = reinterpret_cast<uint8_t*>(m_buff_ptr__);
     if (!buffPtrIncrementSafe(sizeof(uint8_t) * (19))) {
         LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(uint8_t) * (19) << ") Failed!";
