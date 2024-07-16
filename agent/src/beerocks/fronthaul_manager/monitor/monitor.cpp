e271384f8ec663afbff01ea348b88b3aead0f1c3
rramasamy <rramasamy@maxlinear.com>
2024-07-16 12:02:47 +0530

    Testing monitor
    Signed-off-by: rramasamy <rramasamy@maxlinear.com>

--- agent/src/beerocks/fronthaul_manager/monitor/monitor.cpp
+++ agent/src/beerocks/fronthaul_manager/monitor/monitor.cpp
@@ -1922,6 +1922,8 @@ bool Monitor::hal_event_handler(bwl::base_wlan_hal::hal_event_ptr_t event_ptr)
 
     } break;
 
+    case Event::Interface_Connected_OK:
+    case Event::Interface_Reconnected_OK:
     case Event::AP_Enabled: {
         if (!data) {
             LOG(ERROR) << "AP_Enabled without data";
@@ -1933,6 +1935,7 @@ bool Monitor::hal_event_handler(bwl::base_wlan_hal::hal_event_ptr_t event_ptr)
         update_vaps_in_db();
     } break;
 
+    case Event::Interface_Disconnected:
     case Event::AP_Disabled: {
         if (!data) {
             LOG(ERROR) << "AP_Disabled without data!";
